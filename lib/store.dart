00c09d597c60d1f9b14f71bf41f5eb8e7ec5334b
Greg Price <gnprice@gmail.com>
2022-12-18 16:47:37 -0800

    Cut a bunch of template demo code; begin to give structure

---
+++ lib/store.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/foundation.dart';
+
+class PerAccountStore extends ChangeNotifier {
+  // Load the user's data from storage.  (Once we have such a thing.)
+  PerAccountStore.load();
+}
