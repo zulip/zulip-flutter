6e55dbf769dc76008feea130a4c068236b72c40a
Greg Price <gnprice@gmail.com>
2022-12-18 23:16:11 -0800

    msglist: Parse HTML; start rendering it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart';
 
 import 'api/model/model.dart';
 import 'api/route/messages.dart';
@@ -176,7 +178,8 @@ class MessageItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-        child: Column(children: [
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           // TODO recipient headings
           SenderHeading(message: message),
           MessageContent(message: message),
@@ -210,8 +213,76 @@ class MessageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final fragment =
+        HtmlParser(message.content, parseMeta: false).parseFragment();
+    final nodes = fragment.nodes.where(_acceptNode);
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Text(message.content),
+      ...nodes.map(_buildDirectChildNode),
     ]);
   }
+
+  static bool _acceptNode(dom.Node node) {
+    if (node is dom.Element) return true;
+    // We get a bunch of newline Text nodes between paragraphs.
+    // A browser seems to ignore these; let's do the same.
+    if (node is dom.Text && (node.text == "\n")) return false;
+    // Does any other kind of node occur?  Well, we'd see it below.
+    return true;
+  }
+
+  Widget _buildDirectChildNode(dom.Node node) {
+    switch (node.nodeType) {
+      case dom.Node.ELEMENT_NODE:
+        return _buildDirectChildElement(node as dom.Element);
+      case dom.Node.TEXT_NODE:
+        final text = (node as dom.Text).text;
+        return _errorText("text: «$text»"); // TODO can this happen?
+      default:
+        return _errorText(
+            "(node of type ${node.nodeType})"); // TODO can this happen?
+    }
+  }
+
+  Widget _buildDirectChildElement(dom.Element element) {
+    switch (element.localName) {
+      case 'p':
+        return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child:
+                Text.rich(TextSpan(children: _buildInlineList(element.nodes))));
+      default:
+        // TODO handle more types of elements
+        return Text.rich(_errorUnimplemented(element));
+    }
+  }
+
+  List<InlineSpan> _buildInlineList(dom.NodeList nodes) =>
+      List.of(nodes.map(_buildInlineNode));
+
+  InlineSpan _buildInlineNode(dom.Node node) {
+    if (node is dom.Text) return TextSpan(text: node.text);
+    if (node is! dom.Element) {
+      return TextSpan(
+          text: "(unimplemented dom.Node type: ${node.nodeType})",
+          style: errorStyle);
+    }
+    switch (node.localName) {
+      default:
+        return _errorUnimplemented(node);
+    }
+  }
+
+  Widget _errorText(String text) => Text(text, style: errorStyle);
+
+  InlineSpan _errorUnimplemented(dom.Element element) => TextSpan(children: [
+        const TextSpan(text: "(unimplemented:", style: errorStyle),
+        TextSpan(text: element.outerHtml, style: errorCodeStyle),
+        const TextSpan(text: ")", style: errorStyle),
+      ]);
+
+  static const errorStyle =
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.red);
+
+  static const errorCodeStyle =
+      TextStyle(color: Colors.red, fontFamily: 'monospace');
 }
