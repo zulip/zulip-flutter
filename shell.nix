55a377c703b3b76b28cb4a18a9ef7e0788353d85
Greg Price <greg@zulip.com>
2022-12-21 16:30:03 -0800

    nixos: Add more inputs to shell.nix, silencing warnings

--- shell.nix
+++ shell.nix
@@ -14,6 +14,23 @@ mkShell {
     pcre
     epoxy
 
+    # This group all seem not strictly necessary -- commands like
+    # `flutter run -d linux` seem to *work* fine without them, but
+    # the build does print messages about missing packages, like:
+    #   Package mount was not found in the pkg-config search path.
+    #   Perhaps you should add the directory containing `mount.pc'
+    #   to the PKG_CONFIG_PATH environment variable
+    libuuid  # for mount.pc
+    xorg.libXdmcp.dev
+    python310Packages.libselinux.dev # for libselinux.pc
+    libsepol.dev
+    libthai.dev
+    libdatrie.dev
+    libxkbcommon.dev
+    dbus.dev
+    at-spi2-core.dev
+    xorg.libXtst.out
+
     jdk11
     android-studio
     android-tools
