435163fd47813a293b1d222e662aa2cd997bd422
Chris Bobbe <cbobbe@zulip.com>
2023-02-24 16:15:12 -0800

    deps: Add share_plus plugin, for sharing from Zulip to other apps
    This plugin seems to work fine so far. When using its main API,
    `Share.share()`, on iOS, I get some log output that could be noise,
    and also some that might hint at some layout glitches but that I
    haven't noticed as being user-visible:
      https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/flutter.3A.20.60share_plus.60/near/1506606
    
    Here's what I did to install:
    - Added the new dependency line to pubspec.yaml
    
    - Ran `flutter pub get`
    
    - Tried to build/run on my iPhone, and in the `pod install` phase,
      got this error:
    
        [!] Automatically assigning platform `iOS` with version `11.0`
        on target `Runner` because no platform was specified. Please
        specify a platform for this target in your Podfile. See
        `https://guides.cocoapods.org/syntax/podfile.html#platform`
    
      and fixed by uncommenting the `platform` line in ios/Podfile
    
    - Tried to build/run on my iPhone, and in the `pod install` phase,
      got a different error:
    
        [!] Unable to find a target named `RunnerTests` in project
        `Runner.xcodeproj`, did find `Runner`.
    
      and fixed by removing a `target 'RunnerTests' do` block in the iOS
      and macOS Podfiles. Those blocks were added because of
      flutter/flutter@2a502363e, which we recently took when upgrading
      to the Flutter beta 3.8.0-10.1.pre. We were missing new iOS code
      that those blocks refer to, added in that same Flutter commit.
      There doesn't seem to be an automatic step that would give us that
      code, but docs are in the works if we ever want to add it
      manually:
        https://github.com/flutter/flutter/issues/96421#issuecomment-1444401288
      Anyway, it doesn't seem like a lot of code; probably a simple
      "Hello World" example. Leave it out for now, but cleanly, by
      deleting those dangling references in the Podfiles.
    
    - Built and ran successfully on my iPhone; the `pod install` phase
      succeeded, generating ios/Podfile.lock and adding
      reasonable-looking lines in some Xcode files in ios/ (CocoaPods
      stuff).
    
    - (Same with macOS, for the files in macos/.)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
