483a3ef1df4ac2286bc52597ed06e726d69547b8
Greg Price <greg@zulip.com>
2023-02-27 14:19:09 -0800

    deps: Add drift, sqlite3_flutter_libs, path_provider, path, dev:drift_dev
    These are the dependencies called for by the Drift docs:
      https://drift.simonbinder.eu/docs/getting-started/#adding-the-dependency

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import device_info_plus
 import path_provider_foundation
 import share_plus
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
