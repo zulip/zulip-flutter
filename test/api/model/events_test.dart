ee632b647a853a0905977687357679e07e7a4822
Greg Price <greg@zulip.com>
2023-03-06 14:40:16 -0800

    api test: Test our rearranging of flags on message events

---
+++ test/api/model/events_test.dart
@@ -0,0 +1,22 @@
+import 'package:checks/checks.dart';
+import 'package:test/scaffolding.dart';
+import 'package:zulip/api/model/events.dart';
+
+import '../../example_data.dart' as eg;
+import 'events_checks.dart';
+import 'model_checks.dart';
+
+void main() {
+  test('message: move flags into message object', () {
+    final message = eg.streamMessage();
+    MessageEvent mkEvent(List<String> flags) => Event.fromJson({
+        'type': 'message',
+        'id': 1,
+        'message': message.toJson()..remove('flags'),
+        'flags': flags,
+      }) as MessageEvent;
+    check(mkEvent(message.flags)).message.jsonEquals(message);
+    check(mkEvent([])).message.flags.deepEquals([]);
+    check(mkEvent(['read'])).message.flags.deepEquals(['read']);
+  });
+}
