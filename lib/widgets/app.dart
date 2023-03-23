5db427d54f31462ba07dfc6142d1d48f8662dcaa
Greg Price <greg@zulip.com>
2023-03-23 12:51:40 -0700

    widgets/app [nfc]: Use two-space indents consistently
    Much of this code was written before I had a firm grasp on how
    indentation in Dart code is supposed to work.  Among other things,
    this meant I often let the auto-formatting in Android Studio do
    what it seemed to want, even when it was confused.
    
    This diff should be empty under `git diff -b`.  I'll make
    small other tweaks as a separate commit.

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -11,15 +11,15 @@ class ZulipApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = ThemeData(
-        // This applies Material 3's color system to produce a palette of
-        // appropriately matching and contrasting colors for use in a UI.
-        // The Zulip brand color is a starting point, but doesn't end up as
-        // one that's directly used.  (After all, we didn't design it for that
-        // purpose; we designed a logo.)  See docs:
-        //   https://api.flutter.dev/flutter/material/ColorScheme/ColorScheme.fromSeed.html
-        // Or try this tool to see the whole palette:
-        //   https://m3.material.io/theme-builder#/custom
-        colorScheme: ColorScheme.fromSeed(seedColor: kZulipBrandColor));
+      // This applies Material 3's color system to produce a palette of
+      // appropriately matching and contrasting colors for use in a UI.
+      // The Zulip brand color is a starting point, but doesn't end up as
+      // one that's directly used.  (After all, we didn't design it for that
+      // purpose; we designed a logo.)  See docs:
+      //   https://api.flutter.dev/flutter/material/ColorScheme/ColorScheme.fromSeed.html
+      // Or try this tool to see the whole palette:
+      //   https://m3.material.io/theme-builder#/custom
+      colorScheme: ColorScheme.fromSeed(seedColor: kZulipBrandColor));
     return GlobalStoreWidget(
       child: PerAccountStoreWidget(
         // Just one account for now.
@@ -45,37 +45,37 @@ class HomePage extends StatelessWidget {
     final store = PerAccountStoreWidget.of(context);
 
     InlineSpan bold(String text) => TextSpan(
-        text: text, style: const TextStyle(fontWeight: FontWeight.bold));
+      text: text, style: const TextStyle(fontWeight: FontWeight.bold));
 
     return Scaffold(
-        appBar: AppBar(title: const Text("Home")),
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          DefaultTextStyle.merge(
+      appBar: AppBar(title: const Text("Home")),
+      body: Center(
+        child:
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            DefaultTextStyle.merge(
               style: const TextStyle(fontSize: 18),
               child: Column(children: [
                 const Text('🚧 Under construction 🚧'),
                 const SizedBox(height: 12),
                 Text.rich(TextSpan(
-                    text: 'Connected to: ',
-                    children: [bold(store.account.realmUrl)])),
+                  text: 'Connected to: ',
+                  children: [bold(store.account.realmUrl)])),
                 Text.rich(TextSpan(
-                    text: 'Zulip server version: ',
-                    children: [bold(store.zulip_version)])),
+                  text: 'Zulip server version: ',
+                  children: [bold(store.zulip_version)])),
                 Text.rich(TextSpan(text: 'Subscribed to ', children: [
                   bold(store.subscriptions.length.toString()),
                   const TextSpan(text: ' streams'),
                 ])),
               ])),
-          const SizedBox(height: 16),
-          ElevatedButton(
+            const SizedBox(height: 16),
+            ElevatedButton(
               onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const MessageListPage())),
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MessageListPage())),
               child: const Text("All messages"))
-        ])));
+          ])));
   }
 }
 
@@ -85,21 +85,21 @@ class MessageListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("All messages")),
-        body: Builder(
-            builder: (BuildContext context) => Center(
-                child: Column(children: [
-                  MediaQuery.removePadding(
-                      // Scaffold knows about the app bar, and so has run this
-                      // BuildContext, which is under `body`, through
-                      // MediaQuery.removePadding with `removeTop: true`.
-                      context: context,
+      appBar: AppBar(title: const Text("All messages")),
+      body: Builder(
+        builder: (BuildContext context) => Center(
+          child: Column(children: [
+            MediaQuery.removePadding(
+              // Scaffold knows about the app bar, and so has run this
+              // BuildContext, which is under `body`, through
+              // MediaQuery.removePadding with `removeTop: true`.
+              context: context,
 
-                      // The compose box pads the bottom inset.
-                      removeBottom: true,
+              // The compose box pads the bottom inset.
+              removeBottom: true,
 
-                      child: const Expanded(
-                          child: MessageList())),
-                  const StreamComposeBox()]))));
+              child: const Expanded(
+                child: MessageList())),
+            const StreamComposeBox()]))));
   }
 }
