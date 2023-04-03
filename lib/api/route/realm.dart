da0cd63dfbd60e92ad3228c82f66b0f11e640566
Greg Price <greg@zulip.com>
2023-04-03 16:56:09 -0700

    api: Add get-server-settings route

---
+++ lib/api/route/realm.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+part 'realm.g.dart';
+
+/// https://zulip.com/api/get-server-settings
+///
+/// Despite the name, this is really a home for certain realm-specific
+/// settings, as well as some information about the server as a whole.
+///
+/// The Zulip server offers this endpoint at the root domain of a server,
+/// even when there is no Zulip realm at that domain.  This binding, however,
+/// only operates on an actual Zulip realm.
+// TODO(#35): Perhaps detect realmless root domain, for more specific onboarding feedback.
+//   See thread, and the zulip-mobile code and chat thread it links to:
+//     https://github.com/zulip/zulip-flutter/pull/55#discussion_r1160267577
+Future<GetServerSettingsResult> getServerSettings({
+  required String realmUrl,
+}) async {
+  // TODO dedupe this part with LiveApiConnection; make this function testable
+  final response = await http.get(
+    Uri.parse("$realmUrl/api/v1/server_settings"));
+  if (response.statusCode != 200) {
+    throw Exception('error on GET server_settings: status ${response.statusCode}');
+  }
+  final data = utf8.decode(response.bodyBytes);
+
+  final json = jsonDecode(data);
+  return GetServerSettingsResult.fromJson(json);
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class GetServerSettingsResult {
+  final Map<String, bool> authenticationMethods;
+  // final List<ExternalAuthenticationMethod> external_authentication_methods; // TODO handle
+
+  final int zulipFeatureLevel;
+  final String zulipVersion;
+  final String? zulipMergeBase; // TODO(server-5)
+
+  final bool pushNotificationsEnabled;
+  final bool isIncompatible;
+
+  final bool emailAuthEnabled;
+  final bool requireEmailFormatUsernames;
+  final String realmUri;
+  final String realmName;
+  final String realmIcon;
+  final String realmDescription;
+  final bool? realmWebPublicAccessEnabled; // TODO(server-5)
+
+  GetServerSettingsResult({
+    required this.authenticationMethods,
+    required this.zulipFeatureLevel,
+    required this.zulipVersion,
+    this.zulipMergeBase,
+    required this.pushNotificationsEnabled,
+    required this.isIncompatible,
+    required this.emailAuthEnabled,
+    required this.requireEmailFormatUsernames,
+    required this.realmUri,
+    required this.realmName,
+    required this.realmIcon,
+    required this.realmDescription,
+    this.realmWebPublicAccessEnabled,
+  });
+
+  factory GetServerSettingsResult.fromJson(Map<String, dynamic> json) =>
+    _$GetServerSettingsResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetServerSettingsResultToJson(this);
+}
---
+++ lib/api/route/realm.g.dart
@@ -0,0 +1,46 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'realm.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetServerSettingsResult _$GetServerSettingsResultFromJson(
+        Map<String, dynamic> json) =>
+    GetServerSettingsResult(
+      authenticationMethods:
+          Map<String, bool>.from(json['authentication_methods'] as Map),
+      zulipFeatureLevel: json['zulip_feature_level'] as int,
+      zulipVersion: json['zulip_version'] as String,
+      zulipMergeBase: json['zulip_merge_base'] as String?,
+      pushNotificationsEnabled: json['push_notifications_enabled'] as bool,
+      isIncompatible: json['is_incompatible'] as bool,
+      emailAuthEnabled: json['email_auth_enabled'] as bool,
+      requireEmailFormatUsernames:
+          json['require_email_format_usernames'] as bool,
+      realmUri: json['realm_uri'] as String,
+      realmName: json['realm_name'] as String,
+      realmIcon: json['realm_icon'] as String,
+      realmDescription: json['realm_description'] as String,
+      realmWebPublicAccessEnabled:
+          json['realm_web_public_access_enabled'] as bool?,
+    );
+
+Map<String, dynamic> _$GetServerSettingsResultToJson(
+        GetServerSettingsResult instance) =>
+    <String, dynamic>{
+      'authentication_methods': instance.authenticationMethods,
+      'zulip_feature_level': instance.zulipFeatureLevel,
+      'zulip_version': instance.zulipVersion,
+      'zulip_merge_base': instance.zulipMergeBase,
+      'push_notifications_enabled': instance.pushNotificationsEnabled,
+      'is_incompatible': instance.isIncompatible,
+      'email_auth_enabled': instance.emailAuthEnabled,
+      'require_email_format_usernames': instance.requireEmailFormatUsernames,
+      'realm_uri': instance.realmUri,
+      'realm_name': instance.realmName,
+      'realm_icon': instance.realmIcon,
+      'realm_description': instance.realmDescription,
+      'realm_web_public_access_enabled': instance.realmWebPublicAccessEnabled,
+    };
