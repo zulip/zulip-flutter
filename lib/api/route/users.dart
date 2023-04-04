88419f6e45413aa0e5e742a92f617b26f35e4611
Greg Price <greg@zulip.com>
2023-04-03 17:14:12 -0700

    api: Add get-own-user route, abridged

---
+++ lib/api/route/users.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+import '../core.dart';
+
+part 'users.g.dart';
+
+/// https://zulip.com/api/get-own-user, abridged
+///
+/// This route's return type is simplified because we use it only
+/// as a workaround on old servers.
+Future<GetOwnUserResult> getOwnUser(ApiConnection connection) async {
+  final data = await connection.get('users/me', {});
+  return GetOwnUserResult.fromJson(jsonDecode(data));
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class GetOwnUserResult {
+  final int userId;
+
+  // There are many more properties in this route's result.
+  // But we use this route only as a workaround on old servers:
+  //  https://github.com/zulip/zulip/issues/24980
+  //  https://chat.zulip.org/#narrow/stream/378-api-design/topic/user.20ID.20in.20fetch-api-key/near/1540592
+  // for which `userId` is the only property we need.
+  // TODO(server-7): Drop getOwnUser entirely, relying on userId from fetchApiKey.
+
+  GetOwnUserResult({
+    required this.userId,
+  });
+
+  factory GetOwnUserResult.fromJson(Map<String, dynamic> json) =>
+    _$GetOwnUserResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetOwnUserResultToJson(this);
+}
---
+++ lib/api/route/users.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'users.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetOwnUserResult _$GetOwnUserResultFromJson(Map<String, dynamic> json) =>
+    GetOwnUserResult(
+      userId: json['user_id'] as int,
+    );
+
+Map<String, dynamic> _$GetOwnUserResultToJson(GetOwnUserResult instance) =>
+    <String, dynamic>{
+      'user_id': instance.userId,
+    };
