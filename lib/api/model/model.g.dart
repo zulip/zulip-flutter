901a99eb88d4c7f2da8c630cd4e26948b2b7a696
Chris Bobbe <cbobbe@zulip.com>
2023-04-21 13:48:08 -0700

    lint: Suppress unnecessary_cast in generated files
    For how we found out how to do this, see:
      https://github.com/google/json_serializable.dart/issues/625

--- lib/api/model/model.g.dart
+++ lib/api/model/model.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: unnecessary_cast
+
 part of 'model.dart';
 
 // **************************************************************************
