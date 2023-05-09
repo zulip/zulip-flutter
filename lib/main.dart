3c0dee95368367034da2304865c8c8841ca8365b
Greg Price <greg@zulip.com>
2023-05-08 20:43:58 -0700

    log: Do some logging only in debug app, not in tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'log.dart';
 import 'widgets/app.dart';
 
 void main() {
+  assert(() {
+    debugLogEnabled = true;
+    return true;
+  }());
   runApp(const ZulipApp());
 }
