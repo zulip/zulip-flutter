1aacda4d5bf532aa916d0c173affb178b09a8673
Greg Price <greg@zulip.com>
2023-06-06 12:18:14 -0700

    api test [nfc]: Factor out common checks in sendMessage tests
    This will help when we add more complexity to this route binding
    and correspondingly more tests.

--- test/api/route/messages_test.dart
+++ test/api/route/messages_test.dart
@@ -3,50 +3,60 @@ import 'dart:convert';
 import 'package:checks/checks.dart';
 import 'package:http/http.dart' as http;
 import 'package:test/scaffolding.dart';
+import 'package:zulip/api/model/narrow.dart';
 import 'package:zulip/api/route/messages.dart';
+import 'package:zulip/model/narrow.dart';
 
 import '../../stdlib_checks.dart';
 import '../fake_api.dart';
 import 'route_checks.dart';
 
 void main() {
-  test('sendMessage to stream', () {
-    return FakeApiConnection.with_((connection) async {
-      const streamId = 123;
-      const topic = 'world';
-      const content = 'hello';
-      connection.prepare(json: SendMessageResult(id: 42).toJson());
-      final result = await sendMessage(connection,
-        destination: StreamDestination(streamId, topic), content: content);
-      check(result).id.equals(42);
-      check(connection.lastRequest).isNotNull().isA<http.Request>()
-        ..method.equals('POST')
-        ..url.path.equals('/api/v1/messages')
-        ..bodyFields.deepEquals({
-          'type': 'stream',
-          'to': streamId.toString(),
-          'topic': topic,
-          'content': content,
-        });
-    });
-  });
+  group('sendMessage', () {
+    const streamId = 123;
+    const content = 'hello';
+    const topic = 'world';
+    const userIds = [23, 34];
 
-  test('sendMessage to DM conversation', () {
-    return FakeApiConnection.with_((connection) async {
-      const userIds = [23, 34];
-      const content = 'hi there';
+    Future<void> checkSendMessage(
+      FakeApiConnection connection, {
+      required MessageDestination destination,
+      required String content,
+      required Map<String, String> expectedBodyFields,
+    }) async {
       connection.prepare(json: SendMessageResult(id: 42).toJson());
       final result = await sendMessage(connection,
-        destination: DmDestination(userIds: userIds), content: content);
+        destination: destination, content: content);
       check(result).id.equals(42);
       check(connection.lastRequest).isNotNull().isA<http.Request>()
         ..method.equals('POST')
         ..url.path.equals('/api/v1/messages')
-        ..bodyFields.deepEquals({
-          'type': 'private',
-          'to': jsonEncode(userIds),
-          'content': content,
-        });
+        ..bodyFields.deepEquals(expectedBodyFields);
+    }
+
+    test('to stream', () {
+      return FakeApiConnection.with_((connection) async {
+        await checkSendMessage(connection,
+          destination: StreamDestination(streamId, topic), content: content,
+          expectedBodyFields: {
+            'type': 'stream',
+            'to': streamId.toString(),
+            'topic': topic,
+            'content': content,
+          });
+      });
+    });
+
+    test('to DM conversation', () {
+      return FakeApiConnection.with_((connection) async {
+        await checkSendMessage(connection,
+          destination: DmDestination(userIds: userIds), content: content,
+          expectedBodyFields: {
+            'type': 'private',
+            'to': jsonEncode(userIds),
+            'content': content,
+          });
+      });
     });
   });
 }
