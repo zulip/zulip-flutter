3162387b31866dc0995986d3ac0942f8a92d083c
Greg Price <greg@zulip.com>
2023-06-11 16:20:38 -0700

    test [nfc]: Replace a stray `expect` with `check`

--- test/widgets/content_test.dart
+++ test/widgets/content_test.dart
@@ -1,3 +1,4 @@
+import 'package:checks/checks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zulip/widgets/content.dart';
@@ -6,7 +7,7 @@ void main() {
   testWidgets('Throws if no `PerAccountStoreWidget` ancestor', (WidgetTester tester) async {
     await tester.pumpWidget(
       const RealmContentNetworkImage('https://zulip.invalid/path/to/image.png', filterQuality: FilterQuality.medium));
-    expect(tester.takeException(), isAssertionError);
+    check(tester.takeException()).isA<AssertionError>();
   });
 
   // TODO(#30): Simulate a `PerAccountStoreWidget` ancestor, to use in more tests:
