cc969072200bf78cc78763856b007304057a1a20
Greg Price <greg@zulip.com>
2023-06-26 13:16:08 -0700

    content test: Test parsing inline content

--- test/model/content_test.dart
+++ test/model/content_test.dart
@@ -36,11 +36,109 @@ void main() {
   //  * To get the corresponding Markdown source, use the same command
   //    with `apply_markdown` changed to `false`.
 
+  //
+  // Inline content.
+  //
+
+  void testParseInline(String name, String html, InlineContentNode node) {
+    testParse(name, html, [ParagraphNode(nodes: [node])]);
+  }
+
   testParse('parse a plain-text paragraph',
     // "hello world"
-    '<p>hello world</p>', const [
-      ParagraphNode(nodes: [TextNode('hello world')]),
-    ]);
+    '<p>hello world</p>', const [ParagraphNode(nodes: [
+      TextNode('hello world'),
+    ])]);
+
+  testParse('parse <br> inside a paragraph',
+    // "a\nb"
+    '<p>a<br>\nb</p>', const [ParagraphNode(nodes: [
+      TextNode('a'),
+      LineBreakInlineNode(),
+      TextNode('\nb'),
+    ])]);
+
+  testParseInline('parse strong/bold',
+    // "**bold**"
+    '<p><strong>bold</strong></p>',
+    const StrongNode(nodes: [TextNode('bold')]));
+
+  testParseInline('parse emphasis/italic',
+    // "*italic*"
+    '<p><em>italic</em></p>',
+    const EmphasisNode(nodes: [TextNode('italic')]));
+
+  testParseInline('parse inline code',
+    // "`inline code`"
+    '<p><code>inline code</code></p>',
+    const InlineCodeNode(nodes: [TextNode('inline code')]));
+
+  testParseInline('parse nested strong, em, code',
+    // "***`word`***"
+    '<p><strong><em><code>word</code></em></strong></p>',
+    const StrongNode(nodes: [EmphasisNode(nodes: [InlineCodeNode(nodes: [
+      TextNode('word')])])]));
+
+  testParseInline('parse link',
+    // "[text](https://example/)"
+    '<p><a href="https://example/">text</a></p>',
+    const LinkNode(nodes: [TextNode('text')]));
+
+  testParseInline('parse #-mention of stream',
+    // "#**general**"
+    '<p><a class="stream" data-stream-id="2" href="/#narrow/stream/2-general">'
+        '#general</a></p>',
+    const LinkNode(nodes: [TextNode('#general')]));
+
+  testParseInline('parse #-mention of topic',
+    // "#**mobile-team>zulip-flutter**"
+    '<p><a class="stream-topic" data-stream-id="243" '
+        'href="/#narrow/stream/243-mobile-team/topic/zulip-flutter">'
+        '#mobile-team &gt; zulip-flutter</a></p>',
+    const LinkNode(nodes: [TextNode('#mobile-team > zulip-flutter')]));
+
+  testParseInline('parse nested link, strong, em, code',
+    // "[***`word`***](https://example/)"
+    '<p><a href="https://example/"><strong><em><code>word'
+        '</code></em></strong></a></p>',
+    const LinkNode(nodes: [StrongNode(nodes: [
+      EmphasisNode(nodes: [InlineCodeNode(nodes: [
+        TextNode('word')])])])]));
+
+  group('parse @-mentions', () {
+    testParseInline('plain user @-mention',
+      // "@**Greg Price**"
+      '<p><span class="user-mention" data-user-id="2187">@Greg Price</span></p>',
+      const UserMentionNode(nodes: [TextNode('@Greg Price')]));
+
+    testParseInline('silent user @-mention',
+      // "@_**Greg Price**"
+      '<p><span class="user-mention silent" data-user-id="2187">Greg Price</span></p>',
+      const UserMentionNode(nodes: [TextNode('Greg Price')]));
+
+    // TODO test group mentions and wildcard mentions
+  });
+
+  testParseInline('parse Unicode emoji',
+    // ":thumbs_up:"
+    '<p><span aria-label="thumbs up" class="emoji emoji-1f44d" role="img" title="thumbs up">:thumbs_up:</span></p>',
+    const UnicodeEmojiNode(text: ':thumbs_up:'));
+
+  testParseInline('parse custom emoji',
+    // ":flutter:"
+    '<p><img alt=":flutter:" class="emoji" src="/user_avatars/2/emoji/images/204.png" title="flutter"></p>',
+    const ImageEmojiNode(
+      src: '/user_avatars/2/emoji/images/204.png', alt: ':flutter:'));
+
+  testParseInline('parse Zulip extra emoji',
+    // ":zulip:"
+    '<p><img alt=":zulip:" class="emoji" src="/static/generated/emoji/images/emoji/unicode/zulip.png" title="zulip"></p>',
+    const ImageEmojiNode(
+      src: '/static/generated/emoji/images/emoji/unicode/zulip.png', alt: ':zulip:'));
+
+  //
+  // Block content.
+  //
 
   testParse('parse two plain-text paragraphs',
     // "hello\n\nworld"
