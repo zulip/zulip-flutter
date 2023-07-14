4070ceaeece22596000f04b4e9dc6d9ae6863364
Greg Price <greg@zulip.com>
2023-07-14 13:13:56 -0700

    icons: Add a custom icon font
    Fixes: #200

---
+++ lib/widgets/icons.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/widgets.dart';
+
+// ignore_for_file: constant_identifier_names
+
+/// Identifiers for Zulip's custom icons.
+///
+/// Use these with the [Icon] widget, in the same way as one uses
+/// the members of the [Icons] class from Flutter's Material library.
+abstract final class ZulipIcons {
+  // Generated code; do not edit.
+  //
+  // To add a new icon, or otherwise edit the set of icons:
+  //
+  //  * Add an SVG file in `assets/icons/`,
+  //    or otherwise edit the SVG files there.
+  //
+  //  * Then run the command `scripts/icons/build-icon-font`.
+  //    That will update this file and the generated icon font,
+  //    `assets/icons/ZulipIcons.ttf`.
+  //
+  // BEGIN GENERATED ICON DATA
+
+  /// The Zulip custom icon "bot".
+  static const IconData bot = IconData(0xf101, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "globe".
+  static const IconData globe = IconData(0xf102, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "hash_sign".
+  static const IconData hash_sign = IconData(0xf103, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "language".
+  static const IconData language = IconData(0xf104, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "lock".
+  static const IconData lock = IconData(0xf105, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "mute".
+  static const IconData mute = IconData(0xf106, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "read_receipts".
+  static const IconData read_receipts = IconData(0xf107, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "topic".
+  static const IconData topic = IconData(0xf108, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "unmute".
+  static const IconData unmute = IconData(0xf109, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "user".
+  static const IconData user = IconData(0xf10a, fontFamily: "Zulip Icons");
+
+  // END GENERATED ICON DATA
+}
