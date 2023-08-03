1d039bf9cc12767933d9cf6d3475d70a1eb2084d
Chris Bobbe <cbobbe@zulip.com>
2023-08-03 16:34:20 -0700

    test: Fix wrong type in RealmContentNetworkImageChecks
    This was accidentally clobbered in the merge of #247; see
      https://github.com/zulip/zulip-flutter/pull/247#issuecomment-1664710827

--- test/widgets/content_checks.dart
+++ test/widgets/content_checks.dart
@@ -2,6 +2,6 @@ import 'package:checks/checks.dart';
 import 'package:zulip/widgets/content.dart';
 
 extension RealmContentNetworkImageChecks on Subject<RealmContentNetworkImage> {
-  Subject<String> get src => has((i) => i.src, 'src');
+  Subject<Uri> get src => has((i) => i.src, 'src');
   // TODO others
 }
