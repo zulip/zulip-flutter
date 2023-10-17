40f2560fd975a5bccd87daef88a2db3a49c2676d
Greg Price <greg@zulip.com>
2023-10-17 14:21:00 -0700

    api/notif: Parse notification messages the server sends over FCM
    Much of this is based on the Kotlin code that does this same job
    in the zulip-mobile RN app:
      android/app/src/main/java/com/zulipmobile/notifications/FcmMessage.kt
      android/app/src/test/java/com/zulipmobile/notifications/FcmMessageTest.kt
    
    Those Kotlin tests benefit from a feature of their test framework
    where when an expectation like `expect.that(foo).isEqualTo(bar)`
    (analogous to our `check(foo).equals(bar)`) is unsatisfied, it
    records the failure without immediately ending the test function.
    As a result, when something goes wrong we get very fine-grained
    results, much like we would with package:checks if all the test
    functions were translated as `group` and the individual expectations
    as `test`, rather than as `check` -- but without the overhead that
    the latter approach requires in the source code, particularly in
    making up names for the individual test cases.
    
    In this version, we go ahead and translate to `group` and `test` for
    a couple of the test functions where their lack would be most keenly
    felt, resorting to a hack to avoid cluttering the code with lots of
    individual names.  We let the rest be just `test` and `check`.

---
+++ lib/api/notifications.g.dart
@@ -0,0 +1,73 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: constant_identifier_names, unnecessary_cast
+
+part of 'notifications.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MessageFcmMessage _$MessageFcmMessageFromJson(Map<String, dynamic> json) =>
+    MessageFcmMessage(
+      server: json['server'] as String,
+      realmId: const _IntConverter().fromJson(json['realm_id'] as String),
+      realmUri: Uri.parse(json['realm_uri'] as String),
+      userId: const _IntConverter().fromJson(json['user_id'] as String),
+      senderId: const _IntConverter().fromJson(json['sender_id'] as String),
+      senderEmail: json['sender_email'] as String,
+      senderAvatarUrl: Uri.parse(json['sender_avatar_url'] as String),
+      senderFullName: json['sender_full_name'] as String,
+      recipient: FcmMessageRecipient.fromJson(
+          MessageFcmMessage._readWhole(json, 'recipient')
+              as Map<String, dynamic>),
+      zulipMessageId:
+          const _IntConverter().fromJson(json['zulip_message_id'] as String),
+      content: json['content'] as String,
+      time: const _IntConverter().fromJson(json['time'] as String),
+    );
+
+Map<String, dynamic> _$MessageFcmMessageToJson(MessageFcmMessage instance) =>
+    <String, dynamic>{
+      'server': instance.server,
+      'realm_id': const _IntConverter().toJson(instance.realmId),
+      'realm_uri': instance.realmUri.toString(),
+      'user_id': const _IntConverter().toJson(instance.userId),
+      'event': instance.type,
+      'sender_id': const _IntConverter().toJson(instance.senderId),
+      'sender_email': instance.senderEmail,
+      'sender_avatar_url': instance.senderAvatarUrl.toString(),
+      'sender_full_name': instance.senderFullName,
+      'zulip_message_id': const _IntConverter().toJson(instance.zulipMessageId),
+      'time': const _IntConverter().toJson(instance.time),
+      'content': instance.content,
+    };
+
+FcmMessageStreamRecipient _$FcmMessageStreamRecipientFromJson(
+        Map<String, dynamic> json) =>
+    FcmMessageStreamRecipient(
+      streamId: const _IntConverter().fromJson(json['stream_id'] as String),
+      streamName: json['stream'] as String?,
+      topic: json['topic'] as String,
+    );
+
+RemoveFcmMessage _$RemoveFcmMessageFromJson(Map<String, dynamic> json) =>
+    RemoveFcmMessage(
+      server: json['server'] as String,
+      realmId: const _IntConverter().fromJson(json['realm_id'] as String),
+      realmUri: Uri.parse(json['realm_uri'] as String),
+      userId: const _IntConverter().fromJson(json['user_id'] as String),
+      zulipMessageIds: const _IntListConverter()
+          .fromJson(json['zulip_message_ids'] as String),
+    );
+
+Map<String, dynamic> _$RemoveFcmMessageToJson(RemoveFcmMessage instance) =>
+    <String, dynamic>{
+      'server': instance.server,
+      'realm_id': const _IntConverter().toJson(instance.realmId),
+      'realm_uri': instance.realmUri.toString(),
+      'user_id': const _IntConverter().toJson(instance.userId),
+      'event': instance.type,
+      'zulip_message_ids':
+          const _IntListConverter().toJson(instance.zulipMessageIds),
+    };
