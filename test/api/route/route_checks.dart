d09f0c7f7437893d0f82beec04cf54c76d44a34c
Greg Price <greg@zulip.com>
2023-11-03 17:37:24 -0700

    api: Cut SendMessageResult.deliverAt, quietly deleted from API
    This appears to have been done in commit zulip/zulip@bd2545b0d.
    It's fine for it to have gone away -- the new API is better,
    and we weren't using this field -- but there's no notice of the
    change in the API changelog or the endpoint's own API docs.
    That shouldn't happen.  I'll follow up.
    
    Anyway, now that I've sleuthed out the mystery of what happened,
    remove this from our bindings to match.

--- test/api/route/route_checks.dart
+++ test/api/route/route_checks.dart
@@ -3,7 +3,6 @@ import 'package:zulip/api/route/messages.dart';
 
 extension SendMessageResultChecks on Subject<SendMessageResult> {
   Subject<int> get id => has((e) => e.id, 'id');
-  Subject<String?> get deliverAt => has((e) => e.deliverAt, 'deliverAt');
 }
 
 // TODO add similar extensions for other classes in api/route/*.dart
