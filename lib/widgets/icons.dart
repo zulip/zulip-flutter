e2b3b97ae88905767b604c2ce1bdc90eafd57703
Chris Bobbe <cbobbe@zulip.com>
2023-11-12 21:40:57 -0500

    icons: Add arrow_down and arrow_right custom icons
    Taken today from the "Icons" page in Figma:
      https://www.figma.com/file/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?node-id=1651%3A24320&mode=dev
      https://www.figma.com/file/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?node-id=1651%3A24321&mode=dev

--- lib/widgets/icons.dart
+++ lib/widgets/icons.dart
@@ -22,38 +22,44 @@ abstract final class ZulipIcons {
   //
   // BEGIN GENERATED ICON DATA
 
+  /// The Zulip custom icon "arrow_down".
+  static const IconData arrow_down = IconData(0xf101, fontFamily: "Zulip Icons");
+
+  /// The Zulip custom icon "arrow_right".
+  static const IconData arrow_right = IconData(0xf102, fontFamily: "Zulip Icons");
+
   /// The Zulip custom icon "bot".
-  static const IconData bot = IconData(0xf101, fontFamily: "Zulip Icons");
+  static const IconData bot = IconData(0xf103, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "globe".
-  static const IconData globe = IconData(0xf102, fontFamily: "Zulip Icons");
+  static const IconData globe = IconData(0xf104, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "group_dm".
-  static const IconData group_dm = IconData(0xf103, fontFamily: "Zulip Icons");
+  static const IconData group_dm = IconData(0xf105, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "hash_sign".
-  static const IconData hash_sign = IconData(0xf104, fontFamily: "Zulip Icons");
+  static const IconData hash_sign = IconData(0xf106, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "language".
-  static const IconData language = IconData(0xf105, fontFamily: "Zulip Icons");
+  static const IconData language = IconData(0xf107, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "lock".
-  static const IconData lock = IconData(0xf106, fontFamily: "Zulip Icons");
+  static const IconData lock = IconData(0xf108, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "mute".
-  static const IconData mute = IconData(0xf107, fontFamily: "Zulip Icons");
+  static const IconData mute = IconData(0xf109, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "read_receipts".
-  static const IconData read_receipts = IconData(0xf108, fontFamily: "Zulip Icons");
+  static const IconData read_receipts = IconData(0xf10a, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "topic".
-  static const IconData topic = IconData(0xf109, fontFamily: "Zulip Icons");
+  static const IconData topic = IconData(0xf10b, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "unmute".
-  static const IconData unmute = IconData(0xf10a, fontFamily: "Zulip Icons");
+  static const IconData unmute = IconData(0xf10c, fontFamily: "Zulip Icons");
 
   /// The Zulip custom icon "user".
-  static const IconData user = IconData(0xf10b, fontFamily: "Zulip Icons");
+  static const IconData user = IconData(0xf10d, fontFamily: "Zulip Icons");
 
   // END GENERATED ICON DATA
 }
