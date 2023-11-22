0618adc743a00b1c5734871e6009dda63ee1a866
Chris Bobbe <cbobbe@zulip.com>
2023-11-22 17:13:20 -0500

    subscription_list [nfc]: Remove unneeded [Center]
    I think this isn't necessary; the child will expand out as far as
    the parent constraints allow, so a centered alignment isn't
    different from any other kind of alignment.

--- lib/widgets/subscription_list.dart
+++ lib/widgets/subscription_list.dart
@@ -82,25 +82,24 @@ class _SubscriptionListPageState extends State<SubscriptionListPage> with PerAcc
 
     return Scaffold(
       appBar: AppBar(title: const Text("Streams")),
-      body: Center(
-        child: CustomScrollView(
-          slivers: [
-            if (pinned.isEmpty && unpinned.isEmpty)
-              const _NoSubscriptionsItem(),
-            if (pinned.isNotEmpty) ...[
-              const _SubscriptionListHeader(label: "Pinned"),
-              _SubscriptionList(unreadsModel: unreadsModel, subscriptions: pinned),
-            ],
-            if (unpinned.isNotEmpty) ...[
-              const _SubscriptionListHeader(label: "Unpinned"),
-              _SubscriptionList(unreadsModel: unreadsModel, subscriptions: unpinned),
-            ],
+      body: CustomScrollView(
+        slivers: [
+          if (pinned.isEmpty && unpinned.isEmpty)
+            const _NoSubscriptionsItem(),
+          if (pinned.isNotEmpty) ...[
+            const _SubscriptionListHeader(label: "Pinned"),
+            _SubscriptionList(unreadsModel: unreadsModel, subscriptions: pinned),
+          ],
+          if (unpinned.isNotEmpty) ...[
+            const _SubscriptionListHeader(label: "Unpinned"),
+            _SubscriptionList(unreadsModel: unreadsModel, subscriptions: unpinned),
+          ],
 
-            // TODO(#188): add button leading to "All Streams" page with ability to subscribe
+          // TODO(#188): add button leading to "All Streams" page with ability to subscribe
 
-            // This ensures last item in scrollable can settle in an unobstructed area.
-            const SliverSafeArea(sliver: SliverToBoxAdapter(child: SizedBox.shrink())),
-          ])));
+          // This ensures last item in scrollable can settle in an unobstructed area.
+          const SliverSafeArea(sliver: SliverToBoxAdapter(child: SizedBox.shrink())),
+        ]));
   }
 }
 
