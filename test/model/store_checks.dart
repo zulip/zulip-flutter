64447564ee3a4e5416d9581c289517743d5cbe88
Greg Price <greg@zulip.com>
2024-02-14 18:53:52 -0800

    store test: Add package:checks getters for Account
    I didn't end up needing these for the tests I was writing today,
    but since I've written them we might as well save them for later.
    (This Account type is quite stable, so it seems likely we'll have
    a use for these before it changes so much it would have been easier
    to write them from scratch.)

--- test/model/store_checks.dart
+++ test/model/store_checks.dart
@@ -1,6 +1,18 @@
 import 'package:checks/checks.dart';
 import 'package:zulip/model/store.dart';
 
+extension AccountChecks on Subject<Account> {
+  Subject<int> get id => has((x) => x.id, 'id');
+  Subject<Uri> get realmUrl => has((x) => x.realmUrl, 'realmUrl');
+  Subject<int> get userId => has((x) => x.userId, 'userId');
+  Subject<String> get email => has((x) => x.email, 'email');
+  Subject<String> get apiKey => has((x) => x.apiKey, 'apiKey');
+  Subject<String> get zulipVersion => has((x) => x.zulipVersion, 'zulipVersion');
+  Subject<String?> get zulipMergeBase => has((x) => x.zulipMergeBase, 'zulipMergeBase');
+  Subject<int> get zulipFeatureLevel => has((x) => x.zulipFeatureLevel, 'zulipFeatureLevel');
+  Subject<String?> get ackedPushToken => has((x) => x.ackedPushToken, 'ackedPushToken');
+}
+
 extension GlobalStoreChecks on Subject<GlobalStore> {
   Subject<Iterable<Account>> get accounts => has((x) => x.accounts, 'accounts');
   Subject<Iterable<int>> get accountIds => has((x) => x.accountIds, 'accountIds');
