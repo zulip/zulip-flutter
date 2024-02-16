5435296843ea5191687aea8530b5814449fd60ec
Greg Price <greg@zulip.com>
2024-02-16 12:28:01 -0800

    nav [nfc]: Write full docs for MaterialAccountWidgetRoute and friends
    I was rereading some of the code that uses these classes, and found
    I had to reread their implementation to refresh myself on what they
    each did.

--- lib/widgets/page.dart
+++ lib/widgets/page.dart
@@ -14,6 +14,10 @@ abstract class WidgetRoute<T> extends PageRoute<T> {
 /// A [MaterialPageRoute] that always builds the same widget.
 ///
 /// This is useful for making the route more transparent for a test to inspect.
+///
+/// See also:
+///  * [MaterialAccountWidgetRoute], a subclass which automates reusing a
+///    per-account store on the new route.
 class MaterialWidgetRoute<T> extends MaterialPageRoute<T> implements WidgetRoute<T> {
   MaterialWidgetRoute({
     required this.page,
@@ -27,6 +31,7 @@ class MaterialWidgetRoute<T> extends MaterialPageRoute<T> implements WidgetRoute
   final Widget page;
 }
 
+/// A mixin for providing a given account's per-account store on a page route.
 mixin AccountPageRouteMixin<T> on PageRoute<T> {
   int get accountId;
 
@@ -38,6 +43,16 @@ mixin AccountPageRouteMixin<T> on PageRoute<T> {
   }
 }
 
+/// A [MaterialPageRoute] that reuses the given context's per-account store.
+///
+/// This reuse is the desired behavior for any navigation that's meant to stay
+/// within a given account.
+///
+/// See also:
+///  * [MaterialAccountWidgetRoute], a subclass which is more transparent
+///    for tests.
+///  * [AccountPageRouteBuilder], for defining one-off page routes
+///    in terms of callbacks.
 class MaterialAccountPageRoute<T> extends MaterialPageRoute<T> with AccountPageRouteMixin<T> {
   MaterialAccountPageRoute({
     required BuildContext context,
@@ -52,9 +67,20 @@ class MaterialAccountPageRoute<T> extends MaterialPageRoute<T> with AccountPageR
   final int accountId;
 }
 
-/// A [MaterialAccountPageRoute] that always builds the same widget.
+/// A [MaterialPageRoute] that reuses the given context's per-account store
+/// and always builds the same widget.
 ///
-/// This is useful for making the route more transparent for a test to inspect.
+/// This is the [PageRoute] subclass to use for most navigation in the app.
+///
+/// The reuse of the per-account store is the desired behavior for any
+/// navigation that's meant to stay within a given account.
+///
+/// Always building the same widget is useful for making the route
+/// more transparent for a test to inspect.
+///
+/// See also:
+///  * [MaterialWidgetRoute], for routes that need no per-account store
+///    or a different per-account store.
 class MaterialAccountWidgetRoute<T> extends MaterialAccountPageRoute<T> implements WidgetRoute<T> {
   MaterialAccountWidgetRoute({
     required super.context,
@@ -69,6 +95,9 @@ class MaterialAccountWidgetRoute<T> extends MaterialAccountPageRoute<T> implemen
   final Widget page;
 }
 
+/// A [PageRouteBuilder] that reuses the given context's per-account store.
+///
+/// This is the [PageRouteBuilder] analogue of [MaterialAccountPageRoute].
 class AccountPageRouteBuilder<T> extends PageRouteBuilder<T> with AccountPageRouteMixin<T> {
   AccountPageRouteBuilder({
     required BuildContext context,
