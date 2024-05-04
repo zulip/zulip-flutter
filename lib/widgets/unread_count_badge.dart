c64d7a00ee62a4cab20ea904d553ccaf4d227753
Chris Bobbe <cbobbe@zulip.com>
2024-05-03 17:42:20 -0700

    unread_count_badge: Comment on some overlooked uncertainty in text color

--- lib/widgets/unread_count_badge.dart
+++ lib/widgets/unread_count_badge.dart
@@ -47,6 +47,16 @@ class UnreadCountBadge extends StatelessWidget {
             fontSize: 16,
             height: (18 / 16),
             fontFeatures: [FontFeature.enable('smcp')], // small caps
+
+            // From the Figma:
+            //   https://www.figma.com/file/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?type=design&node-id=171-12359&mode=design&t=JKrw76SGUF51nSJG-0
+            // TODO or, when background is stream-colored, follow Vlad's replit?
+            //     https://replit.com/@VladKorobov/zulip-sidebar#script.js
+            //   which would mean:
+            //   - in light mode use `Color.fromRGBO(0, 0, 0, 0.9)`
+            //   - in dark mode use `Color.fromRGBO(255, 255, 255, 0.9)`
+            //   The web app doesn't (yet?) use stream-colored unread markers
+            //   so we can't take direction from there.
             color: Color(0xFF222222),
           ).merge(weightVariableTextStyle(context,
               wght: bold ? 600 : null)),
