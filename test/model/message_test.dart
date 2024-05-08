197f9f4b9a34d1cc272df255b4d4049bebde92c9
Greg Price <greg@zulip.com>
2024-05-07 21:13:37 -0700

    message test [nfc]: Add a MessageListView, optionally
    Some of the existing tests of MessageListView in message_list_test.dart
    are really almost entirely about acting on individual messages rather
    than message lists.  They therefore belong as tests of the overall
    message store, to live in this file.
    
    With that "almost", though, they have a few ties to actually testing
    the MessageListView itself -- notably, its notification behavior.
    So to simplify converting them and moving them into this file,
    we add a MessageListView they can use.  This logic is largely
    copied from message_list_test.dart.

--- test/model/message_test.dart
+++ test/model/message_test.dart
@@ -2,29 +2,74 @@ import 'package:checks/checks.dart';
 import 'package:test/scaffolding.dart';
 import 'package:zulip/api/model/events.dart';
 import 'package:zulip/api/model/model.dart';
+import 'package:zulip/model/message_list.dart';
+import 'package:zulip/model/narrow.dart';
 import 'package:zulip/model/store.dart';
 
+import '../api/fake_api.dart';
 import '../example_data.dart' as eg;
+import 'message_list_test.dart';
+import 'test_store.dart';
 
 void main() {
-  // These variables are the common state operated on by each test.
+  // These "late" variables are the common state operated on by each test.
   // Each test case calls [prepare] to initialize them.
+  late Subscription subscription;
   late PerAccountStore store;
+  late FakeApiConnection connection;
+  // [messageList] is here only for the sake of checking when it notifies.
+  // For anything deeper than that, use `message_list_test.dart`.
+  late MessageListView messageList;
+  late int notifiedCount;
+
+  void checkNotified({required int count}) {
+    check(notifiedCount).equals(count);
+    notifiedCount = 0;
+  }
+  void checkNotNotified() => checkNotified(count: 0);
+  void checkNotifiedOnce() => checkNotified(count: 1);
 
   /// Initialize [store] and the rest of the test state.
-  void prepare() {
+  Future<void> prepare({Narrow narrow = const CombinedFeedNarrow()}) async {
+    final stream = eg.stream();
+    subscription = eg.subscription(stream);
     store = eg.store();
+    await store.addStream(stream);
+    await store.addSubscription(subscription);
+    connection = store.connection as FakeApiConnection;
+    notifiedCount = 0;
+    messageList = MessageListView.init(store: store, narrow: narrow)
+      ..addListener(() {
+        notifiedCount++;
+      });
+    check(messageList).fetched.isFalse();
+    checkNotNotified();
+  }
+
+  /// Perform the initial message fetch for [messageList].
+  ///
+  /// The test case must have already called [prepare] to initialize the state.
+  // ignore: unused_element
+  Future<void> prepareMessages(
+    List<Message> messages, {
+    bool foundOldest = false,
+  }) async {
+    connection.prepare(json:
+      newestResult(foundOldest: foundOldest, messages: messages).toJson());
+    await messageList.fetchInitial();
+    checkNotifiedOnce();
   }
 
   Future<void> addMessages(Iterable<Message> messages) async {
     for (final m in messages) {
       await store.handleEvent(MessageEvent(id: 0, message: m));
     }
+    checkNotified(count: messageList.fetched ? messages.length : 0);
   }
 
   group('reconcileMessages', () {
     test('from empty', () async {
-      prepare();
+      await prepare();
       check(store.messages).isEmpty();
       final message1 = eg.streamMessage();
       final message2 = eg.streamMessage();
@@ -40,7 +85,7 @@ void main() {
     });
 
     test('from not-empty', () async {
-      prepare();
+      await prepare();
       final message1 = eg.streamMessage();
       final message2 = eg.streamMessage();
       final message3 = eg.dmMessage(from: eg.otherUser, to: [eg.selfUser]);
@@ -58,7 +103,7 @@ void main() {
     });
 
     test('on ID collision, new message does not clobber old in store.messages', () async {
-      prepare();
+      await prepare();
       final message = eg.streamMessage(id: 1, content: '<p>foo</p>');
       await addMessages([message]);
       check(store.messages).deepEquals({1: message});
@@ -72,7 +117,7 @@ void main() {
 
   group('handleMessageEvent', () {
     test('from empty', () async {
-      prepare();
+      await prepare();
       check(store.messages).isEmpty();
 
       final newMessage = eg.streamMessage();
@@ -83,7 +128,7 @@ void main() {
     });
 
     test('from not-empty', () async {
-      prepare();
+      await prepare();
       final messages = [
         eg.streamMessage(),
         eg.streamMessage(),
@@ -103,7 +148,7 @@ void main() {
     });
 
     test('new message clobbers old on ID collision', () async {
-      prepare();
+      await prepare();
       final message = eg.streamMessage(id: 1, content: '<p>foo</p>');
       await addMessages([message]);
       check(store.messages).deepEquals({1: message});
