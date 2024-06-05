3c7e89d33efcb5a31660048c129714dd2bf3bae8
Greg Price <greg@zulip.com>
2024-06-04 17:16:45 -0700

    test [nfc]: Fix implicit dynamic in condition-callback types
    These `it` parameters were implicitly getting typed `Subject<dynamic>`.
    We want `Subject<Object?>`, as that's what `deepEquals` expects.
    (The `dynamic` version was working fine, but would be flagged by the
    analyzer under the strict-raw-types option.)
    
    We could explicitly write that in each case... or we could tighten
    a bit by using the `Condition` alias and applying it to the whole
    lists that these appear in.

--- test/notifications/display_test.dart
+++ test/notifications/display_test.dart
@@ -208,13 +208,13 @@ void main() {
     late List<Route<dynamic>> pushedRoutes;
 
     void takeStartingRoutes({bool withAccount = true}) {
-      final expected = [
-        (Subject it) => it.isA<WidgetRoute>().page.isA<ChooseAccountPage>(),
+      final expected = <Condition<Object?>>[
+        (it) => it.isA<WidgetRoute>().page.isA<ChooseAccountPage>(),
         if (withAccount) ...[
-          (Subject it) => it.isA<MaterialAccountWidgetRoute>()
+          (it) => it.isA<MaterialAccountWidgetRoute>()
             ..accountId.equals(eg.selfAccount.id)
             ..page.isA<HomePage>(),
-          (Subject it) => it.isA<MaterialAccountWidgetRoute>()
+          (it) => it.isA<MaterialAccountWidgetRoute>()
             ..accountId.equals(eg.selfAccount.id)
             ..page.isA<InboxPage>(),
         ],
