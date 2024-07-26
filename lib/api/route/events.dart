2e2872b9a033d8c6dea90f198cd02bd9457b9907
Zixuan James Li <zixuan@zulip.com>
2024-07-25 21:31:29 -0400

    register: Ask for stream_typing_notifications.
    We added support for stream typing notifications, and this change
    ensures that we get TypingEvents for them.
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/api/route/events.dart
+++ lib/api/route/events.dart
@@ -16,7 +16,7 @@ Future<InitialSnapshot> registerQueue(ApiConnection connection) {
       'notification_settings_null': true,
       'bulk_message_deletion': true,
       'user_avatar_url_field_optional': false, // TODO(#254): turn on
-      'stream_typing_notifications': false, // TODO implement
+      'stream_typing_notifications': true,
       'user_settings_object': true,
     },
   });
