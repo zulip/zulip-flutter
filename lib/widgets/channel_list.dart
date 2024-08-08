793e9bf474311066f7ea8046b975779f496dd5cc
Khader M Khudair <kkhudair16@gmail.com>
2024-08-08 12:20:30 +0300

    channel_list: Implement subscribe to channel
    Fixes: #188

--- lib/widgets/channel_list.dart
+++ lib/widgets/channel_list.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/zulip_localizations.dart';
 
 import '../api/model/model.dart';
+import '../api/route/channels.dart';
 import '../model/narrow.dart';
 import 'app_bar.dart';
+import '../model/store.dart';
 import 'icons.dart';
 import 'message_list.dart';
 import 'page.dart';
 import 'store.dart';
+import 'text.dart';
 import 'theme.dart';
 
 class ChannelListPage extends StatelessWidget {
@@ -100,6 +103,82 @@ class ChannelItem extends StatelessWidget {
                   maxLines: 1,
                   overflow: TextOverflow.ellipsis),
               ])),
+            const SizedBox(width: 8),
+            if (stream is! Subscription) _ChannelItemSubscribeButton(stream: stream, channelItemContext: context),
           ]))));
+  }
+}
+
+class _ChannelItemSubscribeButton extends StatefulWidget {
+  const _ChannelItemSubscribeButton({required this.stream, required this.channelItemContext});
+
+  final ZulipStream stream;
+  final BuildContext channelItemContext;
+
+  @override
+  State<_ChannelItemSubscribeButton> createState() => _ChannelItemSubscribeButtonState();
+}
+
+class _ChannelItemSubscribeButtonState extends State<_ChannelItemSubscribeButton> {
+  bool _isLoading = false;
+
+  void _setIsLoading(bool value) {
+    if (!mounted) return;
+    setState(() => _isLoading = value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.add),
+      onPressed: _isLoading ? null : () async {
+        _setIsLoading(true);
+        await _subscribeToChannel(context, widget.stream);
+        _setIsLoading(false);
+      });
+  }
+
+  Future<void> _subscribeToChannel(BuildContext context, ZulipStream stream) async {
+    final store = PerAccountStoreWidget.of(context);
+    final connection = store.connection;
+    final zulipLocalizations = ZulipLocalizations.of(context);
+    try {
+      final res = await subscribeToChannels(connection, [stream]);
+      if (!context.mounted) return;
+      if (_subscriptionResultContains(store, res.subscribed, stream.name)) {
+        _showSnackbarWithChannelTitle(context, zulipLocalizations.messageSubscribedToChannel, stream);
+      } else if (_subscriptionResultContains(store, res.alreadySubscribed, stream.name)) {
+        _showSnackbarWithChannelTitle(context, zulipLocalizations.messageAlreadySubscribedToChannel, stream);
+      } else {
+        _showSnackbarWithChannelTitle(context, zulipLocalizations.errorFailedToSubscribeToChannel, stream);
+      }
+    } catch (e) {
+      if (!context.mounted) return;
+      final zulipLocalizations = ZulipLocalizations.of(context);
+      _showSnackbarWithChannelTitle(context, zulipLocalizations.errorFailedToSubscribeToChannel, stream);
+    }
+  }
+
+  bool _subscriptionResultContains(PerAccountStore store, Map<String, List<String>> emailOrIdSubs, String subscription) {
+    // TODO (server-10) Before the user keys were Zulip API email addresses, not user ID.
+    final expectedEmail = store.users[store.selfUserId]?.email;
+    final foundByEmail = emailOrIdSubs[expectedEmail]?.contains(subscription);
+    final foundById = emailOrIdSubs[store.selfUserId.toString()]?.contains(subscription);
+    return foundByEmail ?? foundById ?? false;
+  }
+
+  void _showSnackbarWithChannelTitle(BuildContext context, String message, ZulipStream stream) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text.rich(TextSpan(text: message, children: [
+          WidgetSpan(child: Padding(
+            padding: const EdgeInsets.only(bottom: 2),
+            child: Icon(
+              size: 12,
+              iconDataForStream(stream),
+              color: Theme.of(context).scaffoldBackgroundColor))),
+          TextSpan(text: ' ${stream.name}.', style: weightVariableTextStyle(context, wght: 700)),
+    ]))));
   }
 }
--- test/widgets/channel_list_test.dart
+++ test/widgets/channel_list_test.dart
@@ -1,14 +1,27 @@
-import 'package:checks/checks.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:zulip/api/model/model.dart';
-import 'package:zulip/widgets/channel_list.dart';
+import 'dart:convert';
 
+import 'package:checks/checks.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:zulip/api/model/events.dart';
+import 'package:zulip/api/model/model.dart';
+import 'package:zulip/api/route/channels.dart';
+import 'package:zulip/model/localizations.dart';
+import 'package:zulip/model/store.dart';
+import 'package:zulip/widgets/channel_list.dart';
+import 'package:zulip/widgets/icons.dart';
+
+import '../api/fake_api.dart';
 import '../model/binding.dart';
 import '../example_data.dart' as eg;
+import '../stdlib_checks.dart';
 import 'test_app.dart';
 
 void main() {
   TestZulipBinding.ensureInitialized();
+  late FakeApiConnection connection;
+  late PerAccountStore store;
 
   Future<void> setupChannelListPage(WidgetTester tester, {
     required List<ZulipStream> streams,
@@ -18,8 +31,10 @@ void main() {
     final initialSnapshot = eg.initialSnapshot(
       subscriptions: subscriptions,
       streams: streams,
-    );
+      realmUsers: [eg.selfUser]);
     await testBinding.globalStore.add(eg.selfAccount, initialSnapshot);
+    store = await testBinding.globalStore.perAccount(eg.selfAccount.id);
+    connection = store.connection as FakeApiConnection;
 
     await tester.pumpWidget(TestZulipApp(accountId: eg.selfAccount.id, child: const ChannelListPage()));
 
@@ -65,6 +80,161 @@ void main() {
         subscriptions: [eg.subscription(streams[0])]);
 
       check(listedStreamNames(tester)).deepEquals(['a', 'b', 'c']);
+    });
+  });
+
+  group('subscription toggle', () {
+    final zulipLocalizations = GlobalLocalizations.zulipLocalizations;
+
+    Future<ZulipStream> prepareSingleStream(WidgetTester tester) async {
+        final stream = eg.stream();
+        await setupChannelListPage(tester, streams: [stream], subscriptions: []);
+        return stream;
+    }
+
+    Future<void> tapSubscribeButton(WidgetTester tester) async {
+      await tester.tap(find.byIcon(Icons.add));
+    }
+
+    Future<void> waitAndCheckSnackbarIsShown(WidgetTester tester, String message, String channelName) async {
+      await tester.pump(Duration.zero);
+      await tester.pumpAndSettle();
+      final richTextFinder = find.byWidgetPredicate(
+        (widget) => widget is RichText && widget.text.toPlainText().contains(message));
+      check(richTextFinder.evaluate()).single;
+      final richTextWidget = tester.widget<RichText>(richTextFinder);
+      check(richTextWidget.text.toPlainText()).contains(message);
+      check(richTextWidget.text.toPlainText()).contains(channelName);
+      check(find.descendant(
+        of: richTextFinder,
+        matching: find.byIcon(ZulipIcons.hash_sign),
+      ).evaluate()).single;
+    }
+
+    testWidgets('is affected by subscription events', (WidgetTester tester) async {
+      final stream = await prepareSingleStream(tester);
+      connection.prepare(json: SubscribeToChannelsResult(
+        subscribed: {eg.selfUser.email: [stream.name]},
+        alreadySubscribed: {}).toJson());
+
+      check(find.byIcon(Icons.add).evaluate()).isNotEmpty();
+
+      await store.handleEvent(SubscriptionAddEvent(id: 1,
+        subscriptions: [eg.subscription(stream)]));
+      await tester.pumpAndSettle();
+
+      check(find.byIcon(Icons.add).evaluate()).isEmpty();
+
+      await store.handleEvent(SubscriptionRemoveEvent(id: 2, streamIds: [stream.streamId]));
+      await tester.pumpAndSettle();
+
+      check(find.byIcon(Icons.add).evaluate()).isNotEmpty();
+    });
+
+    testWidgets('is disabled while loading', (WidgetTester tester) async {
+      final stream = eg.stream();
+      await setupChannelListPage(tester, streams: [stream], subscriptions: []);
+      connection.prepare(json: SubscribeToChannelsResult(
+        subscribed: {eg.selfUser.email: [stream.name]},
+        alreadySubscribed: {}).toJson());
+      await tapSubscribeButton(tester);
+      await tester.pump();
+
+      check(tester.widget<IconButton>(
+        find.byType(IconButton)).onPressed).isNull();
+
+      await tester.pump(const Duration(seconds: 2));
+
+      check(tester.widget<IconButton>(
+        find.byType(IconButton)).onPressed).isNotNull();
+    });
+
+    testWidgets('is disabled while loading and enabled back when loading fails', (WidgetTester tester) async {
+      final stream = eg.stream();
+      await setupChannelListPage(tester, streams: [stream], subscriptions: []);
+      connection.prepare(exception: http.ClientException('Oops'), delay: const Duration(seconds: 2));
+      await tapSubscribeButton(tester);
+      await tester.pump();
+
+      check(tester.widget<IconButton>(
+        find.byType(IconButton)).onPressed).isNull();
+
+      await tester.pump(const Duration(seconds: 2));
+
+      check(tester.widget<IconButton>(
+        find.byType(IconButton)).onPressed).isNotNull();
+    });
+
+    group('subscribe', () {
+      testWidgets('is shown only for streams that user is not subscribed to', (tester) async {
+        final streams = [eg.stream(), eg.stream(), eg.subscription(eg.stream())];
+        final subscriptions = [streams[2] as Subscription];
+        await setupChannelListPage(tester, streams: streams, subscriptions: subscriptions);
+
+        check(find.byIcon(Icons.add).evaluate().length).equals(2);
+      });
+
+      testWidgets('smoke api', (tester) async {
+        final stream = await prepareSingleStream(tester);
+        connection.prepare(json: SubscribeToChannelsResult(
+          subscribed: {eg.selfUser.email: [stream.name]},
+          alreadySubscribed: {}).toJson());
+        await tapSubscribeButton(tester);
+
+        await tester.pump(Duration.zero);
+        await tester.pumpAndSettle();
+        check(connection.lastRequest).isA<http.Request>()
+          ..method.equals('POST')
+          ..url.path.equals('/api/v1/users/me/subscriptions')
+          ..bodyFields.deepEquals({
+              'subscriptions':  jsonEncode([{'name': stream.name}])
+            });
+      });
+
+      testWidgets('shows a snackbar when subscription passes', (WidgetTester tester) async {
+        final stream = await prepareSingleStream(tester);
+        connection.prepare(json: SubscribeToChannelsResult(
+          subscribed: {eg.selfUser.email: [stream.name]},
+          alreadySubscribed: {}).toJson());
+        await tapSubscribeButton(tester);
+
+        await waitAndCheckSnackbarIsShown(tester,
+          zulipLocalizations.messageSubscribedToChannel, stream.name);
+      });
+
+      testWidgets('shows a snackbar when already subscribed', (WidgetTester tester) async {
+        final stream = await prepareSingleStream(tester);
+        connection.prepare(json: SubscribeToChannelsResult(
+          subscribed: {},
+          alreadySubscribed: {eg.selfUser.email: [stream.name]}).toJson());
+        await tapSubscribeButton(tester);
+
+        await waitAndCheckSnackbarIsShown(tester,
+          zulipLocalizations.messageAlreadySubscribedToChannel, stream.name);
+      });
+
+      testWidgets('shows a snackbar when subscription fails', (WidgetTester tester) async {
+        final stream = await prepareSingleStream(tester);
+        connection.prepare(json: SubscribeToChannelsResult(
+          subscribed: {},
+          alreadySubscribed: {},
+          unauthorized: [stream.name]).toJson());
+        await tapSubscribeButton(tester);
+
+        await waitAndCheckSnackbarIsShown(tester,
+          zulipLocalizations.errorFailedToSubscribeToChannel, stream.name);
+      });
+
+      testWidgets('catch-all api errors', (WidgetTester tester) async {
+        final stream = await prepareSingleStream(tester);
+        connection.prepare(exception: http.ClientException('Oops'));
+        await tapSubscribeButton(tester);
+        await tester.pump(Duration.zero);
+        await tester.pumpAndSettle();
+
+        await waitAndCheckSnackbarIsShown(tester,
+          zulipLocalizations.errorFailedToSubscribeToChannel, stream.name);
+      });
     });
   });
 }
