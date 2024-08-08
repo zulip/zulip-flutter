96836fc84437b5cea676c7e5087e1c1416fe87cd
Khader M Khudair <kkhudair16@gmail.com>
2024-08-08 12:10:22 +0300

    api: Add route subscribeToChannels

--- lib/api/route/channels.dart
+++ lib/api/route/channels.dart
@@ -1,6 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
 import '../core.dart';
+import '../model/model.dart';
 part 'channels.g.dart';
 
 /// https://zulip.com/api/get-stream-topics
@@ -37,4 +38,28 @@ class GetStreamTopicsEntry {
   factory GetStreamTopicsEntry.fromJson(Map<String, dynamic> json) => _$GetStreamTopicsEntryFromJson(json);
 
   Map<String, dynamic> toJson() => _$GetStreamTopicsEntryToJson(this);
+}
+
+/// https://zulip.com/api/subscribe
+Future<SubscribeToChannelsResult> subscribeToChannels(ApiConnection connection, List<ZulipStream> streams) {
+  return connection.post('subscribe', SubscribeToChannelsResult.fromJson,
+    'users/me/subscriptions', {'subscriptions': streams.map((e) => {'name': e.name}).toList()});
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class SubscribeToChannelsResult {
+  final Map<String, List<String>> subscribed;
+  final Map<String, List<String>> alreadySubscribed;
+  final List<String>? unauthorized;
+
+  SubscribeToChannelsResult({
+    required this.subscribed,
+    required this.alreadySubscribed,
+    this.unauthorized,
+  });
+
+  factory SubscribeToChannelsResult.fromJson(Map<String, dynamic> json) =>
+    _$SubscribeToChannelsResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SubscribeToChannelsResultToJson(this);
 }
--- lib/api/route/channels.g.dart
+++ lib/api/route/channels.g.dart
@@ -35,3 +35,28 @@ Map<String, dynamic> _$GetStreamTopicsEntryToJson(
       'max_id': instance.maxId,
       'name': instance.name,
     };
+
+SubscribeToChannelsResult _$SubscribeToChannelsResultFromJson(
+        Map<String, dynamic> json) =>
+    SubscribeToChannelsResult(
+      subscribed: (json['subscribed'] as Map<String, dynamic>).map(
+        (k, e) =>
+            MapEntry(k, (e as List<dynamic>).map((e) => e as String).toList()),
+      ),
+      alreadySubscribed:
+          (json['already_subscribed'] as Map<String, dynamic>).map(
+        (k, e) =>
+            MapEntry(k, (e as List<dynamic>).map((e) => e as String).toList()),
+      ),
+      unauthorized: (json['unauthorized'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+    );
+
+Map<String, dynamic> _$SubscribeToChannelsResultToJson(
+        SubscribeToChannelsResult instance) =>
+    <String, dynamic>{
+      'subscribed': instance.subscribed,
+      'already_subscribed': instance.alreadySubscribed,
+      'unauthorized': instance.unauthorized,
+    };
