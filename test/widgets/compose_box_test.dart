d02b6bc0a08102f4a0250e8c75b9b440471a5a05
Zixuan James Li <zixuan@zulip.com>
2024-09-04 18:40:20 -0400

    compose_box: Use new icons from the redesign
    The icons are mainly exported from the Figma design:
      https://www.figma.com/design/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?node-id=544-22131&node-type=CANVAS&t=hNeqhPGojoFpTJnp-0
    
    Except image.svg, which was downloaded from the original source of the
    icon (https://lucide.dev/icons/image).
    
    Due to the limitation of the icon font, image.svg and send.svg have
    been adjusted with a tool that converts SVG strokes to fills:
      https://www.npmjs.com/package/oslllo-svg-fixer
    
    See also:
    
      - https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/Icon.20not.20rendering.20correctly/near/1936793
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- test/widgets/compose_box_test.dart
+++ test/widgets/compose_box_test.dart
@@ -15,6 +15,7 @@ import 'package:zulip/model/localizations.dart';
 import 'package:zulip/model/narrow.dart';
 import 'package:zulip/model/store.dart';
 import 'package:zulip/widgets/compose_box.dart';
+import 'package:zulip/widgets/icons.dart';
 
 import '../api/fake_api.dart';
 import '../example_data.dart' as eg;
@@ -252,7 +253,7 @@ void main() {
   group('uploads', () {
     void checkAppearsLoading(WidgetTester tester, bool expected) {
       final sendButtonElement = tester.element(find.ancestor(
-        of: find.byIcon(Icons.send),
+        of: find.byIcon(ZulipIcons.send),
         matching: find.byType(IconButton)));
       final sendButtonWidget = sendButtonElement.widget as IconButton;
       final colorScheme = Theme.of(sendButtonElement).colorScheme;
@@ -285,7 +286,7 @@ void main() {
         connection.prepare(delay: const Duration(seconds: 1), json:
           UploadFileResult(uri: '/user_uploads/1/4e/m2A3MSqFnWRLUf9SaPzQ0Up_/image.jpg').toJson());
 
-        await tester.tap(find.byIcon(Icons.image));
+        await tester.tap(find.byIcon(ZulipIcons.image));
         await tester.pump();
         final call = testBinding.takePickFilesCalls().single;
         check(call.allowMultiple).equals(true);
@@ -341,7 +342,7 @@ void main() {
         connection.prepare(delay: const Duration(seconds: 1), json:
           UploadFileResult(uri: '/user_uploads/1/4e/m2A3MSqFnWRLUf9SaPzQ0Up_/image.jpg').toJson());
 
-        await tester.tap(find.byIcon(Icons.camera_alt));
+        await tester.tap(find.byIcon(ZulipIcons.camera));
         await tester.pump();
         final call = testBinding.takePickImageCalls().single;
         check(call.source).equals(ImageSource.camera);
@@ -386,9 +387,9 @@ void main() {
 
     void checkComposeBoxParts({required bool areShown}) {
       check(contentFieldFinder().evaluate().length).equals(areShown ? 1 : 0);
-      check(attachButtonFinder(Icons.attach_file).evaluate().length).equals(areShown ? 1 : 0);
-      check(attachButtonFinder(Icons.image).evaluate().length).equals(areShown ? 1 : 0);
-      check(attachButtonFinder(Icons.camera_alt).evaluate().length).equals(areShown ? 1 : 0);
+      check(attachButtonFinder(ZulipIcons.attach_file).evaluate().length).equals(areShown ? 1 : 0);
+      check(attachButtonFinder(ZulipIcons.image).evaluate().length).equals(areShown ? 1 : 0);
+      check(attachButtonFinder(ZulipIcons.camera).evaluate().length).equals(areShown ? 1 : 0);
     }
 
     void checkBanner({required bool isShown}) {
