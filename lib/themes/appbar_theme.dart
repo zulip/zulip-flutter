d5ba463811839ac2cd249f40763c471963b6e559
Donald Nweyilobu <donnywellson@gmail.com>
2024-10-14 13:41:19 +0100

    feat(theme): Create custom theme for the AppBar

---
+++ lib/themes/appbar_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/text.dart';
+import '../widgets/theme.dart';
+
+
+class ZAppBarTheme {
+  ZAppBarTheme._(); // Private constructor to prevent instantiation
+
+  // Light theme for AppBar
+  static AppBarTheme lightAppBarTheme(BuildContext context, DesignVariables designVariables) {
+    return AppBarTheme(
+      backgroundColor: designVariables.bgTopBar,
+      actionsIconTheme: IconThemeData(color: designVariables.icon),
+      titleTextStyle: TextStyle(
+        color: designVariables.title,
+        fontSize: 20,
+        fontFamily: kDefaultFontFamily, // Assuming this is a constant for font family
+      ).merge(weightVariableTextStyle(context, wght: 600)), // Assuming weight styling function
+      shape: Border(
+        bottom: BorderSide(color: designVariables.borderBar),
+      ),
+    );
+  }
+
+  // Dark theme for AppBar
+  static AppBarTheme darkAppBarTheme(BuildContext context,DesignVariables designVariables) {
+    return AppBarTheme(
+      backgroundColor: designVariables.bgTopBar,
+      actionsIconTheme: IconThemeData(color: designVariables.icon),
+      titleTextStyle: TextStyle(
+        color: designVariables.title,
+        fontSize: 20,
+        fontFamily: kDefaultFontFamily,
+      ).merge(weightVariableTextStyle(context, wght: 600)),
+      shape: Border(
+        bottom: BorderSide(color: designVariables.borderBar),
+      ),
+    );
+  }
+}
