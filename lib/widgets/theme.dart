6ef1eba2737973a7f9c5b65912f9120569da7870
Donald Nweyilobu <donnywellson@gmail.com>
2024-10-19 01:27:21 +0100

    feature: Add ZUlip.getAppBarTheme to theme.dart

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../api/model/model.dart';
+import '../themes/appbar_theme.dart';
 import 'content.dart';
 import 'emoji_reaction.dart';
 import 'message_list.dart';
@@ -36,48 +37,11 @@ ThemeData zulipThemeData(BuildContext context) {
     brightness: brightness,
     typography: zulipTypography(context),
     extensions: themeExtensions,
-    appBarTheme: AppBarTheme(
-      // Set these two fields to prevent a color change in [AppBar]s when
-      // there is something scrolled under it. If an app bar hasn't been
-      // given a backgroundColor directly or by theme, it uses
-      // ColorScheme.surfaceContainer for the scrolled-under state and
-      // ColorScheme.surface otherwise, and those are different colors.
-      scrolledUnderElevation: 0,
-      backgroundColor: designVariables.bgTopBar,
-
-      // TODO match layout to Figma
-      actionsIconTheme: IconThemeData(
-        color: designVariables.icon,
-      ),
-
-      titleTextStyle: TextStyle(
-        inherit: false,
-        color: designVariables.title,
-        fontSize: 20,
-        letterSpacing: 0.0,
-        height: (30 / 20),
-        textBaseline: localizedTextBaseline(context),
-        leadingDistribution: TextLeadingDistribution.even,
-        decoration: TextDecoration.none,
-        fontFamily: kDefaultFontFamily,
-        fontFamilyFallback: defaultFontFamilyFallback,
-      )
-        .merge(weightVariableTextStyle(context, wght: 600)),
-      titleSpacing: 4,
-
-      // TODO Figma has height 42; we should try `toolbarHeight: 42` and test
-      //   that it looks reasonable with different system text-size settings.
-      //   Also the back button will look too big and need adjusting.
-
-      shape: Border(bottom: BorderSide(
-        color: designVariables.borderBar,
-        strokeAlign: BorderSide.strokeAlignInside, // (default restated for explicitness)
-      )),
-    ),
+    appBarTheme: ZAppBarTheme.getAppBarTheme(context, designVariables),
     // This applies Material 3's color system to produce a palette of
     // appropriately matching and contrasting colors for use in a UI.
     // The Zulip brand color is a starting point, but doesn't end up as
-    // one that's directly used.  (After all, we didn't design it for that
+    // one that's directly used.  (After aZll, we didn't design it for that
     // purpose; we designed a logo.)  See docs:
     //   https://api.flutter.dev/flutter/material/ColorScheme/ColorScheme.fromSeed.html
     // Or try this tool to see the whole palette:
