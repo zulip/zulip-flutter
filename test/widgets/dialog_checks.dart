9d2c4df30d9eb965136820ba2980385de534fa63
u7088495 <u7088495@anu.edu.au>
2024-10-22 15:29:01 +1100

    dialog: display adaptive dialogs and action buttons based on the target platform change AlertDialog to AlertDialog.adaptive implement _adaptiveAction dialog_test: update tests to be adaptive

--- test/widgets/dialog_checks.dart
+++ test/widgets/dialog_checks.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,21 +8,40 @@ import 'package:flutter_test/flutter_test.dart';
 /// Checks for an error dialog matching an expected title
 /// and, optionally, matching an expected message. Fails if none is found.
 ///
-/// On success, returns the widget's "OK" button.
+/// On success, returns the widget's "OK" button
+/// 
 /// Dismiss the dialog by calling `tester.tap(find.byWidget(okButton))`.
 Widget checkErrorDialog(WidgetTester tester, {
   required String expectedTitle,
   String? expectedMessage,
 }) {
-  final dialog = tester.widget<AlertDialog>(find.byType(AlertDialog));
-  tester.widget(find.descendant(matchRoot: true,
-    of: find.byWidget(dialog.title!), matching: find.text(expectedTitle)));
-  if (expectedMessage != null) {
-    tester.widget(find.descendant(matchRoot: true,
-      of: find.byWidget(dialog.content!), matching: find.text(expectedMessage)));
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+    case TargetPlatform.fuchsia:
+    case TargetPlatform.linux:
+    case TargetPlatform.windows: {
+      final dialog = tester.widget<Dialog>(find.byType(Dialog));
+      tester.widget(find.widgetWithText(Dialog, expectedTitle));
+      if (expectedMessage != null) {
+        tester.widget(find.widgetWithText(Dialog, expectedMessage));
+      }
+      return tester.widget(
+        find.descendant(of: find.byWidget(dialog),
+          matching: find.widgetWithText(TextButton, 'OK')));
+    }
+    case TargetPlatform.iOS:
+    case TargetPlatform.macOS: {
+      final dialog = tester.widget<CupertinoAlertDialog>(
+        find.byType(CupertinoAlertDialog));
+      tester.widget(find.descendant(matchRoot: true,
+        of: find.byWidget(dialog.title!), matching: find.text(expectedTitle)));
+      if (expectedMessage != null) {
+        tester.widget(find.descendant(matchRoot: true,
+          of: find.byWidget(dialog.content!), matching: find.text(expectedMessage)));
+      }
+      return tester.widget(find.descendant(of: find.byWidget(dialog),
+        matching: find.widgetWithText(CupertinoDialogAction, 'OK')));
+    }
   }
-
-  return tester.widget(
-    find.descendant(of: find.byWidget(dialog),
-      matching: find.widgetWithText(TextButton, 'OK')));
+    
 }
