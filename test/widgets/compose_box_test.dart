7987cfccf8c5bf97d96c90148f5af4fe74096a80
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2024-10-24 23:17:36 +0430

    compose_box: Replace compose box with a banner when cannot post in a channel
    Fixes: #674

--- test/widgets/compose_box_test.dart
+++ test/widgets/compose_box_test.dart
@@ -396,7 +396,9 @@ void main() {
   });
 
   group('compose box replacing with error banner', () {
-    Finder contentFieldFinder() => find.descendant(
+    final zulipLocalizations = GlobalLocalizations.zulipLocalizations;
+
+    Finder inputFieldFinder() => find.descendant(
       of: find.byType(ComposeBox),
       matching: find.byType(TextField));
 
@@ -405,24 +407,26 @@ void main() {
       matching: find.widgetWithIcon(IconButton, icon));
 
     void checkComposeBoxParts({required bool areShown}) {
-      check(contentFieldFinder().evaluate().length).equals(areShown ? 1 : 0);
+      final inputFieldCount = inputFieldFinder().evaluate().length;
+      areShown ? check(inputFieldCount).isGreaterThan(0) : check(inputFieldCount).equals(0);
       check(attachButtonFinder(Icons.attach_file).evaluate().length).equals(areShown ? 1 : 0);
       check(attachButtonFinder(Icons.image).evaluate().length).equals(areShown ? 1 : 0);
       check(attachButtonFinder(Icons.camera_alt).evaluate().length).equals(areShown ? 1 : 0);
     }
 
-    void checkBanner({required bool isShown}) {
-      final bannerTextFinder = find.text(GlobalLocalizations.zulipLocalizations
-        .errorBannerDeactivatedDmLabel);
-      check(bannerTextFinder.evaluate().length).equals(isShown ? 1 : 0);
+    void checkBannerWithLabel(String label, {required bool isShown}) {
+      check(find.text(label).evaluate().length).equals(isShown ? 1 : 0);
     }
 
-    void checkComposeBox({required bool isShown}) {
+    void checkComposeBoxIsShown(bool isShown, {required String bannerLabel}) {
       checkComposeBoxParts(areShown: isShown);
-      checkBanner(isShown: !isShown);
+      checkBannerWithLabel(bannerLabel, isShown: !isShown);
     }
 
     group('in DMs with deactivated users', () {
+      void checkComposeBox({required bool isShown}) => checkComposeBoxIsShown(isShown,
+        bannerLabel: zulipLocalizations.errorBannerDeactivatedDmLabel);
+
       Future<void> changeUserStatus(WidgetTester tester,
           {required User user, required bool isActive}) async {
         await store.handleEvent(RealmUserUpdateEvent(id: 1,
@@ -499,6 +503,156 @@ void main() {
           await changeUserStatus(tester, user: deactivatedUsers[1], isActive: true);
           checkComposeBox(isShown: true);
         });
+      });
+    });
+
+    group('in topic/channel narrow according to channel post policy', () {
+      void checkComposeBox({required bool isShown}) => checkComposeBoxIsShown(isShown,
+        bannerLabel: zulipLocalizations.errorBannerCannotPostInChannelLabel);
+
+      final testCases = [
+        (ChannelPostPolicy.unknown,        UserRole.unknown,       true),
+        (ChannelPostPolicy.unknown,        UserRole.guest,         true),
+        (ChannelPostPolicy.unknown,        UserRole.member,        true),
+        (ChannelPostPolicy.unknown,        UserRole.moderator,     true),
+        (ChannelPostPolicy.unknown,        UserRole.administrator, true),
+        (ChannelPostPolicy.unknown,        UserRole.owner,         true),
+        (ChannelPostPolicy.any,            UserRole.unknown,       true),
+        (ChannelPostPolicy.any,            UserRole.guest,         true),
+        (ChannelPostPolicy.any,            UserRole.member,        true),
+        (ChannelPostPolicy.any,            UserRole.moderator,     true),
+        (ChannelPostPolicy.any,            UserRole.administrator, true),
+        (ChannelPostPolicy.any,            UserRole.owner,         true),
+        (ChannelPostPolicy.fullMembers,    UserRole.unknown,       true),
+        (ChannelPostPolicy.fullMembers,    UserRole.guest,         false),
+        (ChannelPostPolicy.fullMembers,    UserRole.member,        true),
+        (ChannelPostPolicy.fullMembers,    UserRole.moderator,     true),
+        (ChannelPostPolicy.fullMembers,    UserRole.administrator, true),
+        (ChannelPostPolicy.fullMembers,    UserRole.owner,         true),
+        (ChannelPostPolicy.moderators,     UserRole.unknown,       true),
+        (ChannelPostPolicy.moderators,     UserRole.guest,         false),
+        (ChannelPostPolicy.moderators,     UserRole.member,        false),
+        (ChannelPostPolicy.moderators,     UserRole.moderator,     true),
+        (ChannelPostPolicy.moderators,     UserRole.administrator, true),
+        (ChannelPostPolicy.moderators,     UserRole.owner,         true),
+        (ChannelPostPolicy.administrators, UserRole.unknown,       true),
+        (ChannelPostPolicy.administrators, UserRole.guest,         false),
+        (ChannelPostPolicy.administrators, UserRole.member,        false),
+        (ChannelPostPolicy.administrators, UserRole.moderator,     false),
+        (ChannelPostPolicy.administrators, UserRole.administrator, true),
+        (ChannelPostPolicy.administrators, UserRole.owner,         true),
+      ];
+
+      for (final (ChannelPostPolicy policy, UserRole role, bool canPost) in testCases) {
+        testWidgets('"${role.name}" user ${canPost ? 'can' : "can't"} post in channel with "${policy.name}" policy', (tester) async {
+          final selfUser = eg.user(role: role);
+          await prepareComposeBox(tester,
+            narrow: const ChannelNarrow(1),
+            selfUser: selfUser,
+            streams: [eg.stream(streamId: 1, channelPostPolicy: policy)]);
+          checkComposeBox(isShown: canPost);
+        });
+
+        testWidgets('"${role.name}" user ${canPost ? 'can' : "can't"} post in topic with "${policy.name}" channel policy', (tester) async {
+          final selfUser = eg.user(role: role);
+          await prepareComposeBox(tester,
+            narrow: const TopicNarrow(1, 'topic'),
+            selfUser: selfUser,
+            streams: [eg.stream(streamId: 1, channelPostPolicy: policy)]);
+          checkComposeBox(isShown: canPost);
+        });
+      }
+
+      group('only "full member" user can post in channel with "fullMembers" policy', () {
+        testWidgets('"full member" -> can post in channel', (tester) async {
+          final selfUser = eg.user(role: UserRole.member,
+            dateJoined: DateTime.now().subtract(const Duration(days: 30)).toIso8601String());
+          await prepareComposeBox(tester,
+            narrow: const ChannelNarrow(1),
+            selfUser: selfUser,
+            realmWaitingPeriodThreshold: 30,
+            streams: [eg.stream(streamId: 1, channelPostPolicy: ChannelPostPolicy.fullMembers)]);
+          checkComposeBox(isShown: true);
+        });
+
+        testWidgets('not a "full member" -> cannot post in channel', (tester) async {
+          final selfUser = eg.user(role: UserRole.member,
+            dateJoined: DateTime.now().subtract(const Duration(days: 29)).toIso8601String());
+          await prepareComposeBox(tester,
+            narrow: const ChannelNarrow(1),
+            selfUser: selfUser,
+            realmWaitingPeriodThreshold: 30,
+            streams: [eg.stream(streamId: 1, channelPostPolicy: ChannelPostPolicy.fullMembers)]);
+          checkComposeBox(isShown: false);
+        });
+      });
+
+      Future<void> changeUserRole(WidgetTester tester, {
+        required User user,
+        required UserRole role,
+      }) async {
+        await store.handleEvent(RealmUserUpdateEvent(id: 1,
+          userId: user.userId, role: role));
+        await tester.pump();
+      }
+
+      Future<void> changeChannelPolicy(WidgetTester tester, {
+        required ZulipStream channel,
+        required ChannelPostPolicy policy,
+      }) async {
+        await store.handleEvent(eg.channelUpdateEvent(channel,
+          property: ChannelPropertyName.channelPostPolicy, value: policy));
+        await tester.pump();
+      }
+
+      testWidgets('user role decreases -> compose box is replaced with the banner', (tester) async {
+        final selfUser = eg.user(role: UserRole.administrator);
+        await prepareComposeBox(tester,
+          narrow: const ChannelNarrow(1),
+          selfUser: selfUser,
+          streams: [eg.stream(streamId: 1, channelPostPolicy: ChannelPostPolicy.administrators)]);
+        checkComposeBox(isShown: true);
+
+        await changeUserRole(tester, user: selfUser, role: UserRole.moderator);
+        checkComposeBox(isShown: false);
+      });
+
+      testWidgets('user role increases -> banner is replaced with the compose box', (tester) async {
+        final selfUser = eg.user(role: UserRole.guest);
+        await prepareComposeBox(tester,
+          narrow: const ChannelNarrow(1),
+          selfUser: selfUser,
+          streams: [eg.stream(streamId: 1, channelPostPolicy: ChannelPostPolicy.moderators)]);
+        checkComposeBox(isShown: false);
+
+        await changeUserRole(tester, user: selfUser, role: UserRole.administrator);
+        checkComposeBox(isShown: true);
+      });
+
+      testWidgets('channel policy becomes stricter -> compose box is replaced with the banner', (tester) async {
+        final selfUser = eg.user(role: UserRole.guest);
+        final channel = eg.stream(streamId: 1, channelPostPolicy: ChannelPostPolicy.any);
+        await prepareComposeBox(tester,
+          narrow: const ChannelNarrow(1),
+          selfUser: selfUser,
+          streams: [channel]);
+        checkComposeBox(isShown: true);
+
+        await changeChannelPolicy(tester, channel: channel, policy: ChannelPostPolicy.fullMembers);
+        checkComposeBox(isShown: false);
+      });
+
+      testWidgets('channel policy becomes less strict -> banner is replaced with the compose box', (tester) async {
+        final selfUser = eg.user(role: UserRole.moderator);
+        final channel = eg.stream(streamId: 1, channelPostPolicy: ChannelPostPolicy.administrators);
+        await prepareComposeBox(tester,
+          narrow: const ChannelNarrow(1),
+          selfUser: selfUser,
+          streams: [channel]);
+        checkComposeBox(isShown: false);
+
+        await changeChannelPolicy(tester, channel: channel, policy: ChannelPostPolicy.moderators);
+        checkComposeBox(isShown: true);
       });
     });
   });
