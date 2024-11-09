38213a8f8a414bfaf87bfea0bb420ded0003de79
Chris Bobbe <cbobbe@zulip.com>
2024-11-08 20:23:38 -0800

    widget tests: Add `checkSuggestedActionDialog`
    Like we added `checkErrorDialog` in 02fd562b8.

--- test/widgets/dialog_checks.dart
+++ test/widgets/dialog_checks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:zulip/widgets/dialog.dart';
 
 /// In a widget test, check that showErrorDialog was called with the right text.
 ///
@@ -23,4 +24,35 @@ Widget checkErrorDialog(WidgetTester tester, {
   return tester.widget(
     find.descendant(of: find.byWidget(dialog),
       matching: find.widgetWithText(TextButton, 'OK')));
+}
+
+/// In a widget test, check that [showSuggestedActionDialog] was called
+/// with the right text.
+///
+/// Checks for a suggested-action dialog matching an expected title and message.
+/// Fails if none is found.
+///
+/// On success, returns a Record with the widget's action button first
+/// and its cancel button second.
+/// Tap the action button by calling `tester.tap(find.byWidget(actionButton))`.
+(Widget, Widget) checkSuggestedActionDialog(WidgetTester tester, {
+  required String expectedTitle,
+  required String expectedMessage,
+  String? expectedActionButtonText,
+}) {
+  final dialog = tester.widget<AlertDialog>(find.byType(AlertDialog));
+  tester.widget(find.descendant(matchRoot: true,
+    of: find.byWidget(dialog.title!), matching: find.text(expectedTitle)));
+  tester.widget(find.descendant(matchRoot: true,
+    of: find.byWidget(dialog.content!), matching: find.text(expectedMessage)));
+
+  final actionButton = tester.widget(
+    find.descendant(of: find.byWidget(dialog),
+      matching: find.widgetWithText(TextButton, expectedActionButtonText ?? 'Continue')));
+
+  final cancelButton = tester.widget(
+    find.descendant(of: find.byWidget(dialog),
+      matching: find.widgetWithText(TextButton, 'Cancel')));
+
+  return (actionButton, cancelButton);
 }
