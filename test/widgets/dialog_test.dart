bd112c2fc827cc10bf8812b2f4cb12ea86d7e993
Brynly Mitchell - u7088495 <u7088495@anu.edu.au>
2024-11-18 22:40:48 +1100

    dialog: Use Cupertino-flavored alert dialogs on iOS
    Fixes: #996

--- test/widgets/dialog_test.dart
+++ test/widgets/dialog_test.dart
@@ -1,30 +1,69 @@
 import 'package:checks/checks.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:zulip/widgets/dialog.dart';
 
 import '../model/binding.dart';
+import 'dialog_checks.dart';
 import 'test_app.dart';
 
 void main() {
   TestZulipBinding.ensureInitialized();
 
-  group('showErrorDialog', () {
-    testWidgets('tap "Learn more" button', (tester) async {
-      addTearDown(testBinding.reset);
-      await tester.pumpWidget(TestZulipApp());
-      await tester.pump();
-      final element = tester.element(find.byType(Placeholder));
+  late BuildContext context;
 
-      showErrorDialog(context: element, title: 'hello',
-        learnMoreButtonUrl: Uri.parse('https://foo.example'));
+  const title = "Dialog Title";
+  const message = "Dialog message.";
+
+  Future<void> prepare(WidgetTester tester) async {
+    addTearDown(testBinding.reset);
+
+    await tester.pumpWidget(const TestZulipApp(
+      child: Scaffold(body: Placeholder())));
+    await tester.pump();
+    context = tester.element(find.byType(Placeholder));
+  }
+
+  group('showErrorDialog', () {
+    testWidgets('show error dialog', (tester) async {
+      await prepare(tester);
+
+      showErrorDialog(context: context, title: title, message: message);
       await tester.pump();
+      checkErrorDialog(tester, expectedTitle: title, expectedMessage: message);
+    }, variant: const TargetPlatformVariant({TargetPlatform.android, TargetPlatform.iOS}));
+
+    testWidgets('user closes error dialog', (tester) async {
+      await prepare(tester);
+
+      showErrorDialog(context: context, title: title, message: message);
+      await tester.pump();
+
+      final button = checkErrorDialog(tester, expectedTitle: title);
+      await tester.tap(find.byWidget(button));
+      await tester.pump();
+      checkNoDialog(tester);
+    }, variant: const TargetPlatformVariant({TargetPlatform.android, TargetPlatform.iOS}));
+
+    testWidgets('tap "Learn more" button', (tester) async {
+      await prepare(tester);
+
+      final learnMoreButtonUrl = Uri.parse('https://foo.example');
+      showErrorDialog(context: context, title: title, learnMoreButtonUrl: learnMoreButtonUrl);
+      await tester.pump();
+      checkErrorDialog(tester, expectedTitle: title);
+
       await tester.tap(find.text('Learn more'));
-      check(testBinding.takeLaunchUrlCalls()).single.equals((
-        url: Uri.parse('https://foo.example'),
-        mode: LaunchMode.inAppBrowserView));
-    });
+      final expectedMode = switch (defaultTargetPlatform) {
+        TargetPlatform.android => LaunchMode.inAppBrowserView,
+        TargetPlatform.iOS =>     LaunchMode.externalApplication,
+        _ => throw StateError('attempted to test with $defaultTargetPlatform'),
+      };
+      check(testBinding.takeLaunchUrlCalls()).single
+        .equals((url: learnMoreButtonUrl, mode: expectedMode));
+    }, variant: const TargetPlatformVariant({TargetPlatform.android, TargetPlatform.iOS}));
   });
 
   group('showSuggestedActionDialog', () {
@@ -41,7 +80,7 @@ void main() {
       await tester.pump();
       await tester.tap(find.text('Sure'));
       await check(dialog.result).completes((it) => it.equals(true));
-    });
+    }, variant: const TargetPlatformVariant({TargetPlatform.android, TargetPlatform.iOS}));
 
     testWidgets('tap cancel', (tester) async {
       addTearDown(testBinding.reset);
@@ -56,7 +95,7 @@ void main() {
       await tester.pump();
       await tester.tap(find.text('Cancel'));
       await check(dialog.result).completes((it) => it.equals(null));
-    });
+    }, variant: const TargetPlatformVariant({TargetPlatform.android, TargetPlatform.iOS}));
 
     testWidgets('tap outside dialog area', (tester) async {
       addTearDown(testBinding.reset);
@@ -71,7 +110,7 @@ void main() {
       await tester.pump();
       await tester.tapAt(tester.getTopLeft(find.byType(TestZulipApp)));
       await check(dialog.result).completes((it) => it.equals(null));
-    });
+    }, variant: const TargetPlatformVariant({TargetPlatform.android, TargetPlatform.iOS}));
   });
 
   // TODO(#1594): test UpgradeWelcomeDialog
