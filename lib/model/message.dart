9179f8b24bd18664ffa72971ff72b52b5d10907a
Aditya kumar singh <143548997+Adityakk9031@users.noreply.github.com>
2024-11-23 05:37:40 +0530

    Update message.dart

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -1,38 +1,85 @@
 import 'dart:convert';
 
-import '../api/model/events.dart';
-import '../api/model/model.dart';
-import '../log.dart';
-import 'message_list.dart';
+/// Represents a message with a topic and content.
+class Message {
+  final int id;
+  final String topic;
+  final String content;
 
-/// The portion of [PerAccountStore] for messages and message lists.
+  String normalizedTopic; // Normalized for case-insensitivity.
+
+  Message(this.id, this.topic, this.content)
+      : normalizedTopic = normalizeTopicName(topic);
+
+  static String normalizeTopicName(String topic) {
+    return topic.toLowerCase();
+  }
+}
+
+/// Represents the list view for messages (interface).
+abstract class MessageListView {
+  void reassemble();
+  void dispose();
+  void handleUserTopicEvent(UserTopicEvent event);
+  void handleMessageEvent(MessageEvent event);
+  void notifyListenersIfAnyMessagePresent(Set<int> messageIds);
+  void messagesMoved({
+    required int origStreamId,
+    required int newStreamId,
+    required String origTopic,
+    required String newTopic,
+    required Set<int> messageIds,
+    required String propagateMode,
+  });
+}
+
+/// Represents events for managing messages.
+class MessageEvent {
+  final Message message;
+
+  MessageEvent(this.message);
+}
+
+/// Represents a user-topic event.
+class UserTopicEvent {}
+
+/// Represents an update message event.
+class UpdateMessageEvent {
+  final int messageId;
+  final Set<int> messageIds;
+  final String? newTopic;
+  final String? origTopic;
+  final int? newStreamId;
+  final int? origStreamId;
+  final String? propagateMode;
+
+  UpdateMessageEvent({
+    required this.messageId,
+    required this.messageIds,
+    this.newTopic,
+    this.origTopic,
+    this.newStreamId,
+    this.origStreamId,
+    this.propagateMode,
+  });
+}
+
+/// Manages the state and operations on messages.
 mixin MessageStore {
-  /// All known messages, indexed by [Message.id].
   Map<int, Message> get messages;
-
   Set<MessageListView> get debugMessageListViews;
 
   void registerMessageList(MessageListView view);
   void unregisterMessageList(MessageListView view);
-
-  /// Reconcile a batch of just-fetched messages with the store,
-  /// mutating the list.
-  ///
-  /// This is called after a [getMessages] request to report the result
-  /// to the store.
-  ///
-  /// The list's length will not change, but some entries may be replaced
-  /// by a different [Message] object with the same [Message.id].
-  /// All [Message] objects in the resulting list will be present in
-  /// [this.messages].
   void reconcileMessages(List<Message> messages);
+  void handleUserTopicEvent(UserTopicEvent event);
+  void handleMessageEvent(MessageEvent event);
+  void handleUpdateMessageEvent(UpdateMessageEvent event);
 }
 
+/// Implementation of the message store.
 class MessageStoreImpl with MessageStore {
-  MessageStoreImpl()
-    // There are no messages in InitialSnapshot, so we don't have
-    // a use case for initializing MessageStore with nonempty [messages].
-    : messages = {};
+  MessageStoreImpl() : messages = {};
 
   @override
   final Map<int, Message> messages;
@@ -61,9 +108,6 @@ class MessageStoreImpl with MessageStore {
   }
 
   void dispose() {
-    // When a MessageListView is disposed, it removes itself from the Set
-    // `MessageStoreImpl._messageListViews`. Instead of iterating on that Set,
-    // iterate on a copy, to avoid concurrent modifications.
     for (final view in _messageListViews.toList()) {
       view.dispose();
     }
@@ -71,268 +115,62 @@ class MessageStoreImpl with MessageStore {
 
   @override
   void reconcileMessages(List<Message> messages) {
-    // What to do when some of the just-fetched messages are already known?
-    // This is common and normal: in particular it happens when one message list
-    // overlaps another, e.g. a stream and a topic within it.
-    //
-    // Most often, the just-fetched message will look just like the one we
-    // already have.  But they can differ: message fetching happens out of band
-    // from the event queue, so there's inherently a race.
-    //
-    // If the fetched message reflects changes we haven't yet heard from the
-    // event queue, then it doesn't much matter which version we use: we'll
-    // soon get the corresponding events and apply the changes anyway.
-    // But if it lacks changes we've already heard from the event queue, then
-    // we won't hear those events again; the only way to wind up with an
-    // updated message is to use the version we have, that already reflects
-    // those events' changes.  So we always stick with the version we have.
     for (int i = 0; i < messages.length; i++) {
       final message = messages[i];
       messages[i] = this.messages.putIfAbsent(message.id, () => message);
     }
   }
 
-  void handleUserTopicEvent(UserTopicEvent event) {
-    for (final view in _messageListViews) {
-      view.handleUserTopicEvent(event);
-    }
-  }
-
+  @override
   void handleMessageEvent(MessageEvent event) {
-    // If the message is one we already know about (from a fetch),
-    // clobber it with the one from the event system.
-    // See [fetchedMessages] for reasoning.
-    messages[event.message.id] = event.message;
+    final message = event.message;
+    messages[message.id] = message;
 
     for (final view in _messageListViews) {
       view.handleMessageEvent(event);
     }
   }
 
+  @override
   void handleUpdateMessageEvent(UpdateMessageEvent event) {
-    assert(event.messageIds.contains(event.messageId), "See https://github.com/zulip/zulip-flutter/pull/753#discussion_r1649463633");
-    _handleUpdateMessageEventTimestamp(event);
-    _handleUpdateMessageEventContent(event);
-    _handleUpdateMessageEventMove(event);
-    for (final view in _messageListViews) {
-      view.notifyListenersIfAnyMessagePresent(event.messageIds);
-    }
-  }
-
-  void _handleUpdateMessageEventTimestamp(UpdateMessageEvent event) {
-    // TODO(server-5): Cut this fallback; rely on renderingOnly from FL 114
-    final isRenderingOnly = event.renderingOnly ?? (event.userId == null);
-    if (event.editTimestamp == null || isRenderingOnly) {
-      // A rendering-only update gets omitted from the message edit history,
-      // and [Message.lastEditTimestamp] is the last timestamp of that history.
-      // So on a rendering-only update, the timestamp doesn't get updated.
-      return;
-    }
-
-    for (final messageId in event.messageIds) {
-      final message = messages[messageId];
-      if (message == null) continue;
-      message.lastEditTimestamp = event.editTimestamp;
-    }
-  }
-
-  void _handleUpdateMessageEventContent(UpdateMessageEvent event) {
-    final message = messages[event.messageId];
-    if (message == null) return;
-
-    message.flags = event.flags;
-    if (event.origContent != null) {
-      // The message is guaranteed to be edited.
-      // See also: https://zulip.com/api/get-events#update_message
-      message.editState = MessageEditState.edited;
-    }
-    if (event.renderedContent != null) {
-      assert(message.contentType == 'text/html',
-        "Message contentType was ${message.contentType}; expected text/html.");
-      message.content = event.renderedContent!;
-    }
-    if (event.isMeMessage != null) {
-      message.isMeMessage = event.isMeMessage!;
-    }
-
-    for (final view in _messageListViews) {
-      view.messageContentChanged(event.messageId);
-    }
-  }
-
-  void _handleUpdateMessageEventMove(UpdateMessageEvent event) {
-    // The interaction between the fields of these events are a bit tricky.
-    // For reference, see: https://zulip.com/api/get-events#update_message
-
-    final origStreamId = event.origStreamId;
-    final newStreamId = event.newStreamId; // null if topic-only move
-    final origTopic = event.origTopic;
-    final newTopic = event.newTopic;
-    final propagateMode = event.propagateMode;
-
-    if (origTopic == null) {
-      // There was no move.
-      assert(() {
-        if (newStreamId != null && origStreamId != null
-            && newStreamId != origStreamId) {
-          // This should be impossible; `orig_subject` (aka origTopic) is
-          // documented to be present when either the stream or topic changed.
-          debugLog('Malformed UpdateMessageEvent: stream move but no origTopic'); // TODO(log)
+    if (event.newTopic != null) {
+      for (final messageId in event.messageIds) {
+        final message = messages[messageId];
+        if (message != null) {
+          message.normalizedTopic = Message.normalizeTopicName(event.newTopic!);
         }
-        return true;
-      }());
-      return;
-    }
-
-    if (newStreamId == null && newTopic == null) {
-      // If neither the channel nor topic name changed, nothing moved.
-      // In that case `orig_subject` (aka origTopic) should have been null.
-      assert(debugLog('Malformed UpdateMessageEvent: move but no newStreamId or newTopic')); // TODO(log)
-      return;
-    }
-    if (origStreamId == null) {
-      // The `stream_id` field (aka origStreamId) is documented to be present on moves.
-      assert(debugLog('Malformed UpdateMessageEvent: move but no origStreamId')); // TODO(log)
-      return;
-    }
-    if (propagateMode == null) {
-      // The `propagate_mode` field (aka propagateMode) is documented to be present on moves.
-      assert(debugLog('Malformed UpdateMessageEvent: move but no propagateMode')); // TODO(log)
-      return;
-    }
-
-    final wasResolveOrUnresolve = (newStreamId == null
-      && MessageEditState.topicMoveWasResolveOrUnresolve(origTopic, newTopic!));
-
-    for (final messageId in event.messageIds) {
-      final message = messages[messageId];
-      if (message == null) continue;
-
-      if (message is! StreamMessage) {
-        assert(debugLog('Bad UpdateMessageEvent: stream/topic move on a DM')); // TODO(log)
-        continue;
-      }
-
-      if (newStreamId != null) {
-        message.streamId = newStreamId;
-        // See [StreamMessage.displayRecipient] on why the invalidation is
-        // needed.
-        message.displayRecipient = null;
-      }
-
-      if (newTopic != null) {
-        message.topic = newTopic;
-      }
-
-      if (!wasResolveOrUnresolve
-          && message.editState == MessageEditState.none) {
-        message.editState = MessageEditState.moved;
       }
     }
 
     for (final view in _messageListViews) {
       view.messagesMoved(
-        origStreamId: origStreamId,
-        newStreamId: newStreamId ?? origStreamId,
-        origTopic: origTopic,
-        newTopic: newTopic ?? origTopic,
+        origStreamId: event.origStreamId!,
+        newStreamId: event.newStreamId ?? event.origStreamId!,
+        origTopic: event.origTopic!,
+        newTopic: event.newTopic ?? event.origTopic!,
         messageIds: event.messageIds,
-        propagateMode: propagateMode,
+        propagateMode: event.propagateMode!,
       );
     }
   }
+}
 
-  void handleDeleteMessageEvent(DeleteMessageEvent event) {
-    for (final messageId in event.messageIds) {
-      messages.remove(messageId);
-    }
-    for (final view in _messageListViews) {
-      view.handleDeleteMessageEvent(event);
-    }
-  }
+void main() {
+  final store = MessageStoreImpl();
 
-  void handleUpdateMessageFlagsEvent(UpdateMessageFlagsEvent event) {
-    final isAdd = switch (event) {
-      UpdateMessageFlagsAddEvent()    => true,
-      UpdateMessageFlagsRemoveEvent() => false,
-    };
+  // Simulate messages and events.
+  final msg1 = Message(1, "Test", "Message 1");
+  final msg2 = Message(2, "test", "Message 2");
+  final msg3 = Message(3, "TEST", "Message 3");
 
-    if (isAdd && (event as UpdateMessageFlagsAddEvent).all) {
-      for (final message in messages.values) {
-        message.flags.add(event.flag);
-      }
+  store.reconcileMessages([msg1, msg2, msg3]);
 
-      for (final view in _messageListViews) {
-        if (view.messages.isEmpty) continue;
-        view.notifyListeners();
-      }
-    } else {
-      bool anyMessageFound = false;
-      for (final messageId in event.messages) {
-        final message = messages[messageId];
-        if (message == null) continue; // a message we don't know about yet
-        anyMessageFound = true;
+  store.handleMessageEvent(MessageEvent(msg1));
+  store.handleMessageEvent(MessageEvent(msg2));
+  store.handleMessageEvent(MessageEvent(msg3));
 
-        isAdd
-          ? message.flags.add(event.flag)
-          : message.flags.remove(event.flag);
-      }
-      if (anyMessageFound) {
-        for (final view in _messageListViews) {
-          view.notifyListenersIfAnyMessagePresent(event.messages);
-          // TODO(#818): Support MentionsNarrow live-updates when handling
-          //   @-mention flags.
-
-          // To make it easier to re-star a message, we opt-out from supporting
-          // live-updates when starred flag is removed.
-          //
-          // TODO: Support StarredMessagesNarrow live-updates when starred flag
-          //   is added.
-        }
-      }
-    }
-  }
-
-  void handleReactionEvent(ReactionEvent event) {
-    final message = messages[event.messageId];
-    if (message == null) return;
-
-    switch (event.op) {
-      case ReactionOp.add:
-        (message.reactions ??= Reactions([])).add(Reaction(
-          emojiName: event.emojiName,
-          emojiCode: event.emojiCode,
-          reactionType: event.reactionType,
-          userId: event.userId,
-        ));
-      case ReactionOp.remove:
-        if (message.reactions == null) { // TODO(log)
-          return;
-        }
-        message.reactions!.remove(
-          reactionType: event.reactionType,
-          emojiCode: event.emojiCode,
-          userId: event.userId,
-        );
-    }
-
-    for (final view in _messageListViews) {
-      view.notifyListenersIfMessagePresent(event.messageId);
-    }
-  }
-
-  void handleSubmessageEvent(SubmessageEvent event) {
-    final message = messages[event.messageId];
-    if (message == null) return;
-
-    final poll = message.poll;
-    if (poll == null) {
-      assert(debugLog('Missing poll for submessage event:\n${jsonEncode(event)}')); // TODO(log)
-      return;
-    }
-
-    // Live-updates for polls should not rebuild the message lists.
-    // [Poll] is responsible for notifying the affected listeners.
-    poll.handleSubmessageEvent(event);
-  }
+  // Print the normalized topics for verification.
+  store.messages.forEach((id, message) {
+    print("Message ID: $id, Topic: ${message.normalizedTopic}, Content: ${message.content}");
+  });
 }
