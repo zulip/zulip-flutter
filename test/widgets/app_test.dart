f83c795bc6640daef8cef10e668da76024411201
Zixuan James Li <zixuan@zulip.com>
2024-12-04 21:33:00 -0500

    app test [nfc]: Convert initialRoutes into a generic helper
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- test/widgets/app_test.dart
+++ test/widgets/app_test.dart
@@ -24,31 +24,31 @@ void main() {
   group('ZulipApp initial navigation', () {
     late List<Route<dynamic>> pushedRoutes = [];
 
-    Future<List<Route<dynamic>>> initialRoutes(WidgetTester tester) async {
+    Future<void> prepare(WidgetTester tester) async {
+      addTearDown(testBinding.reset);
+
       pushedRoutes = [];
       final testNavObserver = TestNavigatorObserver()
         ..onPushed = (route, prevRoute) => pushedRoutes.add(route);
       await tester.pumpWidget(ZulipApp(navigatorObservers: [testNavObserver]));
       await tester.pump();
-      return pushedRoutes;
     }
 
     testWidgets('when no accounts, go to choose account', (tester) async {
-      addTearDown(testBinding.reset);
-      check(await initialRoutes(tester)).deepEquals(<Condition<Object?>>[
+      await prepare(tester);
+      check(pushedRoutes).deepEquals(<Condition<Object?>>[
         (it) => it.isA<WidgetRoute>().page.isA<ChooseAccountPage>(),
       ]);
     });
 
     testWidgets('when have accounts, go to inbox for first account', (tester) async {
-      addTearDown(testBinding.reset);
-
       // We'll need per-account data for the account that a page will be opened
       // for, but not for the other account.
       await testBinding.globalStore.add(eg.selfAccount, eg.initialSnapshot());
       await testBinding.globalStore.insertAccount(eg.otherAccount.toCompanion(false));
+      await prepare(tester);
 
-      check(await initialRoutes(tester)).deepEquals(<Condition<Object?>>[
+      check(pushedRoutes).deepEquals(<Condition<Object?>>[
         (it) => it.isA<WidgetRoute>().page.isA<ChooseAccountPage>(),
         (it) => it.isA<MaterialAccountWidgetRoute>()
           ..accountId.equals(eg.selfAccount.id)
