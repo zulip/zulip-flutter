dbe1cad073ef2574db9cc4070fea57fa0ad8e841
Chris Bobbe <cbobbe@zulip.com>
2024-12-11 15:58:40 -0800

    test_app: Assert the store has an Account for the passed `accountId`
    This is nice and explicit compared to the behavior I got when I
    simply forgot to call `testBinding.globalStore.add` when working on
    some upcoming tests.

--- test/widgets/test_app.dart
+++ test/widgets/test_app.dart
@@ -9,11 +9,28 @@ class TestZulipApp extends StatelessWidget {
   const TestZulipApp({
     super.key,
     this.accountId,
+    this.skipAssertAccountExists = false,
     this.navigatorObservers,
     this.child = const Placeholder(),
-  });
+  }) : assert(!skipAssertAccountExists || accountId != null);
 
   final int? accountId;
+
+  /// Whether to proceed if [accountId] doesn't have an [Account] in the store.
+  ///
+  /// If this widget's [GlobalStoreWidget] loads
+  /// with no [Account] for [accountId],
+  /// [build] will error unless this param is true.
+  ///
+  /// Usually, this case is just a mistake;
+  /// the caller either forgot to add the account to the store
+  /// or they didn't want to simulate a per-account context in the first place.
+  ///
+  /// Sometimes we want to simulate an account's UI
+  /// just after the account is logged out (so is absent in the store)
+  /// but before we tear down that UI.
+  /// Pass true to silence the assertion in that case.
+  final bool skipAssertAccountExists;
 
   /// A list to pass through to [MaterialApp.navigatorObservers].
   final List<NavigatorObserver>? navigatorObservers;
@@ -27,8 +44,31 @@ class TestZulipApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GlobalStoreWidget(
-      child: MaterialApp(
+    return GlobalStoreWidget(child: Builder(builder: (context) {
+      assert(() {
+        if (accountId != null && !skipAssertAccountExists) {
+          final account = GlobalStoreWidget.of(context).getAccount(accountId!);
+          if (account == null) {
+            throw FlutterError.fromParts([
+              ErrorSummary(
+                'TestZulipApp() was called with [accountId] but a corresponding '
+                'Account was not found in the GlobalStore.'),
+              ErrorHint(
+                'If [child] needs per-account data, consider calling '
+                '`testBinding.globalStore.add` before pumping `TestZulipApp`.'),
+              ErrorHint(
+                'If [child] is not specific to an account, omit [accountId].'),
+              ErrorHint(
+                'If you are testing behavior when an account is logged out, '
+                'consider building ZulipApp instead of TestZulipApp, '
+                'or pass `skipAssertAccountExists: true`.'),
+            ]);
+          }
+        }
+        return true;
+      }());
+
+      return MaterialApp(
         title: 'Zulip',
         localizationsDelegates: ZulipLocalizations.localizationsDelegates,
         supportedLocales: ZulipLocalizations.supportedLocales,
@@ -39,6 +79,7 @@ class TestZulipApp extends StatelessWidget {
         home: accountId != null
           ? PerAccountStoreWidget(accountId: accountId!, child: child)
           : child,
-      ));
+      );
+    }));
   }
 }
