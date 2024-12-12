76425ed6a1d0426b97421dc0b703ad83199199ea
Zixuan James Li <zixuan@zulip.com>
2024-12-12 17:33:34 -0500

    i18n: Sweep for non-localized '(unknown user)' strings
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/widgets/inbox.dart
+++ lib/widgets/inbox.dart
@@ -384,16 +384,20 @@ class _DmItem extends StatelessWidget {
     final store = PerAccountStoreWidget.of(context);
     final selfUser = store.users[store.selfUserId]!;
 
+    final zulipLocalizations = ZulipLocalizations.of(context);
     final designVariables = DesignVariables.of(context);
 
     final title = switch (narrow.otherRecipientIds) { // TODO dedupe with [RecentDmConversationsItem]
       [] => selfUser.fullName,
-      [var otherUserId] => store.users[otherUserId]?.fullName ?? '(unknown user)',
+      [var otherUserId] =>
+        store.users[otherUserId]?.fullName ?? zulipLocalizations.unknownUserName,
 
       // TODO(i18n): List formatting, like you can do in JavaScript:
       //   new Intl.ListFormat('ja').format(['Chris', 'Greg', 'Alya', 'Shu'])
       //   // 'Chris、Greg、Alya、Shu'
-      _ => narrow.otherRecipientIds.map((id) => store.users[id]?.fullName ?? '(unknown user)').join(', '),
+      _ => narrow.otherRecipientIds.map(
+        (id) => store.users[id]?.fullName ?? zulipLocalizations.unknownUserName
+      ).join(', '),
     };
 
     return Material(
--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -415,7 +415,8 @@ class MessageListAppBarTitle extends StatelessWidget {
         if (otherRecipientIds.isEmpty) {
           return Text(zulipLocalizations.dmsWithYourselfPageTitle);
         } else {
-          final names = otherRecipientIds.map((id) => store.users[id]?.fullName ?? '(unknown user)');
+          final names = otherRecipientIds.map(
+            (id) => store.users[id]?.fullName ?? zulipLocalizations.unknownUserName);
           return Text("DMs with ${names.join(", ")}"); // TODO show avatars
         }
     }
