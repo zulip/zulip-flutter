afc1b3d4b727d9947486b0cf63b30813cb4b6060
Zixuan James Li <zixuan@zulip.com>
2024-12-18 15:46:28 -0500

    db: Start generating step by step migration helper
    An alternative to this is `dart run drift_dev make-migrations`, which is
    essentially a wrapper for the `schema {dump,generate,steps}`
    subcommands.  `make-migrations` let us manage multiple database schemas
    by configuring them with `build.yaml`, and it dictates the
    which subdirectories the generated files will be created at.
    
    Because `make-migrations` does not offer the same level of customizations
    to designate exactly where the output files will be, opting out from it
    for now.
    
    We can revisit this if it starts to offer features that are not
    available with the subcommands, or that we find the need for managing
    multiple databases.
    
    See also:
      https://drift.simonbinder.eu/migrations/step_by_step/#manual-generation
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/model/database.dart
+++ lib/model/database.dart
@@ -2,6 +2,8 @@ import 'package:drift/drift.dart';
 import 'package:drift/remote.dart';
 import 'package:sqlite3/common.dart';
 
+import 'schema_versions.g.dart';
+
 part 'database.g.dart';
 
 /// The table of [Account] records in the app's database.
@@ -82,12 +84,13 @@ class AppDatabase extends _$AppDatabase {
         }
         assert(1 <= from && from <= to && to <= schemaVersion);
 
-        if (from < 2 && 2 <= to) {
-          await m.addColumn(accounts, accounts.ackedPushToken);
-        }
-        // New migrations go here.
-      }
-    );
+        await m.runMigrationSteps(from: from, to: to,
+          steps: migrationSteps(
+            from1To2: (m, schema) async {
+              await m.addColumn(schema.accounts, schema.accounts.ackedPushToken);
+            },
+          ));
+      });
   }
 
   Future<int> createAccount(AccountsCompanion values) async {
