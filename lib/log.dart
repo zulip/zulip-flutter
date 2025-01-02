bc2692d47b71a980357568c7fe13d50d4f2d27c4
Zixuan James Li <zixuan@zulip.com>
2025-01-02 02:12:20 -0500

    log: Add reportErrorModally
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/log.dart
+++ lib/log.dart
@@ -32,6 +32,7 @@ bool debugLog(String message) {
 }
 
 typedef ReportErrorCancellablyCallback = void Function(String? message, {String? details});
+typedef ReportErrorCallback = void Function(String message, {String? details});
 
 /// Show the user an error message, without requiring them to interact with it.
 ///
@@ -49,6 +50,17 @@ typedef ReportErrorCancellablyCallback = void Function(String? message, {String?
 // from importing widget code, because the file is a dependency for the rest of
 // the app.
 ReportErrorCancellablyCallback reportErrorToUserBriefly = reportErrorToConsole;
+
+/// Show the user a dismissable error message in a modal popup.
+///
+/// Typically this shows a [AlertDialog] containing the message.
+/// If called before the app's widget tree is ready (see [ZulipApp.ready]),
+/// then we give up on showing the message to the user,
+/// and just log the message to the console.
+// This gets set in [ZulipApp].  We need this indirection to keep `lib/log.dart`
+// from importing widget code, because the file is a dependency for the rest of
+// the app.
+ReportErrorCallback reportErrorToUserModally = reportErrorToConsole;
 
 void reportErrorToConsole(String? message, {String? details}) {
   // Error dismissing is a no-op to the default handler.
