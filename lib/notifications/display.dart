5e9933a09dd101096f22f2f4bc8e6982664ef564
Zixuan James Li <zixuan@zulip.com>
2025-01-15 16:34:45 -0500

    display [nfc]: Mention issue for notification channel name's i18n TODO
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/notifications/display.dart
+++ lib/notifications/display.dart
@@ -204,7 +204,7 @@ class NotificationChannelManager {
 
     await _androidHost.createNotificationChannel(NotificationChannel(
       id: kChannelId,
-      name: 'Messages', // TODO(i18n)
+      name: 'Messages', // TODO(#1284)
       importance: NotificationImportance.high,
       lightsEnabled: true,
       soundUrl: defaultSoundUrl,
