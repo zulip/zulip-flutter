a50d8ae5277b36fb5674499386779fd283c61aae
Zixuan James Li <zixuan@zulip.com>
2025-01-16 15:07:49 -0500

    api: Indicate support for handling empty topics
    Look for `allow_empty_topic_name` and `empty_topic_name` under "Feature
    level 334" in the API Changelog to verify the affected routes:
      https://zulip.com/api/changelog
    
    To keep the API bindings thin, instead of setting
    `allow_empty_topic_name` for the callers, we require the callers to pass
    the appropriate values instead.
    
    Instead of making this parameter a `bool` that defaults to `false`
    (and have the bindings remove the field when it's `false`), we type it
    as `bool?` and only drop it when it is `null`.  This is also for making
    the API binding thin.
    
    Fixes: #1250
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- test/model/autocomplete_test.dart
+++ test/model/autocomplete_test.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:checks/checks.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 import 'package:test/scaffolding.dart';
 import 'package:zulip/api/model/initial_snapshot.dart';
 import 'package:zulip/api/model/model.dart';
@@ -19,6 +20,7 @@ import 'package:zulip/widgets/compose_box.dart';
 import '../api/fake_api.dart';
 import '../example_data.dart' as eg;
 import '../fake_async.dart';
+import '../stdlib_checks.dart';
 import 'test_store.dart';
 import 'autocomplete_checks.dart';
 
@@ -1024,6 +1026,38 @@ void main() {
     check(done).isFalse();
     await Future(() {});
     check(done).isTrue();
+  });
+
+  test('TopicAutocompleteView allow empty topic name on modern servers', () async {
+    final account = eg.account(user: eg.selfUser, zulipFeatureLevel: 334);
+    final store = eg.store(account: account, initialSnapshot: eg.initialSnapshot(
+      zulipFeatureLevel: 334));
+    final connection = store.connection as FakeApiConnection;
+
+    connection.prepare(json: GetStreamTopicsResult(
+      topics: [eg.getStreamTopicsEntry(name: '')],
+    ).toJson());
+    TopicAutocompleteView.init(store: store, streamId: 1000,
+      query: TopicAutocompleteQuery('foo'));
+    check(connection.lastRequest).isA<http.Request>()
+      ..url.path.equals('/api/v1/users/me/1000/topics')
+      ..url.queryParameters['allow_empty_topic_name'].equals('true');
+  });
+
+  test('TopicAutocompleteView disallow empty topic name on legacy servers', () async {
+    final account = eg.account(user: eg.selfUser, zulipFeatureLevel: 333);
+    final store = eg.store(account: account, initialSnapshot: eg.initialSnapshot(
+      zulipFeatureLevel: 333));
+    final connection = store.connection as FakeApiConnection;
+
+    connection.prepare(json: GetStreamTopicsResult(
+      topics: [eg.getStreamTopicsEntry(name: 'foo')],
+    ).toJson());
+    TopicAutocompleteView.init(store: store, streamId: 1000,
+      query: TopicAutocompleteQuery('foo'));
+    check(connection.lastRequest).isA<http.Request>()
+      ..url.path.equals('/api/v1/users/me/1000/topics')
+      ..url.queryParameters.isEmpty();
   });
 
   group('TopicAutocompleteQuery.testTopic', () {
