118746e3b44ae384ff57110f64d46165bf66ce36
Zixuan James Li <zixuan@zulip.com>
2025-01-21 16:39:00 -0500

    do not merge; compose: Handle hint text for empty topics
    This is a placeholder before we actually handle empty topics for compose
    box in general.
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- lib/widgets/compose_box.dart
+++ lib/widgets/compose_box.dart
@@ -586,6 +586,10 @@ class _StreamContentInputState extends State<_StreamContentInput> {
     final streamName = store.streams[widget.narrow.streamId]?.name
       ?? zulipLocalizations.unknownChannelName;
     final topic = TopicName(widget.controller.topic.textNormalized);
+    // Until we support sending to empty topics, `topic.displayName` will remain
+    // non-null because the topic input text gets normalized to "(no topic)".
+    // ignore: unnecessary_null_comparison // null topic names soon to be enabled
+    assert(topic.displayName != null);
     return _ContentInput(
       narrow: widget.narrow,
       destination: TopicNarrow(widget.narrow.streamId, topic),
@@ -658,7 +662,8 @@ class _FixedDestinationContentInput extends StatelessWidget {
           // Zulip expresses channels and topics, not any normal English punctuation,
           // so don't make sense to translate. See:
           //   https://github.com/zulip/zulip-flutter/pull/1148#discussion_r1941990585
-          '#$streamName > ${topic.displayName}');
+          // ignore: dead_null_aware_expression // null topic names soon to be enabled
+          '#$streamName > ${topic.displayName ?? store.realmEmptyTopicDisplayName}');
 
       case DmNarrow(otherRecipientIds: []): // The self-1:1 thread.
         return zulipLocalizations.composeBoxSelfDmContentHint;
