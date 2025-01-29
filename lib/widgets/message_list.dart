92acf67499760f520fd75b72eaaf1f4f7c38c7b3
lakshya1goel <lakshya1234goel@gmail.com>
2025-01-29 10:29:50 +0530

    msglist: Move star icon 2px away from the edge of the screen
    Fixes: #1247

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -4,7 +4,7 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_color_models/flutter_color_models.dart';
 import 'package:intl/intl.dart';
-
+import 'package:flutter/widgets.dart' as flutter;
 import '../api/model/model.dart';
 import '../generated/l10n/zulip_localizations.dart';
 import '../model/message_list.dart';
@@ -1381,8 +1381,15 @@ class MessageWithPossibleSender extends StatelessWidget {
                 ])),
               SizedBox(width: 16,
                 child: message.flags.contains(MessageFlag.starred)
-                  ? Icon(ZulipIcons.star_filled, size: 16, color: designVariables.star)
-                  : null),
+                    ? Transform.translate(
+                        offset: Offset(
+                          Directionality.of(context) == flutter.TextDirection.rtl ? 2 : -2,
+                          0,
+                        ),
+                        child: Icon(ZulipIcons.star_filled, size: 16, color: designVariables.star)
+                      )
+                    : null,
+              ),
             ]),
         ])));
   }
