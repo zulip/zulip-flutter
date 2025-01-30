897ed88659e27236602c3ce8b8c5c6405f5d10b7
Greg Price <greg@zulip.com>
2025-01-30 14:56:21 -0800

    page [nfc]: Add interface to get account ID for most of our routes

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -30,7 +30,7 @@ class ProfilePage extends StatelessWidget {
 
   final int userId;
 
-  static Route<void> buildRoute({int? accountId, BuildContext? context,
+  static AccountRoute<void> buildRoute({int? accountId, BuildContext? context,
       required int userId}) {
     return MaterialAccountWidgetRoute(accountId: accountId, context: context,
       page: ProfilePage(userId: userId));
