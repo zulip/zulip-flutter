39b2c53cf91ab1eecc54fc1d695bec1c3c1261a0
Komyyy <pol_tta@outlook.jp>
2025-02-17 19:46:29 +0900

    message_list: reduce redundant arguments

--- test/widgets/message_list_test.dart
+++ test/widgets/message_list_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:checks/checks.dart';
+import 'package:clock/clock.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -1421,7 +1422,6 @@ void main() {
 
   group('formatHeaderDate', () {
     final zulipLocalizations = GlobalLocalizations.zulipLocalizations;
-    final now = DateTime.parse("2023-01-10 12:00");
     final testCases = [
       ("2023-01-10 12:00", zulipLocalizations.today),
       ("2023-01-10 00:00", zulipLocalizations.today),
@@ -1436,8 +1436,10 @@ void main() {
     ];
     for (final (dateTime, expected) in testCases) {
       test('$dateTime returns $expected', () {
-        check(formatHeaderDate(zulipLocalizations, DateTime.parse(dateTime), now: now))
-          .equals(expected);
+        withClock(Clock.fixed(DateTime.parse("2023-01-10 12:00")), () {
+          check(formatHeaderDate(zulipLocalizations, DateTime.parse(dateTime)))
+            .equals(expected);
+        });
       });
     }
   });
