a1070e88a616efca84ed2f556cf517b15cbae3c6
Komyyy <pol_tta@outlook.jp>
2025-02-17 16:36:03 +0900

    message_list: make current time testable

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart' hide TextDirection;
 
 import '../api/model/model.dart';
 import '../generated/l10n/zulip_localizations.dart';
+import '../model/binding.dart';
 import '../model/message_list.dart';
 import '../model/narrow.dart';
 import '../model/store.dart';
@@ -1370,7 +1371,7 @@ class DateText extends StatelessWidget {
       formatHeaderDate(
         zulipLocalizations,
         DateTime.fromMillisecondsSinceEpoch(timestamp * 1000),
-        now: DateTime.now()));
+        now: ZulipBinding.instance.utcNow().toLocal()));
   }
 }
 
