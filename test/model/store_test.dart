cb1f71a34084e7128727013b867c3cef50f41fbb
Zixuan James Li <zixuan@zulip.com>
2025-02-20 18:01:11 -0500

    test [nfc]: Use eg.globalStore when possible
    Later we will add more required parameters to TestGlobalStore.
    
    Signed-off-by: Zixuan James Li <zixuan@zulip.com>

--- test/model/store_test.dart
+++ test/model/store_test.dart
@@ -428,7 +428,7 @@ void main() {
     late FakeApiConnection connection;
 
     Future<void> prepareStore({Account? account}) async {
-      globalStore = TestGlobalStore(accounts: []);
+      globalStore = eg.globalStore();
       account ??= eg.selfAccount;
       await globalStore.insertAccount(account.toCompanion(false));
       connection = (globalStore.apiConnectionFromAccount(account)
@@ -603,7 +603,7 @@ void main() {
     }
 
     Future<void> preparePoll({int? lastEventId}) async {
-      globalStore = TestGlobalStore(accounts: []);
+      globalStore = eg.globalStore();
       await globalStore.add(eg.selfAccount, eg.initialSnapshot(
         lastEventId: lastEventId));
       await globalStore.perAccount(eg.selfAccount.id);
