572b1c2d11d646ac87a192e0adc3884176f7d969
E-m-i-n-e-n-c-e <kssakhilraj@gmail.com>
2025-02-21 20:11:21 +0530

    msglist: Fix channel header tap behaviour in multi-channel narrows
    Set gesture detecter behavior of streamWidget to HitTestBehavior.opaque
    to handle taps in empty space around the header.
    
    Fixes #1179.

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -1082,6 +1082,7 @@ class StreamMessageRecipientHeader extends StatelessWidget {
         ?? zulipLocalizations.unknownChannelName; // TODO(log)
 
       streamWidget = GestureDetector(
+        behavior: HitTestBehavior.opaque,
         onTap: () => Navigator.push(context,
           MessageListPage.buildRoute(context: context,
             narrow: ChannelNarrow(message.streamId))),
