6eda47ea4c9e8ea3248b3050bf2d3c8765cc89fe
Rajesh Malviya <rmalviya@zulip.com>
2025-02-28 21:20:42 +0530

    notif ios: Navigate when app launched from notification
    Introduces a new Pigeon API file, and adds the corresponding
    bindings in Swift. Unlike the `pigeon/android_notifications.dart`
    API this doesn't use the ZulipPlugin hack, as that is
    only needed when we want the Pigeon functions to be available
    inside a background isolate (see doc in `zulip_plugin/pubspec.yaml`).
    
    Since the notification tap will trigger an app launch first
    (if not running already) anyway, we can be sure that these new
    functions won't be running on a Dart background isolate, thus not
    needing the ZulipPlugin hack.

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -11,6 +11,7 @@ import '../model/binding.dart';
 import '../model/localizations.dart';
 import '../model/store.dart';
 import '../notifications/display.dart';
+import '../notifications/open.dart';
 import 'about_zulip.dart';
 import 'dialog.dart';
 import 'home.dart';
@@ -160,6 +161,8 @@ class _ZulipAppState extends State<ZulipApp> with WidgetsBindingObserver {
     WidgetsBinding.instance.addObserver(this);
     () async {
       final globalStore = await ZulipBinding.instance.getGlobalStoreUniquely();
+      final notifFuture = NotificationOpenManager.instance.initializationFuture;
+      if (notifFuture != null) await notifFuture;
       if (!mounted) return;
       setState(() {
         _globalStore = globalStore;
@@ -194,6 +197,31 @@ class _ZulipAppState extends State<ZulipApp> with WidgetsBindingObserver {
         // The account didn't match any existing accounts,
         // fall through to show the default route below.
       }
+    }
+
+    final globalStore = GlobalStoreWidget.of(context);
+    // TODO(#524) choose initial account as last one used
+    final initialAccountId = globalStore.accounts.firstOrNull?.id;
+    return [
+      if (initialAccountId == null)
+        MaterialWidgetRoute(page: const ChooseAccountPage())
+      else
+        HomePage.buildRoute(accountId: initialAccountId),
+    ];
+  }
+
+  List<Route<dynamic>> _handleGenerateInitialRoutesIos(_) {
+    // The `_ZulipAppState.context` lacks the required ancestors. Instead
+    // we use the Navigator which should be available when this callback is
+    // called and its context should have the required ancestors.
+    final context = ZulipApp.navigatorKey.currentContext!;
+
+    final route = NotificationOpenManager.instance.routeForNotificationFromLaunch(context: context);
+    if (route != null) {
+      return [
+        HomePage.buildRoute(accountId: route.accountId),
+        route,
+      ];
     }
 
     final globalStore = GlobalStoreWidget.of(context);
@@ -260,8 +288,10 @@ class _ZulipAppState extends State<ZulipApp> with WidgetsBindingObserver {
           // handles startup, and then we always push whole routes with methods
           // like [Navigator.push], never mere names as with [Navigator.pushNamed].
           onGenerateRoute: (_) => null,
-
-          onGenerateInitialRoutes: _handleGenerateInitialRoutes);
+          // TODO migrate Android's handling to the new Pigeon API.
+          onGenerateInitialRoutes: defaultTargetPlatform == TargetPlatform.iOS
+            ? _handleGenerateInitialRoutesIos
+            : _handleGenerateInitialRoutes);
       }));
   }
 }
