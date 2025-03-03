07290de5f4eb0d340eda27b8a20f55ff65a60995
Rajesh Malviya <rmalviya@zulip.com>
2025-03-03 20:57:00 +0530

    notif ios: Navigate when app running but in background

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private var notificationTapEventListener: NotificationTapEventListener?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -16,7 +18,26 @@ import Flutter
     let api = NotificationHostApiImpl(notificationPayload.map { NotificationDataFromLaunch(payload: $0) })
     NotificationHostApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: api)
 
+    notificationTapEventListener = NotificationTapEventListener()
+    NotificationTapEventsStreamHandler.register(with: controller.binaryMessenger, streamHandler: notificationTapEventListener!)
+
+    // Setup handler for notification tap while the app is running.
+    UNUserNotificationCenter.current().delegate = self
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    didReceive response: UNNotificationResponse,
+    withCompletionHandler completionHandler: @escaping () -> Void
+  ) {
+    if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
+      let listener = notificationTapEventListener!
+      let userInfo = response.notification.request.content.userInfo
+      listener.onNotificationTapEvent(payload: userInfo)
+    }
+    completionHandler()
   }
 }
 
@@ -29,5 +50,17 @@ private class NotificationHostApiImpl: NotificationHostApi {
 
   func getNotificationDataFromLaunch() -> NotificationDataFromLaunch? {
     maybeDataFromLaunch
+  }
+}
+
+class NotificationTapEventListener: NotificationTapEventsStreamHandler {
+  var eventSink: PigeonEventSink<NotificationTapEvent>?
+
+  override func onListen(withArguments arguments: Any?, sink: PigeonEventSink<NotificationTapEvent>) {
+    eventSink = sink
+  }
+
+  func onNotificationTapEvent(payload: [AnyHashable : Any]) {
+    eventSink?.success(NotificationTapEvent(payload: payload))
   }
 }
