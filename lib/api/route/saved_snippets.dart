c5cc482e3049ece5d4cde202d8053ae8d943c372
Zixuan James Li <zixuan@zulip.com>
2025-03-06 14:23:52 -0500

    api: Add createSavedSnippet route

---
+++ lib/api/route/saved_snippets.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../core.dart';
+
+part 'saved_snippets.g.dart';
+
+/// https://zulip.com/api/create-saved-snippet
+Future<CreateSavedSnippetResult> createSavedSnippet(ApiConnection connection, {
+  required String title,
+  required String content,
+}) {
+  return connection.post('createSavedSnippet', CreateSavedSnippetResult.fromJson, 'saved_snippets', {
+    'title': RawParameter(title),
+    'content': RawParameter(content),
+  });
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class CreateSavedSnippetResult {
+  final int savedSnippetId;
+
+  CreateSavedSnippetResult({
+    required this.savedSnippetId,
+  });
+
+  factory CreateSavedSnippetResult.fromJson(Map<String, dynamic> json) =>
+    _$CreateSavedSnippetResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CreateSavedSnippetResultToJson(this);
+}
