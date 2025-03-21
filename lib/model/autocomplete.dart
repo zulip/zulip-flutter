50c6764be24df664be118e1b08d2205da8a14ba0
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2025-03-21 21:17:04 +0430

    autocomplete: Exclude muted users from user-mention autocomplete
    Fixes: #296

--- lib/model/autocomplete.dart
+++ lib/model/autocomplete.dart
@@ -12,6 +12,7 @@ import 'compose.dart';
 import 'emoji.dart';
 import 'narrow.dart';
 import 'store.dart';
+import 'user.dart';
 
 extension ComposeContentAutocomplete on ComposeContentController {
   AutocompleteIntent<ComposeAutocompleteQuery>? autocompleteIntent() {
@@ -449,6 +450,8 @@ class MentionAutocompleteView extends AutocompleteView<MentionAutocompleteQuery,
     required PerAccountStore store,
     required Narrow narrow,
   }) {
+    // return store.allUsers.whereNot((user) => store.isUserMuted(user.userId)).toList()
+    //   ..sort(_comparator(store: store, narrow: narrow));
     return store.allUsers.toList()
       ..sort(_comparator(store: store, narrow: narrow));
   }
@@ -649,7 +652,7 @@ class MentionAutocompleteView extends AutocompleteView<MentionAutocompleteQuery,
   }
 
   MentionAutocompleteResult? _testUser(MentionAutocompleteQuery query, User user) {
-    if (query.testUser(user, store.autocompleteViewManager.autocompleteDataCache)) {
+    if (query.testUser(user, store.autocompleteViewManager.autocompleteDataCache, store)) {
       return UserMentionAutocompleteResult(userId: user.userId);
     }
     return null;
@@ -754,9 +757,10 @@ class MentionAutocompleteQuery extends ComposeAutocompleteQuery {
       || wildcardOption.localizedCanonicalString(localizations).contains(_lowercase);
   }
 
-  bool testUser(User user, AutocompleteDataCache cache) {
+  bool testUser(User user, AutocompleteDataCache cache, UserStore userStore) {
     // TODO(#236) test email too, not just name
     if (!user.isActive) return false;
+    if (userStore.isUserMuted(user.userId)) return false;
 
     return _testName(user, cache);
   }
