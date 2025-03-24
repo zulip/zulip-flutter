2c5bc33350795668b951a6adb25b7fdc6f062786
Zixuan James Li <zixuan@zulip.com>
2025-03-24 19:17:54 -0400

    compose: Unskip test; handle TopicName with empty displayName for now
    This logic was introduced in 769cc7df, which relied on the fact that
    TopicName.displayName is `null` when the topic is empty.  Normally it is
    fine to assume that the displayName is non-empty with the server flags,
    but in this case the TopicName is constructed by our code.

--- lib/widgets/compose_box.dart
+++ lib/widgets/compose_box.dart
@@ -642,8 +642,10 @@ class _StreamContentInputState extends State<_StreamContentInput> {
       // so don't make sense to translate. See:
       //   https://github.com/zulip/zulip-flutter/pull/1148#discussion_r1941990585
       ? '#$streamName'
-      // ignore: dead_null_aware_expression // null topic names soon to be enabled
-      : '#$streamName > ${hintTopic.displayName ?? store.realmEmptyTopicDisplayName}';
+      // null topic names soon to be enabled
+      : '#$streamName > '
+        '${hintTopic.displayName.isEmpty ? store.realmEmptyTopicDisplayName
+                                         : hintTopic.displayName}';
 
     return _ContentInput(
       narrow: widget.narrow,
