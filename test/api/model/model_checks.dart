d4a1100d5112f7304e5fbbfbceadebc9928e7e61
Zixuan James Li <zixuan@zulip.com>
2025-03-27 18:19:12 -0400

    api [nfc]: Pull out MessageBase, make Message generic
    See also CZO discussion on the design of this, and the drawbacks of
    the alternatives:
      https://chat.zulip.org/#narrow/channel/243-mobile-team/topic/A.20new.20variant.20of.20Zulip.20messages.20-.20inheritance.20structure/with/2141288
    
    This will help support outbox messages in MessageListView later.
    
    This requiring specifying the element type of `List`s in some tests (or
    in some cases, the type of a variable declared).
    
    This is a side effect of making `StreamMessage` and `DmMessage` extend
    `Message<T>` with different `T`'s. When both appear in the same `List`,
    the upper bound is `Object`, instead of the more specific
    `Message<Conversation>`.
    See "least-upper-bound" tagged issues for reference:
      https://github.com/dart-lang/language/issues?q=state%3Aopen%20label%3A%22least-upper-bound%22

--- test/api/model/model_checks.dart
+++ test/api/model/model_checks.dart
@@ -29,6 +29,13 @@ extension TopicNameChecks on Subject<TopicName> {
   Subject<String> get displayName => has((x) => x.displayName, 'displayName');
 }
 
+extension MessageBaseChecks<T extends Conversation> on Subject<MessageBase<T>> {
+  Subject<int?> get id => has((e) => e.id, 'id');
+  Subject<int> get senderId => has((e) => e.senderId, 'senderId');
+  Subject<int> get timestamp => has((e) => e.timestamp, 'timestamp');
+  Subject<T> get conversation => has((e) => e.conversation, 'conversation');
+}
+
 extension MessageChecks on Subject<Message> {
   Subject<String> get client => has((e) => e.client, 'client');
   Subject<String> get content => has((e) => e.content, 'content');
@@ -41,10 +48,8 @@ extension MessageChecks on Subject<Message> {
   Subject<int> get recipientId => has((e) => e.recipientId, 'recipientId');
   Subject<String> get senderEmail => has((e) => e.senderEmail, 'senderEmail');
   Subject<String> get senderFullName => has((e) => e.senderFullName, 'senderFullName');
-  Subject<int> get senderId => has((e) => e.senderId, 'senderId');
   Subject<String> get senderRealmStr => has((e) => e.senderRealmStr, 'senderRealmStr');
   Subject<Poll?> get poll => has((e) => e.poll, 'poll');
-  Subject<int> get timestamp => has((e) => e.timestamp, 'timestamp');
   Subject<String> get type => has((e) => e.type, 'type');
   Subject<List<MessageFlag>> get flags => has((e) => e.flags, 'flags');
   Subject<String?> get matchContent => has((e) => e.matchContent, 'matchContent');
