5634c57c4dea97fe9614143f8db65c53a0c3b7d7
Zixuan James Li <zixuan@zulip.com>
2025-03-27 16:46:51 -0400

    store test: Add a test for throwing on missing queueId

--- test/model/store_test.dart
+++ test/model/store_test.dart
@@ -296,6 +296,16 @@ void main() {
     check(connection).isOpen.isFalse();
   }));
 
+  test('GlobalStore.perAccount throws if missing queueId', () async {
+    final globalStore = UpdateMachineTestGlobalStore(accounts: [eg.selfAccount]);
+    globalStore.prepareRegisterQueueResponse = (connection) {
+      connection.prepare(json:
+        deepToJson(eg.initialSnapshot()) as Map<String, dynamic>
+          ..['queue_id'] = null);
+    };
+    await check(globalStore.perAccount(eg.selfAccount.id)).throws();
+  });
+
   // TODO test insertAccount
 
   group('GlobalStore.updateAccount', () {
