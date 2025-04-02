d4c4388a80a2bff9e28487cb183d70b78285d191
Hosted Weblate <hosted@weblate.org>
2025-04-02 12:03:07 +0200

    l10n: Update translations from Weblate.

--- lib/generated/l10n/zulip_localizations_pl.dart
+++ lib/generated/l10n/zulip_localizations_pl.dart
@@ -349,7 +349,7 @@ class ZulipLocalizationsPl extends ZulipLocalizations {
   String get dialogClose => 'Zamknij';
 
   @override
-  String get errorDialogLearnMore => 'Learn more';
+  String get errorDialogLearnMore => 'Dowiedz się więcej';
 
   @override
   String get errorDialogContinue => 'OK';
@@ -418,7 +418,7 @@ class ZulipLocalizationsPl extends ZulipLocalizations {
 
   @override
   String errorServerVersionUnsupportedMessage(String url, String zulipVersion, String minSupportedZulipVersion) {
-    return '$url is running Zulip Server $zulipVersion, which is unsupported. The minimum supported version is Zulip Server $minSupportedZulipVersion.';
+    return '$url uruchamia Zulip Server $zulipVersion, który nie jest obsługiwany. Minimalna obsługiwana wersja to Zulip Server $minSupportedZulipVersion.';
   }
 
   @override
@@ -652,10 +652,10 @@ class ZulipLocalizationsPl extends ZulipLocalizations {
   String get pollWidgetOptionsMissing => 'Ta sonda nie ma opcji do wyboru.';
 
   @override
-  String get experimentalFeatureSettingsPageTitle => 'Experimental features';
+  String get experimentalFeatureSettingsPageTitle => 'Funkcje eksperymentalne';
 
   @override
-  String get experimentalFeatureSettingsWarning => 'These options enable features which are still under development and not ready. They may not work, and may cause issues in other areas of the app.\n\nThe purpose of these settings is for experimentation by people working on developing Zulip.';
+  String get experimentalFeatureSettingsWarning => 'W ten sposób aktywujesz funkcje, które są w fazie testów. Mogą one nie działać lub powodować problemy z tym co bez nich działa poprawnie.\n\nTo ustawienie przewidziane jest dla tych, którzy pracują nad ulepszeniem aplikacji Zulip.';
 
   @override
   String get errorNotificationOpenTitle => 'Otwieranie powiadomienia bez powodzenia';
