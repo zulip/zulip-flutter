a602c811e59c870b9879079dce5a4704d8d127ff
Zixuan James Li <zixuan@zulip.com>
2025-04-08 16:15:23 -0400

    msglist test [nfc]: Add MessageEvent test group

--- test/model/message_list_test.dart
+++ test/model/message_list_test.dart
@@ -360,37 +360,39 @@ void main() {
     });
   });
 
-  test('MessageEvent', () async {
-    final stream = eg.stream();
-    await prepare(narrow: ChannelNarrow(stream.streamId));
-    await prepareMessages(foundOldest: true, messages:
-      List.generate(30, (i) => eg.streamMessage(stream: stream)));
+  group('MessageEvent', () {
+    test('in narrow', () async {
+      final stream = eg.stream();
+      await prepare(narrow: ChannelNarrow(stream.streamId));
+      await prepareMessages(foundOldest: true, messages:
+        List.generate(30, (i) => eg.streamMessage(stream: stream)));
 
-    check(model).messages.length.equals(30);
-    await store.addMessage(eg.streamMessage(stream: stream));
-    checkNotifiedOnce();
-    check(model).messages.length.equals(31);
-  });
+      check(model).messages.length.equals(30);
+      await store.addMessage(eg.streamMessage(stream: stream));
+      checkNotifiedOnce();
+      check(model).messages.length.equals(31);
+    });
 
-  test('MessageEvent, not in narrow', () async {
-    final stream = eg.stream();
-    await prepare(narrow: ChannelNarrow(stream.streamId));
-    await prepareMessages(foundOldest: true, messages:
-      List.generate(30, (i) => eg.streamMessage(stream: stream)));
+    test('not in narrow', () async {
+      final stream = eg.stream();
+      await prepare(narrow: ChannelNarrow(stream.streamId));
+      await prepareMessages(foundOldest: true, messages:
+        List.generate(30, (i) => eg.streamMessage(stream: stream)));
 
-    check(model).messages.length.equals(30);
-    final otherStream = eg.stream();
-    await store.addMessage(eg.streamMessage(stream: otherStream));
-    checkNotNotified();
-    check(model).messages.length.equals(30);
-  });
+      check(model).messages.length.equals(30);
+      final otherStream = eg.stream();
+      await store.addMessage(eg.streamMessage(stream: otherStream));
+      checkNotNotified();
+      check(model).messages.length.equals(30);
+    });
 
-  test('MessageEvent, before fetch', () async {
-    final stream = eg.stream();
-    await prepare(narrow: ChannelNarrow(stream.streamId));
-    await store.addMessage(eg.streamMessage(stream: stream));
-    checkNotNotified();
-    check(model).fetched.isFalse();
+    test('before fetch', () async {
+      final stream = eg.stream();
+      await prepare(narrow: ChannelNarrow(stream.streamId));
+      await store.addMessage(eg.streamMessage(stream: stream));
+      checkNotNotified();
+      check(model).fetched.isFalse();
+    });
   });
 
   group('UserTopicEvent', () {
