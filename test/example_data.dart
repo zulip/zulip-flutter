b7cde83ebade9dfccff790281380dc475c21b3a1
Zixuan James Li <zixuan@zulip.com>
2025-04-22 13:38:07 -0400

    test [nfc]: Extract {dm,stream}OutboxMessage helpers

--- test/example_data.dart
+++ test/example_data.dart
@@ -12,6 +12,7 @@ import 'package:zulip/api/route/realm.dart';
 import 'package:zulip/api/route/channels.dart';
 import 'package:zulip/model/binding.dart';
 import 'package:zulip/model/database.dart';
+import 'package:zulip/model/message.dart';
 import 'package:zulip/model/narrow.dart';
 import 'package:zulip/model/settings.dart';
 import 'package:zulip/model/store.dart';
@@ -565,6 +566,44 @@ GetMessagesResult olderGetMessagesResult({
     historyLimited: historyLimited,
     messages: messages,
   );
+}
+
+int _nextLocalMessageId = 1;
+
+StreamOutboxMessage streamOutboxMessage({
+  int? localMessageId,
+  int? selfUserId,
+  int? timestamp,
+  ZulipStream? stream,
+  String? topic,
+  String? content,
+}) {
+  final effectiveStream = stream ?? _stream(streamId: defaultStreamMessageStreamId);
+  return StreamOutboxMessage(
+    localMessageId: localMessageId ?? _nextLocalMessageId++,
+    selfUserId: selfUserId ?? selfUser.userId,
+    timestamp: timestamp ?? utcTimestamp(),
+    conversation: StreamConversation(
+      effectiveStream.streamId, TopicName(topic ?? 'topic'),
+      displayRecipient: null,
+    ),
+    content: content ?? 'content');
+}
+
+DmOutboxMessage dmOutboxMessage({
+  int? localMessageId,
+  required User from,
+  required List<User> to,
+  int? timestamp,
+  String? content,
+}) {
+  final allRecipientIds = [from, ...to].map((user) => user.userId).toList();
+  return DmOutboxMessage(
+    localMessageId: localMessageId ?? _nextLocalMessageId++,
+    selfUserId: from.userId,
+    timestamp: timestamp ?? utcTimestamp(),
+    conversation: DmConversation(allRecipientIds: allRecipientIds),
+    content: content ?? 'content');
 }
 
 PollWidgetData pollWidgetData({
