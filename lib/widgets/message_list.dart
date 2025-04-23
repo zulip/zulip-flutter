c1f6b8dca6598499d5cf2c6909a6fe03d0506112
Zixuan James Li <zixuan@zulip.com>
2025-04-23 12:32:59 -0400

    msglist [nfc]: Make trailingWhitespace a constant
    This 11px whitespace can be traced back to 311d4d56e in 2022.
    
    While this is not present in the Figma design, it would be a
    good idea to refine it in the future.  See discussion:
      https://github.com/zulip/zulip-flutter/pull/1453#discussion_r2106526985

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -718,7 +718,6 @@ class _MessageListState extends State<MessageList> with PerAccountStoreAwareStat
         return MessageItem(
           key: ValueKey(data.message.id),
           header: header,
-          trailingWhitespace: 11,
           item: data);
     }
   }
@@ -1017,12 +1016,10 @@ class MessageItem extends StatelessWidget {
     super.key,
     required this.item,
     required this.header,
-    this.trailingWhitespace,
   });
 
   final MessageListMessageBaseItem item;
   final Widget header;
-  final double? trailingWhitespace;
 
   @override
   Widget build(BuildContext context) {
@@ -1035,7 +1032,9 @@ class MessageItem extends StatelessWidget {
         switch (item) {
           MessageListMessageItem() => MessageWithPossibleSender(item: item),
         },
-        if (trailingWhitespace != null && item.isLastInBlock) SizedBox(height: trailingWhitespace!),
+        // TODO refine this padding; discussion:
+        //   https://github.com/zulip/zulip-flutter/pull/1453#discussion_r2106526985
+        if (item.isLastInBlock) const SizedBox(height: 11),
       ]));
     if (item case MessageListMessageItem(:final message)) {
       child = _UnreadMarker(
