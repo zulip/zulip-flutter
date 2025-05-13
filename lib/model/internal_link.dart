a074719827511ef27d1ca432505d3c90f32abda3
Greg Price <greg@zulip.com>
2025-05-12 20:11:15 -0700

    wip parse /near/; TODO test

--- lib/model/internal_link.dart
+++ lib/model/internal_link.dart
@@ -120,9 +120,10 @@ sealed class InternalLink {
 /// The result of parsing some URL that points to a narrow on a Zulip realm,
 /// when the narrow is of a type that this app understands.
 class NarrowLink extends InternalLink {
-  NarrowLink(this.narrow, {required super.realmUrl});
+  NarrowLink(this.narrow, this.nearMessageId, {required super.realmUrl});
 
   final Narrow narrow;
+  final int? nearMessageId;
 }
 
 /// A [Narrow] from a given URL, on `store`'s realm.
@@ -180,6 +181,7 @@ NarrowLink? _interpretNarrowSegments(List<String> segments, PerAccountStore stor
   ApiNarrowDm? dmElement;
   ApiNarrowWith? withElement;
   Set<IsOperand> isElementOperands = {};
+  int? nearMessageId;
 
   for (var i = 0; i < segments.length; i += 2) {
     final (operator, negated) = _parseOperator(segments[i]);
@@ -217,8 +219,9 @@ NarrowLink? _interpretNarrowSegments(List<String> segments, PerAccountStore stor
         // It is fine to have duplicates of the same [IsOperand].
         isElementOperands.add(IsOperand.fromRawString(operand));
 
-      case _NarrowOperator.near: // TODO(#82): support for near
-        continue;
+      case _NarrowOperator.near:
+        if (nearMessageId != null) return null;
+        nearMessageId = int.tryParse(operand, radix: 10);
 
       case _NarrowOperator.unknown:
         return null;
@@ -260,7 +263,7 @@ NarrowLink? _interpretNarrowSegments(List<String> segments, PerAccountStore stor
     return null;
   }
 
-  return NarrowLink(narrow, realmUrl: store.realmUrl);
+  return NarrowLink(narrow, nearMessageId, realmUrl: store.realmUrl);
 }
 
 @JsonEnum(fieldRename: FieldRename.kebab, alwaysCreate: true)
