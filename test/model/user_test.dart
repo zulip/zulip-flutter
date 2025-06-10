d4d97113ff7f552d8854bea37f1bc79fecc35026
Chris Bobbe <cbobbe@zulip.com>
2025-06-09 21:17:03 -0700

    recent-dms: Exclude DM conversations where all other recipients are muted
    Co-authored-by: Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>

--- test/model/user_test.dart
+++ test/model/user_test.dart
@@ -2,6 +2,8 @@ import 'package:checks/checks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zulip/api/model/events.dart';
 import 'package:zulip/api/model/model.dart';
+import 'package:zulip/model/narrow.dart';
+import 'package:zulip/model/store.dart';
 
 import '../api/model/model_checks.dart';
 import '../example_data.dart' as eg;
@@ -81,25 +83,39 @@ void main() {
   });
 
   testWidgets('MutedUsersEvent', (tester) async {
+    late PerAccountStore store;
+
+    void checkDmConversationMuted(List<int> otherUserIds, bool expected) {
+      final narrow = DmNarrow.withOtherUsers(otherUserIds, selfUserId: store.selfUserId);
+      check(store.shouldMuteDmConversation(narrow)).equals(expected);
+    }
+
     final user1 = eg.user(userId: 1);
     final user2 = eg.user(userId: 2);
     final user3 = eg.user(userId: 3);
 
-    final store = eg.store(initialSnapshot: eg.initialSnapshot(
+    store = eg.store(initialSnapshot: eg.initialSnapshot(
       realmUsers: [user1, user2, user3],
       mutedUsers: [MutedUserItem(id: 2), MutedUserItem(id: 1)]));
     check(store.isUserMuted(1)).isTrue();
     check(store.isUserMuted(2)).isTrue();
     check(store.isUserMuted(3)).isFalse();
+    checkDmConversationMuted([1], true);
+    checkDmConversationMuted([1, 2], true);
+    checkDmConversationMuted([2, 3], false);
+    checkDmConversationMuted([1, 2, 3], false);
 
     await store.handleEvent(eg.mutedUsersEvent([2, 1, 3]));
     check(store.isUserMuted(1)).isTrue();
     check(store.isUserMuted(2)).isTrue();
     check(store.isUserMuted(3)).isTrue();
+    checkDmConversationMuted([1, 2, 3], true);
 
     await store.handleEvent(eg.mutedUsersEvent([2, 3]));
     check(store.isUserMuted(1)).isFalse();
     check(store.isUserMuted(2)).isTrue();
     check(store.isUserMuted(3)).isTrue();
+    checkDmConversationMuted([1], false);
+    checkDmConversationMuted([], false);
   });
 }
