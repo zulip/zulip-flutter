8f37237681d962b7836a60ad7b597b024a8ad378
Greg Price <greg@zulip.com>
2025-06-11 22:02:06 -0700

    content: Render KaTeX by default, as planned for the upcoming launch

--- lib/model/settings.dart
+++ lib/model/settings.dart
@@ -133,7 +133,7 @@ enum BoolGlobalSetting {
   placeholderIgnore(GlobalSettingType.placeholder, false),
 
   /// An experimental flag to toggle rendering KaTeX content in messages.
-  renderKatex(GlobalSettingType.experimentalFeatureFlag, false),
+  renderKatex(GlobalSettingType.experimentalFeatureFlag, true),
 
   /// An experimental flag to enable rendering KaTeX even when some
   /// errors are encountered.
