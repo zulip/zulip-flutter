5be0e70cee7a3c908b19475f3b1ea05bdd983dff
Greg Price <greg@zulip.com>
2025-06-15 13:52:52 -0700

    l10n: Add translatable strings from welcome dialog in v30.0.256 release

--- lib/generated/l10n/zulip_localizations_it.dart
+++ lib/generated/l10n/zulip_localizations_it.dart
@@ -21,6 +21,20 @@ class ZulipLocalizationsIt extends ZulipLocalizations {
   String get aboutPageTapToView => 'Tap per visualizzare';
 
   @override
+  String get upgradeWelcomeDialogTitle => 'Welcome to the new Zulip app!';
+
+  @override
+  String get upgradeWelcomeDialogMessage =>
+      'You’ll find a familiar experience in a faster, sleeker package.';
+
+  @override
+  String get upgradeWelcomeDialogLinkText =>
+      'Check out the announcement blog post!';
+
+  @override
+  String get upgradeWelcomeDialogDismiss => 'Let\'s go';
+
+  @override
   String get chooseAccountPageTitle => 'Scegli account';
 
   @override
