6b1f9af5efc69ea2f91dd6e8c1c1ae6eb1b83498
Greg Price <greg@zulip.com>
2025-07-06 14:04:28 -0700

    store test: Test handling of customProfileFields

--- test/model/store_test.dart
+++ test/model/store_test.dart
@@ -563,6 +563,48 @@ void main() {
     });
   });
 
+  group('PerAccountStore.customProfileFields', () {
+    test('update clobbers old list', () async {
+      final store = eg.store(initialSnapshot: eg.initialSnapshot(
+        customProfileFields: [
+          eg.customProfileField(0, CustomProfileFieldType.shortText),
+          eg.customProfileField(1, CustomProfileFieldType.shortText),
+        ]));
+      check(store.customProfileFields.map((f) => f.id)).deepEquals([0, 1]);
+
+      await store.handleEvent(CustomProfileFieldsEvent(id: 0, fields: [
+        eg.customProfileField(0, CustomProfileFieldType.shortText),
+        eg.customProfileField(2, CustomProfileFieldType.shortText),
+      ]));
+      check(store.customProfileFields.map((f) => f.id)).deepEquals([0, 2]);
+    });
+
+    test('sorts by displayInProfile', () async {
+      // Sorts both the data from the initial snapshot…
+      final store = eg.store(initialSnapshot: eg.initialSnapshot(
+        customProfileFields: [
+          eg.customProfileField(0, CustomProfileFieldType.shortText,
+            displayInProfileSummary: false),
+          eg.customProfileField(1, CustomProfileFieldType.shortText,
+            displayInProfileSummary: true),
+          eg.customProfileField(2, CustomProfileFieldType.shortText,
+            displayInProfileSummary: false),
+        ]));
+      check(store.customProfileFields.map((f) => f.id)).deepEquals([1, 0, 2]);
+
+      // … and from an event.
+      await store.handleEvent(CustomProfileFieldsEvent(id: 0, fields: [
+        eg.customProfileField(0, CustomProfileFieldType.shortText,
+          displayInProfileSummary: false),
+        eg.customProfileField(1, CustomProfileFieldType.shortText,
+          displayInProfileSummary: false),
+        eg.customProfileField(2, CustomProfileFieldType.shortText,
+          displayInProfileSummary: true),
+      ]));
+      check(store.customProfileFields.map((f) => f.id)).deepEquals([2, 0, 1]);
+    });
+  });
+
   group('PerAccountStore.handleEvent', () {
     // Mostly this method just dispatches to ChannelStore and MessageStore etc.,
     // and so its tests generally live in the test files for those
