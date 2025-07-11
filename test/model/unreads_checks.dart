d6b4e75c0e14758e59e15c5dae71cecb0e3f7fa1
Chris Bobbe <cbobbe@zulip.com>
2025-07-11 17:06:47 -0400

    unreads: Add/use locatorMap, to efficiently locate messages
    Fixes #332.
    
    Really the same optimization as described for zulip-mobile in
      https://github.com/zulip/zulip-mobile/issues/4684
    , and makes mark-as-read take 0ms (to the nearest ms) down from 3 or
    4, in my testing, and so fixes #332.

--- test/model/unreads_checks.dart
+++ test/model/unreads_checks.dart
@@ -5,6 +5,7 @@ import 'package:zulip/model/narrow.dart';
 import 'package:zulip/model/unreads.dart';
 
 extension UnreadsChecks on Subject<Unreads> {
+  Subject<Map<int, SendableNarrow>> get locatorMap => has((u) => u.locatorMap, 'locatorMap');
   Subject<Map<int, Map<TopicName, QueueList<int>>>> get streams => has((u) => u.streams, 'streams');
   Subject<Map<DmNarrow, QueueList<int>>> get dms => has((u) => u.dms, 'dms');
   Subject<Set<int>> get mentions => has((u) => u.mentions, 'mentions');
