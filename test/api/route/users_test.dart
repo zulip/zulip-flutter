94687675ff4e223c14e56a8f9cb62fad6355c148
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2025-07-19 00:23:35 +0430

    api: Add route updateStatus

--- test/api/route/users_test.dart
+++ test/api/route/users_test.dart
@@ -3,11 +3,31 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zulip/api/model/model.dart';
 import 'package:zulip/api/route/users.dart';
+import 'package:zulip/basic.dart';
 
 import '../../stdlib_checks.dart';
 import '../fake_api.dart';
 
 void main() {
+  test('smoke updateStatus', () {
+    return FakeApiConnection.with_((connection) async {
+      connection.prepare(json: {});
+      await updateStatus(connection, change: UserStatusChange(
+        text: OptionSome('Busy'),
+        emoji: OptionSome(StatusEmoji(emojiName: 'working_on_it',
+          emojiCode: '1f6e0', reactionType: ReactionType.unicodeEmoji))));
+      check(connection.takeRequests()).single.isA<http.Request>()
+        ..method.equals('POST')
+        ..url.path.equals('/api/v1/users/me/status')
+        ..bodyFields.deepEquals({
+          'status_text': 'Busy',
+          'emoji_name': 'working_on_it',
+          'emoji_code': '1f6e0',
+          'reaction_type': 'unicode_emoji',
+        });
+    });
+  });
+
   test('smoke updatePresence', () {
     return FakeApiConnection.with_((connection) async {
       final response = UpdatePresenceResult(
