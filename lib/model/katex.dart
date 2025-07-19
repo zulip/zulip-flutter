16deb86bb1b05495b372c8264132084182acecd3
Greg Price <greg@zulip.com>
2025-07-18 18:50:09 -0700

    katex [nfc]: Cut stray import of widgets library
    This was here only for a reference in a doc.  In general we try to
    avoid imports of widgets from model code; it's an inversion of layers.

--- lib/model/katex.dart
+++ lib/model/katex.dart
@@ -2,7 +2,6 @@ import 'package:collection/collection.dart';
 import 'package:csslib/parser.dart' as css_parser;
 import 'package:csslib/visitor.dart' as css_visitor;
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:html/dom.dart' as dom;
 
 import '../log.dart';
