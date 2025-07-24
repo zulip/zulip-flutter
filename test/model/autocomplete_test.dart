527e8be9463777b149d2c76d2720bc81568df123
Greg Price <greg@zulip.com>
2025-07-24 14:10:37 -0700

    autocomplete test [nfc]: Reorder mention sort/match tests to match impl

--- test/model/autocomplete_test.dart
+++ test/model/autocomplete_test.dart
@@ -403,66 +403,6 @@ void main() {
       ..not((results) => results.contains(11000));
   });
 
-  group('MentionAutocompleteQuery.testUser', () {
-    late PerAccountStore store;
-
-    void doCheck(String rawQuery, User user, bool expected) {
-      final result = MentionAutocompleteQuery(rawQuery).testUser(user, store);
-      expected
-        ? check(result).isA<UserMentionAutocompleteResult>()
-        : check(result).isNull();
-    }
-
-    test('user is always excluded when not active regardless of other criteria', () {
-      store = eg.store();
-
-      doCheck('Full Name', eg.user(fullName: 'Full Name', isActive: false), false);
-      // When active then other criteria will be checked
-      doCheck('Full Name', eg.user(fullName: 'Full Name', isActive: true), true);
-    });
-
-    test('user is always excluded when muted, regardless of other criteria', () async {
-      store = eg.store();
-      await store.setMutedUsers([1]);
-      doCheck('Full Name', eg.user(userId: 1, fullName: 'Full Name'), false);
-      // When not muted, then other criteria will be checked
-      doCheck('Full Name', eg.user(userId: 2, fullName: 'Full Name'), true);
-    });
-
-    test('user is included if fullname words match the query', () {
-      store = eg.store();
-
-      doCheck('', eg.user(fullName: 'Full Name'), true);
-      doCheck('', eg.user(fullName: ''), true); // Unlikely case, but should not crash
-      doCheck('Full Name', eg.user(fullName: 'Full Name'), true);
-      doCheck('full name', eg.user(fullName: 'Full Name'), true);
-      doCheck('Full Name', eg.user(fullName: 'full name'), true);
-      doCheck('Full', eg.user(fullName: 'Full Name'), true);
-      doCheck('Name', eg.user(fullName: 'Full Name'), true);
-      doCheck('Full Name', eg.user(fullName: 'Fully Named'), true);
-      doCheck('Full Four', eg.user(fullName: 'Full Name Four Words'), true);
-      doCheck('Name Words', eg.user(fullName: 'Full Name Four Words'), true);
-      doCheck('Full F', eg.user(fullName: 'Full Name Four Words'), true);
-      doCheck('F Four', eg.user(fullName: 'Full Name Four Words'), true);
-      doCheck('full full', eg.user(fullName: 'Full Full Name'), true);
-      doCheck('full full', eg.user(fullName: 'Full Name Full'), true);
-
-      doCheck('F', eg.user(fullName: ''), false); // Unlikely case, but should not crash
-      doCheck('Fully Named', eg.user(fullName: 'Full Name'), false);
-      doCheck('Full Name', eg.user(fullName: 'Full'), false);
-      doCheck('Full Name', eg.user(fullName: 'Name'), false);
-      doCheck('ull ame', eg.user(fullName: 'Full Name'), false);
-      doCheck('ull Name', eg.user(fullName: 'Full Name'), false);
-      doCheck('Full ame', eg.user(fullName: 'Full Name'), false);
-      doCheck('Full Full', eg.user(fullName: 'Full Name'), false);
-      doCheck('Name Name', eg.user(fullName: 'Full Name'), false);
-      doCheck('Name Full', eg.user(fullName: 'Full Name'), false);
-      doCheck('Name Four Full Words', eg.user(fullName: 'Full Name Four Words'), false);
-      doCheck('F Full', eg.user(fullName: 'Full Name Four Words'), false);
-      doCheck('Four F', eg.user(fullName: 'Full Name Four Words'), false);
-    });
-  });
-
   group('MentionAutocompleteView sorting users results', () {
     late PerAccountStore store;
 
@@ -953,6 +893,66 @@ void main() {
       check(getWildcardOptionsFor('topic',
           narrow: channelNarrow, zulipFeatureLevel: 223))
         .deepEquals([]);
+    });
+  });
+
+  group('MentionAutocompleteQuery.testUser', () {
+    late PerAccountStore store;
+
+    void doCheck(String rawQuery, User user, bool expected) {
+      final result = MentionAutocompleteQuery(rawQuery).testUser(user, store);
+      expected
+        ? check(result).isA<UserMentionAutocompleteResult>()
+        : check(result).isNull();
+    }
+
+    test('user is always excluded when not active regardless of other criteria', () {
+      store = eg.store();
+
+      doCheck('Full Name', eg.user(fullName: 'Full Name', isActive: false), false);
+      // When active then other criteria will be checked
+      doCheck('Full Name', eg.user(fullName: 'Full Name', isActive: true), true);
+    });
+
+    test('user is always excluded when muted, regardless of other criteria', () async {
+      store = eg.store();
+      await store.setMutedUsers([1]);
+      doCheck('Full Name', eg.user(userId: 1, fullName: 'Full Name'), false);
+      // When not muted, then other criteria will be checked
+      doCheck('Full Name', eg.user(userId: 2, fullName: 'Full Name'), true);
+    });
+
+    test('user is included if fullname words match the query', () {
+      store = eg.store();
+
+      doCheck('', eg.user(fullName: 'Full Name'), true);
+      doCheck('', eg.user(fullName: ''), true); // Unlikely case, but should not crash
+      doCheck('Full Name', eg.user(fullName: 'Full Name'), true);
+      doCheck('full name', eg.user(fullName: 'Full Name'), true);
+      doCheck('Full Name', eg.user(fullName: 'full name'), true);
+      doCheck('Full', eg.user(fullName: 'Full Name'), true);
+      doCheck('Name', eg.user(fullName: 'Full Name'), true);
+      doCheck('Full Name', eg.user(fullName: 'Fully Named'), true);
+      doCheck('Full Four', eg.user(fullName: 'Full Name Four Words'), true);
+      doCheck('Name Words', eg.user(fullName: 'Full Name Four Words'), true);
+      doCheck('Full F', eg.user(fullName: 'Full Name Four Words'), true);
+      doCheck('F Four', eg.user(fullName: 'Full Name Four Words'), true);
+      doCheck('full full', eg.user(fullName: 'Full Full Name'), true);
+      doCheck('full full', eg.user(fullName: 'Full Name Full'), true);
+
+      doCheck('F', eg.user(fullName: ''), false); // Unlikely case, but should not crash
+      doCheck('Fully Named', eg.user(fullName: 'Full Name'), false);
+      doCheck('Full Name', eg.user(fullName: 'Full'), false);
+      doCheck('Full Name', eg.user(fullName: 'Name'), false);
+      doCheck('ull ame', eg.user(fullName: 'Full Name'), false);
+      doCheck('ull Name', eg.user(fullName: 'Full Name'), false);
+      doCheck('Full ame', eg.user(fullName: 'Full Name'), false);
+      doCheck('Full Full', eg.user(fullName: 'Full Name'), false);
+      doCheck('Name Name', eg.user(fullName: 'Full Name'), false);
+      doCheck('Name Full', eg.user(fullName: 'Full Name'), false);
+      doCheck('Name Four Full Words', eg.user(fullName: 'Full Name Four Words'), false);
+      doCheck('F Full', eg.user(fullName: 'Full Name Four Words'), false);
+      doCheck('Four F', eg.user(fullName: 'Full Name Four Words'), false);
     });
   });
 
