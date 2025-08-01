9b1a7416e74fbacd0aa22e30c139fc3f9013c6dd
Rajesh Malviya <rmalviya@zulip.com>
2025-08-01 09:04:17 +0530

    subscription_list [nfc]: Add a flag to disable channel action sheet
    This will be used soon to avoid unintended flows when sharing content
    received from other apps.

--- lib/widgets/subscription_list.dart
+++ lib/widgets/subscription_list.dart
@@ -15,7 +15,12 @@ import 'unread_count_badge.dart';
 
 /// Scrollable listing of subscribed streams.
 class SubscriptionListPageBody extends StatefulWidget {
-  const SubscriptionListPageBody({super.key});
+  const SubscriptionListPageBody({
+    super.key,
+    this.disableChannelActionSheet = false,
+  });
+
+  final bool disableChannelActionSheet;
 
   @override
   State<SubscriptionListPageBody> createState() => _SubscriptionListPageBodyState();
@@ -106,11 +111,17 @@ class _SubscriptionListPageBodyState extends State<SubscriptionListPageBody> wit
         slivers: [
           if (pinned.isNotEmpty) ...[
             _SubscriptionListHeader(label: zulipLocalizations.pinnedSubscriptionsLabel),
-            _SubscriptionList(unreadsModel: unreadsModel, subscriptions: pinned),
+            _SubscriptionList(
+              unreadsModel: unreadsModel,
+              subscriptions: pinned,
+              disableChannelActionSheet: widget.disableChannelActionSheet),
           ],
           if (unpinned.isNotEmpty) ...[
             _SubscriptionListHeader(label: zulipLocalizations.unpinnedSubscriptionsLabel),
-            _SubscriptionList(unreadsModel: unreadsModel, subscriptions: unpinned),
+            _SubscriptionList(
+              unreadsModel: unreadsModel,
+              subscriptions: unpinned,
+              disableChannelActionSheet: widget.disableChannelActionSheet),
           ],
 
           // TODO(#188): add button leading to "All Streams" page with ability to subscribe
@@ -160,10 +171,12 @@ class _SubscriptionList extends StatelessWidget {
   const _SubscriptionList({
     required this.unreadsModel,
     required this.subscriptions,
+    required this.disableChannelActionSheet,
   });
 
   final Unreads? unreadsModel;
   final List<Subscription> subscriptions;
+  final bool disableChannelActionSheet;
 
   @override
   Widget build(BuildContext context) {
@@ -176,7 +189,8 @@ class _SubscriptionList extends StatelessWidget {
           && unreadsModel!.countInChannelNarrow(subscription.streamId) > 0;
         return SubscriptionItem(subscription: subscription,
           unreadCount: unreadCount,
-          showMutedUnreadBadge: showMutedUnreadBadge);
+          showMutedUnreadBadge: showMutedUnreadBadge,
+          disableChannelActionSheet: disableChannelActionSheet);
     });
   }
 }
@@ -188,11 +202,13 @@ class SubscriptionItem extends StatelessWidget {
     required this.subscription,
     required this.unreadCount,
     required this.showMutedUnreadBadge,
+    required this.disableChannelActionSheet,
   });
 
   final Subscription subscription;
   final int unreadCount;
   final bool showMutedUnreadBadge;
+  final bool disableChannelActionSheet;
 
   @override
   Widget build(BuildContext context) {
@@ -210,7 +226,9 @@ class SubscriptionItem extends StatelessWidget {
             MessageListPage.buildRoute(context: context,
               narrow: ChannelNarrow(subscription.streamId)));
         },
-        onLongPress: () => showChannelActionSheet(context, channelId: subscription.streamId),
+        onLongPress: !disableChannelActionSheet
+          ? () => showChannelActionSheet(context, channelId: subscription.streamId)
+          : null,
         child: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
           const SizedBox(width: 16),
           Padding(
