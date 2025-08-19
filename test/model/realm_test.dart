1550173ab9f0d1eb457240c6231676a07f64b6f4
Greg Price <greg@zulip.com>
2025-08-19 16:10:21 -0700

    realm test [nfc]: Reorder tests to match implementation

--- test/model/realm_test.dart
+++ test/model/realm_test.dart
@@ -6,6 +6,34 @@ import 'package:zulip/api/model/model.dart';
 import '../example_data.dart' as eg;
 
 void main() {
+  test('processTopicLikeServer', () {
+    final emptyTopicDisplayName = eg.defaultRealmEmptyTopicDisplayName;
+
+    TopicName process(TopicName topic, int zulipFeatureLevel) {
+      final account = eg.selfAccount.copyWith(zulipFeatureLevel: zulipFeatureLevel);
+      final store = eg.store(account: account, initialSnapshot: eg.initialSnapshot(
+        zulipFeatureLevel: zulipFeatureLevel,
+        realmEmptyTopicDisplayName: emptyTopicDisplayName));
+      return store.processTopicLikeServer(topic);
+    }
+
+    void doCheck(TopicName topic, TopicName expected, int zulipFeatureLevel) {
+      check(process(topic, zulipFeatureLevel)).equals(expected);
+    }
+
+    check(() => process(eg.t(''), 333)).throws<void>();
+    doCheck(eg.t('(no topic)'),          eg.t('(no topic)'),          333);
+    doCheck(eg.t(emptyTopicDisplayName), eg.t(emptyTopicDisplayName), 333);
+    doCheck(eg.t('other topic'),         eg.t('other topic'),         333);
+
+    doCheck(eg.t(''),                    eg.t(''),                    334);
+    doCheck(eg.t('(no topic)'),          eg.t('(no topic)'),          334);
+    doCheck(eg.t(emptyTopicDisplayName), eg.t(''),                    334);
+    doCheck(eg.t('other topic'),         eg.t('other topic'),         334);
+
+    doCheck(eg.t('(no topic)'),          eg.t(''),                    370);
+  });
+
   group('customProfileFields', () {
     test('update clobbers old list', () async {
       final store = eg.store(initialSnapshot: eg.initialSnapshot(
@@ -46,33 +74,5 @@ void main() {
       ]));
       check(store.customProfileFields.map((f) => f.id)).deepEquals([2, 0, 1]);
     });
-  });
-
-  test('processTopicLikeServer', () {
-    final emptyTopicDisplayName = eg.defaultRealmEmptyTopicDisplayName;
-
-    TopicName process(TopicName topic, int zulipFeatureLevel) {
-      final account = eg.selfAccount.copyWith(zulipFeatureLevel: zulipFeatureLevel);
-      final store = eg.store(account: account, initialSnapshot: eg.initialSnapshot(
-        zulipFeatureLevel: zulipFeatureLevel,
-        realmEmptyTopicDisplayName: emptyTopicDisplayName));
-      return store.processTopicLikeServer(topic);
-    }
-
-    void doCheck(TopicName topic, TopicName expected, int zulipFeatureLevel) {
-      check(process(topic, zulipFeatureLevel)).equals(expected);
-    }
-
-    check(() => process(eg.t(''), 333)).throws<void>();
-    doCheck(eg.t('(no topic)'),          eg.t('(no topic)'),          333);
-    doCheck(eg.t(emptyTopicDisplayName), eg.t(emptyTopicDisplayName), 333);
-    doCheck(eg.t('other topic'),         eg.t('other topic'),         333);
-
-    doCheck(eg.t(''),                    eg.t(''),                    334);
-    doCheck(eg.t('(no topic)'),          eg.t('(no topic)'),          334);
-    doCheck(eg.t(emptyTopicDisplayName), eg.t(''),                    334);
-    doCheck(eg.t('other topic'),         eg.t('other topic'),         334);
-
-    doCheck(eg.t('(no topic)'),          eg.t(''),                    370);
   });
 }
