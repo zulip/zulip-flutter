f41c44d88c5631ce7e5e197f902dbb811b1c168d
Greg Price <greg@zulip.com>
2025-09-22 13:01:46 -0700

    api: Add route registerPushDevice

---
+++ lib/api/route/notifications.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: constant_identifier_names, unnecessary_cast
+
+part of 'notifications.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$PushRegistrationToJson(PushRegistration instance) =>
+    <String, dynamic>{
+      'token_kind': instance.tokenKind,
+      'token': instance.token,
+      'timestamp': instance.timestamp,
+    };
+
+const _$PushTokenKindEnumMap = {
+  PushTokenKind.fcm: 'fcm',
+  PushTokenKind.apns: 'apns',
+};
