3ed8e3ce616ed193d533e3aad906fe1ff366ee7f
Vinod Singh <vinodsingh.dev.2025@gmail.com>
2025-10-03 19:31:19 +0530

    avatar: Show placeholder on image load error

--- test/widgets/user_test.dart
+++ test/widgets/user_test.dart
@@ -1,26 +1,19 @@
 import 'package:checks/checks.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:zulip/widgets/icons.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zulip/model/store.dart';
 import 'package:zulip/widgets/content.dart';
+import 'package:zulip/widgets/icons.dart';
 import 'package:zulip/widgets/store.dart';
 import 'package:zulip/widgets/user.dart';
-import 'dart:io';
 import '../example_data.dart' as eg;
 import '../model/binding.dart';
 import '../model/test_store.dart';
 import '../stdlib_checks.dart';
 import '../test_images.dart';
 import 'test_app.dart';
-
-class MockHttpClient extends Fake implements HttpClient {
-  @override
-  Future<HttpClientRequest> getUrl(Uri url) async {
-    throw const SocketException('test error');
-  }
-}
 
 void main() {
   TestZulipBinding.ensureInitialized();
@@ -86,25 +79,26 @@ void main() {
       check(await actualUrl(tester, avatarUrl)).isNull();
       debugNetworkImageHttpClientProvider = null;
     });
-
     testWidgets('shows placeholder when image URL gives error', (WidgetTester tester) async {
-      await HttpOverrides.runZoned(() async {
-        addTearDown(testBinding.reset);
-        await testBinding.globalStore.add(eg.selfAccount, eg.initialSnapshot());
-        final store = await testBinding.globalStore.perAccount(eg.selfAccount.id);
-        final user = eg.user(avatarUrl: 'https://zulip.com/avatar.png');
-        await store.addUser(user);
-
-        await tester.pumpWidget(TestZulipApp(
+      addTearDown(testBinding.reset);
+      prepareBoringImageHttpClient(success: false);
+      await testBinding.globalStore.add(eg.selfAccount, eg.initialSnapshot());
+      final store = await testBinding.globalStore.perAccount(eg.selfAccount.id);
+      final badUser = eg.user(avatarUrl: 'https://zulip.com/avatarinvalid.png');
+      await store.addUser(badUser);
+      await tester.pumpWidget(TestZulipApp(
+        child: PerAccountStoreWidget(
           accountId: eg.selfAccount.id,
-          child: AvatarImage(userId: user.userId, size: 32)));
-        await tester.pump(); // Image provider is created
-        await tester.pump(); // Image fails to load
-        expect(find.byIcon(ZulipIcons.person), findsOneWidget);
-
-        expect(find.byType(DecoratedBox), findsOneWidget);
-
-      }, createHttpClient: (context) => MockHttpClient());
+          child: AvatarImage(userId: badUser.userId, size: 30))));
+      await tester.pumpAndSettle();
+      expect(
+        find.descendant(
+          of: find.byType(AvatarImage),
+          matching: find.byIcon(ZulipIcons.person),
+        ),
+        findsOneWidget,
+      );
+  debugNetworkImageHttpClientProvider = null;
     });
   });
 }
