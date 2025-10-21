8a6afdf6beaa777ce88a15540ee9a21bfae8e79b
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2025-10-21 20:35:55 +0430

    autocomplete test: Make setupToComposeInput accept `channels` param

--- test/widgets/autocomplete_test.dart
+++ test/widgets/autocomplete_test.dart
@@ -32,7 +32,7 @@ late PerAccountStore store;
 
 /// Simulates loading a [MessageListPage] and tapping to focus the compose input.
 ///
-/// Also adds [users] to the [PerAccountStore],
+/// Also adds [users] and [channels] to the [PerAccountStore],
 /// so they can show up in autocomplete.
 ///
 /// Also sets [debugNetworkImageHttpClientProvider] to return a constant image.
@@ -41,6 +41,7 @@ late PerAccountStore store;
 /// before the end of the test.
 Future<Finder> setupToComposeInput(WidgetTester tester, {
   List<User> users = const [],
+  List<ZulipStream> channels = const [],
   Narrow? narrow,
 }) async {
   assert(narrow is ChannelNarrow? || narrow is SendableNarrow?);
@@ -52,6 +53,7 @@ Future<Finder> setupToComposeInput(WidgetTester tester, {
   store = await testBinding.globalStore.perAccount(eg.selfAccount.id);
   await store.addUsers([eg.selfUser, eg.otherUser]);
   await store.addUsers(users);
+  await store.addStreams(channels);
   final connection = store.connection as FakeApiConnection;
 
   narrow ??= DmNarrow(
