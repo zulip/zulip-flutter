36229c74e4867bab9aa382bab23b8a5965f5223f
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2025-11-03 13:40:31 +0430

    compose: Introduce `fallbackMarkdownLink` function

--- test/model/compose_test.dart
+++ test/model/compose_test.dart
@@ -1,6 +1,7 @@
 import 'package:checks/checks.dart';
 import 'package:test/scaffolding.dart';
 import 'package:zulip/api/model/events.dart';
+import 'package:zulip/api/model/model.dart';
 import 'package:zulip/model/compose.dart';
 import 'package:zulip/model/localizations.dart';
 import 'package:zulip/model/store.dart';
@@ -332,6 +333,38 @@ hello
           .equals('@_*Group Name*');
       });
     });
+  });
+
+  test('fallbackMarkdownLink', () async {
+    final store = eg.store();
+    final channels = [
+      eg.stream(streamId: 1, name: '`code`'),
+      eg.stream(streamId: 2, name: 'score > 90'),
+      eg.stream(streamId: 3, name: 'A*'),
+      eg.stream(streamId: 4, name: 'R&D'),
+      eg.stream(streamId: 5, name: 'UI [v2]'),
+      eg.stream(streamId: 6, name: r'Save $$'),
+    ];
+    await store.addStreams(channels);
+
+    check(fallbackMarkdownLink(store: store,
+        channel: channels[1 - 1]))
+      .equals('[#&#96;code&#96;](#narrow/channel/1-.60code.60)');
+    check(fallbackMarkdownLink(store: store,
+        channel: channels[2 - 1], topic: TopicName('topic')))
+      .equals('[#score &gt; 90 > topic](#narrow/channel/2-score-.3E-90/topic/topic)');
+    check(fallbackMarkdownLink(store: store,
+        channel: channels[3 - 1], topic: TopicName('R&D')))
+      .equals('[#A&#42; > R&amp;D](#narrow/channel/3-A*/topic/R.26D)');
+    check(fallbackMarkdownLink(store: store,
+        channel: channels[4 - 1], topic: TopicName('topic'), nearMessageId: 10))
+      .equals('[#R&amp;D > topic @ 💬](#narrow/channel/4-R.26D/topic/topic/near/10)');
+    check(fallbackMarkdownLink(store: store,
+        channel: channels[5 - 1], topic: TopicName(r'Save $$'), nearMessageId: 10))
+      .equals('[#UI &#91;v2&#93; > Save &#36;&#36; @ 💬](#narrow/channel/5-UI-.5Bv2.5D/topic/Save.20.24.24/near/10)');
+    check(() => fallbackMarkdownLink(store: store,
+        channel: channels[6 - 1], nearMessageId: 10))
+      .throws<AssertionError>();
   });
 
   test('inlineLink', () {
