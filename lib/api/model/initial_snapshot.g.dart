43ae6967f8bdabe7a0c941bfbc5779e87e2d2ec8
MritunjayTiwari14 <shreyashtiwari14.11@gmail.com>
2025-11-23 11:04:18 +0530

    api: Add realmVideoChatProvider to InitialSnapshot

--- lib/api/model/initial_snapshot.g.dart
+++ lib/api/model/initial_snapshot.g.dart
@@ -104,6 +104,10 @@ InitialSnapshot _$InitialSnapshotFromJson(
   ),
   realmMandatoryTopics: json['realm_mandatory_topics'] as bool,
   realmName: json['realm_name'] as String,
+  realmVideoChatProvider: $enumDecode(
+    _$RealmVideoChatProviderEnumMap,
+    json['realm_video_chat_provider'],
+  ),
   realmWaitingPeriodThreshold: (json['realm_waiting_period_threshold'] as num)
       .toInt(),
   realmMessageContentDeleteLimitSeconds:
@@ -185,6 +189,7 @@ Map<String, dynamic> _$InitialSnapshotToJson(
   'realm_wildcard_mention_policy': instance.realmWildcardMentionPolicy,
   'realm_mandatory_topics': instance.realmMandatoryTopics,
   'realm_name': instance.realmName,
+  'realm_video_chat_provider': instance.realmVideoChatProvider,
   'realm_waiting_period_threshold': instance.realmWaitingPeriodThreshold,
   'realm_message_content_delete_limit_seconds':
       instance.realmMessageContentDeleteLimitSeconds,
@@ -218,6 +223,15 @@ const _$RealmWildcardMentionPolicyEnumMap = {
   RealmWildcardMentionPolicy.admins: 5,
   RealmWildcardMentionPolicy.nobody: 6,
   RealmWildcardMentionPolicy.moderators: 7,
+};
+
+const _$RealmVideoChatProviderEnumMap = {
+  RealmVideoChatProvider.none: 0,
+  RealmVideoChatProvider.jitsiMeet: 1,
+  RealmVideoChatProvider.zoomUserOAuth: 3,
+  RealmVideoChatProvider.bigBlueButton: 4,
+  RealmVideoChatProvider.zoomServerToServerOAuth: 5,
+  RealmVideoChatProvider.unknown: null,
 };
 
 RealmDefaultExternalAccount _$RealmDefaultExternalAccountFromJson(
