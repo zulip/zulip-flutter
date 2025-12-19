121e1de2ae0c260366729b4f4c5f2c97d25ec7ff
MritunjayTiwari14 <shreyashtiwari14.11@gmail.com>
2025-12-19 18:31:32 +0530

    compose_box: Add `Add video call` button
    Fixes #1824.
    
    Generation and Insertion of url is based on
    similar logic as used in web.

--- lib/generated/l10n/zulip_localizations_ja.dart
+++ lib/generated/l10n/zulip_localizations_ja.dart
@@ -399,6 +399,9 @@ class ZulipLocalizationsJa extends ZulipLocalizations {
   String get errorCouldNotEditMessageTitle => 'メッセージを編集できませんでした';
 
   @override
+  String get errorCouldNotAppendCallUrl => 'Fail to get call URL';
+
+  @override
   String get successLinkCopied => 'リンクをコピーしました';
 
   @override
@@ -474,6 +477,12 @@ class ZulipLocalizationsJa extends ZulipLocalizations {
   String get composeBoxAttachFromCameraTooltip => '写真を撮る';
 
   @override
+  String get composeBoxAddVideoCallTooltip => 'Add video call';
+
+  @override
+  String get composeBoxAddVoiceCallTooltip => 'Add voice call';
+
+  @override
   String get composeBoxGenericContentHint => 'メッセージを入力';
 
   @override
@@ -531,6 +540,12 @@ class ZulipLocalizationsJa extends ZulipLocalizations {
   String composeBoxUploadingFilename(String filename) {
     return '$filename をアップロード中…';
   }
+
+  @override
+  String get composeBoxVideoCallLinkText => 'Join video call.';
+
+  @override
+  String get composeBoxVoiceCallLinkText => 'Join voice call.';
 
   @override
   String composeBoxLoadingMessage(int messageId) {
