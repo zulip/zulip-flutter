ac29f047da13fd425c96651559da764626cdff67
Sarafaraj Nasardi <sarafarajnasardi786@gmail.com>
2026-01-28 07:17:49 +0530

    fix: Search Box styling to match figma design

--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -17,35 +17,33 @@ class SearchBox extends StatelessWidget {
   Widget build(BuildContext context) {
     final designVariables = DesignVariables.of(context);
 
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 8, 16, 4),
-      child: TextField(
-        controller: controller,
-        autocorrect: false,
-        cursorColor: designVariables.textInput,
-        style: TextStyle(
-          color: designVariables.textInput,
+    return TextField(
+      controller: controller,
+      autocorrect: false,
+      cursorColor: designVariables.textInput,
+      style: TextStyle(
+        color: designVariables.textInput,
+        fontSize: 17,
+        height: 22 / 17,
+      ),
+      decoration: InputDecoration(
+        isDense: true,
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: designVariables.labelSearchPrompt,
           fontSize: 17,
-          height: 22 / 17,
-        ),
-        decoration: InputDecoration(
-          isDense: true,
-          hintText: hintText,
-          hintStyle: TextStyle(
-            color: designVariables.labelSearchPrompt,
-            fontSize: 17,
-            height: 22 / 17),
-          prefixIcon: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 8, 8),
-            child: Icon(size: 20, ZulipIcons.search)),
-          prefixIconColor: designVariables.labelSearchPrompt,
-          prefixIconConstraints: const BoxConstraints(),
-          contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-          filled: true,
-          fillColor: designVariables.bgSearchInput,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide.none),
-        )));
+          height: 22 / 17),
+        prefixIcon: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(12, 8, 8, 8),
+          child: Icon(size: 20, ZulipIcons.search)),
+        prefixIconColor: designVariables.labelSearchPrompt,
+        prefixIconConstraints: const BoxConstraints(),
+        contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+        filled: true,
+        fillColor: designVariables.bgSearchInput,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.zero,
+          borderSide: BorderSide.none),
+      ));
   }
 }
