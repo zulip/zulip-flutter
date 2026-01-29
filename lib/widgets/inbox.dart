8670349b5d015b0345541f6947ea351ea582791d
Chris Bobbe <cbobbe@zulip.com>
2026-01-28 22:54:34 -0800

    inbox: Make "@" and topic-visibility icons larger and text-scalable (to 1.5x)
    Fixes #2108.
    
    The 16px size matches the Figma and this icon in the topic list page.

--- lib/widgets/inbox.dart
+++ lib/widgets/inbox.dart
@@ -587,12 +587,16 @@ class _IconMarker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final designVariables = DesignVariables.of(context);
+    final textScaler = MediaQuery.textScalerOf(context);
     // Design for icon markers based on Figma screen:
     //   https://www.figma.com/file/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?type=design&node-id=224-16386&mode=design&t=JsNndFQ8fKFH0SjS-0
     return Padding(
       padding: const EdgeInsetsDirectional.only(end: 4),
       // This color comes from the Figma screen for the "@" marker, but not
       // the topic visibility markers.
-      child: Icon(icon, size: 14, color: designVariables.inboxItemIconMarker));
+      child: Icon(
+        size: textScaler.clamp(maxScaleFactor: 1.5).scale(16),
+        color: designVariables.inboxItemIconMarker,
+        icon));
   }
 }
