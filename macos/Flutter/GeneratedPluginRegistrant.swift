97d13760aeb0bfa4029dcae8dcfa6e0d9e9db0c5
Rajesh Malviya <rmalviya@zulip.com>
2026-02-10 17:08:38 +0530

    deps: Upgrade path_provider_foundation to latest
    This commit is the result of the following commands:
      flutter pub upgrade path_provider_foundation
      pod update --project-directory=ios/
      pod update --project-directory=macos/
    
    Changelog:
      https://pub.dev/packages/path_provider_foundation/changelog#260
    
    It now calls to Foundation APIs directly using FFI instead of
    Pigeon/MethodChannels.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import file_selector_macos
 import firebase_core
 import firebase_messaging
 import package_info_plus
-import path_provider_foundation
 import share_plus
 import sqlite3_flutter_libs
 import url_launcher_macos
@@ -27,7 +26,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
