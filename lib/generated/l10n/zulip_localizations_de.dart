5644150cbbcab9118f269b1609bc5928bf1c6d29
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2026-02-12 23:34:36 +0430

    autocomplete: Finish topic link autocomplete for compose box
    Fixes #124.
    
    Adapted from the Figma design and the web app design:
      https://www.figma.com/design/1JTNtYo9memgW7vV6d0ygq/Zulip-Mobile?node-id=7953-30398&t=IeFc4HZDSNwrfGkU-0
      https://chat.zulip.org/#narrow/channel/530-mobile-design/topic/topic.20link.20autocomplete/near/2370602

--- lib/generated/l10n/zulip_localizations_de.dart
+++ lib/generated/l10n/zulip_localizations_de.dart
@@ -1312,6 +1312,12 @@ class ZulipLocalizationsDe extends ZulipLocalizations {
   String get wildcardMentionTopicDescription => 'Thema benachrichtigen';
 
   @override
+  String get topicAutocompleteChannelOptionLabel => '(link to channel)';
+
+  @override
+  String get topicAutocompleteNewOptionLabel => 'New';
+
+  @override
   String get systemGroupNameEveryoneOnInternet => 'Jeder im Internet';
 
   @override
