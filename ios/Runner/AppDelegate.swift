e462c5bb5fbd44918a1e068d8542ea05d6c375e4
Rajesh Malviya <rmalviya@zulip.com>
2026-02-18 13:52:46 +0530

    ios [nfc]: In Xcode, run swift-format
    In the Xcode menu bar, Editor -> Structure -> Format file with 'swift-format'.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,5 @@
-import UIKit
 import Flutter
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,28 +7,41 @@ import Flutter
 
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplication
+      .LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
 
     // Use `DesignVariables.mainBackground` color as the background color
     // of the default UIView.
-    window?.backgroundColor = UIColor(named: "LaunchBackground");
+    window?.backgroundColor = UIColor(named: "LaunchBackground")
 
     let controller = window?.rootViewController as! FlutterViewController
 
     // Retrieve the remote notification payload from launch options;
     // this will be null if the launch wasn't triggered by a notification.
-    let notificationPayload = launchOptions?[.remoteNotification] as? [AnyHashable : Any]
-    let api = NotificationHostApiImpl(notificationPayload.map { NotificationDataFromLaunch(payload: $0) })
-    NotificationHostApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: api)
+    let notificationPayload =
+      launchOptions?[.remoteNotification] as? [AnyHashable: Any]
+    let api = NotificationHostApiImpl(
+      notificationPayload.map { NotificationDataFromLaunch(payload: $0) }
+    )
+    NotificationHostApiSetup.setUp(
+      binaryMessenger: controller.binaryMessenger,
+      api: api
+    )
 
     notificationTapEventListener = NotificationTapEventListener()
-    NotificationTapEventsStreamHandler.register(with: controller.binaryMessenger, streamHandler: notificationTapEventListener!)
+    NotificationTapEventsStreamHandler.register(
+      with: controller.binaryMessenger,
+      streamHandler: notificationTapEventListener!
+    )
 
     UNUserNotificationCenter.current().delegate = self
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
   }
 
   override func userNotificationCenter(
@@ -61,11 +74,14 @@ private class NotificationHostApiImpl: NotificationHostApi {
 class NotificationTapEventListener: NotificationTapEventsStreamHandler {
   var eventSink: PigeonEventSink<NotificationTapEvent>?
 
-  override func onListen(withArguments arguments: Any?, sink: PigeonEventSink<NotificationTapEvent>) {
+  override func onListen(
+    withArguments arguments: Any?,
+    sink: PigeonEventSink<NotificationTapEvent>
+  ) {
     eventSink = sink
   }
 
-  func onNotificationTapEvent(payload: [AnyHashable : Any]) {
+  func onNotificationTapEvent(payload: [AnyHashable: Any]) {
     eventSink?.success(IosNotificationTapEvent(payload: payload))
   }
 }
