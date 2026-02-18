62c1bcd59cf73eb09dcd19dfe2fb8b5263d956d2
Rajesh Malviya <rmalviya@zulip.com>
2026-02-18 20:29:32 +0530

    ios: Implement ability to intercept APNs push notifications from Dart
    This uses Pigeons's handy `@FlutterApi` API to define an interface
    method that allows passing arguments from Swift to Dart and also
    get a return value back from Dart to Swift.
    
    Fixes: #1265

--- ios/NotificationService/NotificationService.swift
+++ ios/NotificationService/NotificationService.swift
@@ -19,7 +19,6 @@ class NotificationService: UNNotificationServiceExtension {
     }
 
     // Modify the notification content here...
-    // bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
 
     let headlessEngine = FlutterEngine(
       name: "zulip_headless",
@@ -37,6 +36,29 @@ class NotificationService: UNNotificationServiceExtension {
 
     GeneratedPluginRegistrant.register(with: headlessEngine)
 
+    let iosNotifFlutterApi = IosNotifFlutterApi(
+      binaryMessenger: headlessEngine.binaryMessenger
+    )
+
+    var loopRunning = true
+    iosNotifFlutterApi.didReceivePushNotification(
+      content: NotificationContent(payload: bestAttemptContent.userInfo)
+    ) { result in
+      defer { loopRunning = false }
+
+      switch result {
+      case .success(let improvedNotificationContent):
+        bestAttemptContent.title = improvedNotificationContent.title
+        if let body = improvedNotificationContent.body {
+          bestAttemptContent.body = body
+        }
+        contentHandler(bestAttemptContent)
+
+      case .failure(let error):
+        contentHandler(bestAttemptContent)
+      }
+    }
+
     // FlutterEngine even in the headless mode assumes that the event loop of
     // current thread is being polled by the system. Which is not the case in
     // the NotificationService extension, so here we manually poll the event loop.
@@ -47,25 +69,15 @@ class NotificationService: UNNotificationServiceExtension {
     //     https://github.com/flutter/flutter/pull/181645
 
     // Adapted from: https://github.com/flutter/flutter/blob/65b1ec407/engine/src/flutter/fml/platform/darwin/message_loop_darwin.mm#L44-L62
-    loop: while true {
-      let result = CFRunLoopRunInMode(.defaultMode, 1, true)
-
-      switch result {
-      case .handledSource:
-        // Keep polling until there are events in the event loop.
-        continue
-
-      case .finished, .stopped, .timedOut:
-        break loop
-
-      @unknown default:
-        fatalError()
+    let kDistantFuture = 1.0e10
+    while loopRunning {
+      let result = CFRunLoopRunInMode(.defaultMode, kDistantFuture, true)
+      if result == .stopped || result == .finished {
+        loopRunning = false
       }
     }
 
     headlessEngine.destroyContext()
-
-    contentHandler(bestAttemptContent)
   }
 
   override func serviceExtensionTimeWillExpire() {
---
+++ test/notifications/ios_service_test.dart
@@ -0,0 +1,36 @@
+import 'package:checks/checks.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zulip/host/ios_notifications.g.dart';
+import 'package:zulip/notifications/ios_service.dart';
+
+import '../example_data.dart' as eg;
+import '../model/binding.dart';
+import 'open_test.dart';
+
+void main() {
+  TestZulipBinding.ensureInitialized();
+
+  IosNotifFlutterApi iosNotifFlutterApi() => TestZulipBinding.instance.iosNotifFlutterApi!;
+
+  test('smoke', () async {
+    addTearDown(testBinding.reset);
+    IosNotificationService.init();
+
+    final title = 'test title';
+    final content = 'test content';
+    final payload = messageApnsPayload(
+      eg.streamMessage(content: content),
+      title: title);
+
+    final result = await iosNotifFlutterApi()
+      .didReceivePushNotification(NotificationContent(payload: payload));
+    check(result)
+      ..title.equals(title)
+      ..body.equals(content);
+  });
+}
+
+extension on Subject<ImprovedNotificationContent> {
+  Subject<String> get title => has((x) => x.title, 'title');
+  Subject<String?> get body => has((x) => x.body, 'body');
+}
