9290cab6cd3e4dac19710c212bfa17b08e145f73
Ruhaan <ruhaansande@gmail.com>
2026-02-28 14:48:47 +0530

    api: Add warning banner for unsupported servers
    Fixes #1457.
    
    Added a warning banner at the top of the home page for servers
    older than version '9.2' warning users that their Zulip server
    version is outside the official support window with different
    messages for administrators and regular users.
    
    The app now mirrors the legacy app behavior of a hard block
    (addressed by #267) and a soft warning.

--- lib/generated/l10n/zulip_localizations_et.dart
+++ lib/generated/l10n/zulip_localizations_et.dart
@@ -824,6 +824,22 @@ class ZulipLocalizationsEt extends ZulipLocalizations {
       'The server URL must start with http:// or https://.';
 
   @override
+  String serverCompatBannerAdminMessage(String url, String zulipVersion) {
+    return '$url is running Zulip Server $zulipVersion, which is unsupported. Please upgrade your server as soon as possible.';
+  }
+
+  @override
+  String serverCompatBannerUserMessage(String url, String zulipVersion) {
+    return '$url is running Zulip Server $zulipVersion, which is unsupported. Please contact your server administrator about upgrading.';
+  }
+
+  @override
+  String get serverCompatBannerDismissLabel => 'Dismiss';
+
+  @override
+  String get serverCompatBannerLearnMoreLabel => 'Learn more';
+
+  @override
   String get spoilerDefaultHeaderText => 'Spoiler';
 
   @override
