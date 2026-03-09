49a77e202fef29dc116ec49e675d3fc94801bf57
Khader M Khudair <kkhudair16@gmail.com>
2026-03-09 13:29:00 +0200

    store: Handle dateJoined in realm_user update events
    Starting at FL 475, the server sends realm_user update events
    for date_joined when an imported stub user or a user created
    via the API logs in for the first time.

--- test/model/user_test.dart
+++ test/model/user_test.dart
@@ -84,6 +84,18 @@ void main() {
         deliveryEmail: const JsonNullable('c@mail.example')));
       check(getUser()).deliveryEmail.equals('c@mail.example');
     });
+
+    test('dateJoined', () async {
+      final user = eg.user(dateJoined: '2024-01-01T00:00:00+00:00');
+      final store = eg.store(initialSnapshot: eg.initialSnapshot(
+        realmUsers: [eg.selfUser, user]));
+
+      User getUser() => store.getUser(user.userId)!;
+
+      await store.handleEvent(RealmUserUpdateEvent(id: 1, userId: user.userId,
+        dateJoined: '2024-06-15T12:00:00+00:00'));
+      check(getUser()).dateJoined.equals('2024-06-15T12:00:00+00:00');
+    });
   });
 
   testWidgets('UserStatusEvent', (tester) async {
