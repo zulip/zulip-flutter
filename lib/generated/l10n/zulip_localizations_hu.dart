ed7b5115b0c4f4ab935a22d00269584cef404ec7
Khader M Khudair <kkhudair16@gmail.com>
2026-03-09 13:30:09 +0200

    profile: Show date joined on profile page
    Show the user's date joined (or "Imported on" for imported stub
    users) as the first row in the profile data table, styled like
    the custom profile fields.
    
    Fixes #2201.

--- lib/generated/l10n/zulip_localizations_hu.dart
+++ lib/generated/l10n/zulip_localizations_hu.dart
@@ -81,6 +81,12 @@ class ZulipLocalizationsHu extends ZulipLocalizations {
   String get profileButtonSendDirectMessage => 'Send direct message';
 
   @override
+  String get profileDateJoinedLabel => 'Date joined';
+
+  @override
+  String get profileDateImportedLabel => 'Imported on';
+
+  @override
   String get errorCouldNotShowUserProfile => 'Could not show user profile.';
 
   @override
