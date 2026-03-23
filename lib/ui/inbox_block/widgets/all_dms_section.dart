65f81fdbd75131d40415e14179a79cdf8c98c2d1
Михаил <>
2026-03-23 01:46:27 -0700

    - провел декомпозицию инбокс-блока

---
+++ lib/ui/inbox_block/widgets/all_dms_section.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/sticky_header.dart';
+import '../inbox.dart';
+import '../inbox_section_data_model.dart';
+import 'headers/inbox_all_dms_header_item.dart';
+import 'inbox_dm_item.dart';
+
+class AllDmsSection extends StatelessWidget {
+  const AllDmsSection({
+    super.key,
+    required this.data,
+    required this.collapsed,
+    required this.pageState,
+  });
+
+  final AllDmsSectionData data;
+  final bool collapsed;
+  final InboxPageState pageState;
+
+  @override
+  Widget build(BuildContext context) {
+    final header = InboxAllDmsHeaderItem(
+      count: data.count,
+      hasMention: data.hasMention,
+      collapsed: collapsed,
+      pageState: pageState,
+      sectionContext: context,
+    );
+    return StickyHeaderItem(
+      header: header,
+      child: Column(
+        children: [
+          header,
+          if (!collapsed)
+            ...data.items.map((item) {
+              final (narrow, count, hasMention) = item;
+              return InboxDmItem(
+                narrow: narrow,
+                count: count,
+                hasMention: hasMention,
+              );
+            }),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/inbox_block/widgets/headers/inbox_channel_header_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../../api/model/model.dart';
+import '../../../../generated/l10n/zulip_localizations.dart';
+import '../../../values/icons.dart';
+import '../../../values/theme.dart';
+import '../../../widgets/action_sheet.dart';
+import 'header_item.dart';
+
+@visibleForTesting
+class InboxChannelHeaderItem extends HeaderItem with InboxLongPressable {
+  final Subscription subscription;
+
+  const InboxChannelHeaderItem({
+    super.key,
+    required this.subscription,
+    required super.collapsed,
+    required super.pageState,
+    required super.count,
+    required super.hasMention,
+    required super.sectionContext,
+  });
+
+  @override
+  String title(ZulipLocalizations zulipLocalizations) => subscription.name;
+  @override
+  IconData get icon => iconDataForStream(subscription);
+  @override
+  Color collapsedIconColor(context) =>
+      colorSwatchFor(context, subscription).iconOnPlainBackground;
+  @override
+  Color uncollapsedIconColor(context) =>
+      colorSwatchFor(context, subscription).iconOnBarBackground;
+  @override
+  Color uncollapsedBackgroundColor(context) =>
+      colorSwatchFor(context, subscription).barBackground;
+  @override
+  int? get channelId => subscription.streamId;
+
+  @override
+  Future<void> onCollapseButtonTap() async {
+    await super.onCollapseButtonTap();
+    if (collapsed) {
+      pageState.uncollapseStream(subscription.streamId);
+    } else {
+      pageState.collapseStream(subscription.streamId);
+    }
+  }
+
+  @override
+  Future<void> onRowTap() => onCollapseButtonTap(); // TODO open channel narrow
+
+  @override
+  Future<void> onLongPress() async {
+    showChannelActionSheet(sectionContext, channelId: subscription.streamId);
+  }
+}
