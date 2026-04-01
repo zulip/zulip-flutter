59ebe9450ce69cb187dc8b48d1633aa4ad0439b7
Chris Bobbe <cbobbe@zulip.com>
2026-03-31 17:03:27 -0700

    action_sheet: Use ZulipIcons.check for resolved topic in action sheet header

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -790,8 +790,15 @@ InlineSpan channelTopicLabelSpan({
         color: color,
         padBefore: true,
         padAfter: true),
-      if (topic.displayName != null)
-        TextSpan(text: topic.displayName)
+      if (topic.isResolved)
+        InlineIcon.asWidgetSpan(
+          icon: ZulipIcons.check,
+          fontSize: fontSize,
+          baselineType: baselineType,
+          color: color,
+          padAfter: true),
+      if (topic.unresolve().displayName != null)
+        TextSpan(text: topic.unresolve().displayName)
       else
         TextSpan(
           style: TextStyle(fontStyle: FontStyle.italic),
