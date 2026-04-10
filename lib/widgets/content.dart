4770e1acbca5d948db247ce4898a577f5a9c139d
MritunjayTiwari14 <shreyashtiwari14.11@gmail.com>
2026-04-10 17:21:57 +0530

    content: Scale thumbnail for BlockImage and video according to `realmMediaPreviewSize`
    Fixes #2177.
    
    Post FL-469, the server introduced a realm setting that
    controls the size of both block and inline images, as
    well as video thumbnails in content. The Flutter client
    should follow the same steps to scale the image accordingly.
    See api changelog:
      https://chat.zulip.org/api/changelog#:~:text=Feature%20level%20469,thumbnails%20in%20messages.
    
    Following the previous commit, we continue extending
    support for upsizing thumbnail for video and block image.
    
    Note:
    - We currently do scale video thumbnail
    even though it's just a black container just
    for the sake of complexity and consistency.
    Though, it would be implemented somewhere in future.
    For more details, see:
      https://chat.zulip.org/#narrow/channel/530-mobile-design/topic/Scaling.20of.20Media.20according.20to.20.60RealmMediaPreviewSize.60.20.23F2177/near/2430006

--- lib/widgets/content.dart
+++ lib/widgets/content.dart
@@ -642,7 +642,10 @@ class MessageImagePreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _Image(node: node, size: MessageMediaContainer.size,
+    final store = PerAccountStoreWidget.of(context);
+    final sizeMultiplier = store.realmMediaPreviewSize.multiplier;
+
+    return _Image(node: node, size: MessageMediaContainer.baseSize * sizeMultiplier,
       buildContainer: (onTap, child) {
         return MessageMediaContainer(onTap: onTap, child: child);
       });
@@ -719,10 +722,13 @@ class MessageMediaContainer extends StatelessWidget {
   final Widget? child;
 
   /// The container's size, in logical pixels.
-  static const size = Size(150, 100);
+  static const baseSize = Size(150, 100);
 
   @override
   Widget build(BuildContext context) {
+    final store = PerAccountStoreWidget.of(context);
+    final sizeMultiplier = store.realmMediaPreviewSize.multiplier;
+
     return GestureDetector(
       onTap: onTap,
       child: UnconstrainedBox(
@@ -736,7 +742,7 @@ class MessageMediaContainer extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(1),
               child: SizedBox.fromSize(
-                size: size,
+                size: baseSize * sizeMultiplier,
                 child: child))))));
   }
 }
@@ -1378,7 +1384,7 @@ class InlineImage extends StatelessWidget {
       ? Size(node.originalWidth!, node.originalHeight!) / devicePixelRatio * sizeMultiplier
       // Layout plan when original dimensions are unknown:
       // a [MessageMediaContainer]-sized and -colored rectangle.
-      : MessageMediaContainer.size * sizeMultiplier;
+      : MessageMediaContainer.baseSize * sizeMultiplier;
 
     // (a) Don't let tall, thin images take up too much vertical space,
     //     which could be annoying to scroll through. And:
