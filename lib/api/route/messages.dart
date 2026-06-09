fd08bb0cf1937469273b33389431ed7219608fc7
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2026-06-09 22:52:18 +0430

    api: In sendMessage, use "channel" when supported
    The modern type "channel" was added in server-9 (FL-248).

--- lib/api/route/messages.dart
+++ lib/api/route/messages.dart
@@ -134,12 +134,13 @@ Future<SendMessageResult> sendMessage(
   String? localId,
   bool? readBySender,
 }) {
+  final supportsTypeChannel = connection.zulipFeatureLevel! >= 248; // TODO(server-9)
   final supportsTypeDirect = connection.zulipFeatureLevel! >= 174; // TODO(server-7)
   final supportsReadBySender = connection.zulipFeatureLevel! >= 236; // TODO(server-8)
   return connection.post('sendMessage', SendMessageResult.fromJson, 'messages', {
     ...(switch (destination) {
       StreamDestination() => {
-        'type': RawParameter('stream'),
+        'type': supportsTypeChannel ? RawParameter('channel') : RawParameter('stream'),
         'to': destination.streamId,
         'topic': RawParameter(destination.topic.apiName),
       },
--- test/widgets/compose_box_test.dart
+++ test/widgets/compose_box_test.dart
@@ -1027,7 +1027,7 @@ void main() {
         ..method.equals('POST')
         ..url.path.equals('/api/v1/messages')
         ..bodyFields.deepEquals({
-            'type': 'stream',
+            'type': 'channel',
             'to': '123',
             'topic': 'some topic',
             'content': 'hello world',
