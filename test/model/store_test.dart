0a13d1e357b9335550206dae518cdf2b0589ec47
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2026-06-10 22:55:44 +0430

    dart [nfc]: Use dot shorthands for TwentyFourHourTimeMode

--- test/model/store_test.dart
+++ test/model/store_test.dart
@@ -12,7 +12,6 @@ import 'package:zulip/api/core.dart';
 import 'package:zulip/api/exception.dart';
 import 'package:zulip/api/model/events.dart';
 import 'package:zulip/api/model/initial_snapshot.dart';
-import 'package:zulip/api/model/model.dart';
 import 'package:zulip/api/route/events.dart';
 import 'package:zulip/api/route/realm.dart';
 import 'package:zulip/log.dart';
@@ -815,7 +814,7 @@ void main() {
 
       // Pick some arbitrary event and check it gets processed on the store.
       check(store.userSettings.twentyFourHourTime)
-        .equals(TwentyFourHourTimeMode.twelveHour);
+        .equals(.twelveHour);
       connection.prepare(json: GetEventsResult(events: [
         UserSettingsUpdateEvent(id: 2,
           property: .twentyFourHourTime, value: true),
@@ -823,7 +822,7 @@ void main() {
       updateMachine.debugAdvanceLoop();
       async.elapse(Duration.zero);
       check(store.userSettings.twentyFourHourTime)
-        .equals(TwentyFourHourTimeMode.twentyFourHour);
+        .equals(.twentyFourHour);
     }));
 
     void checkReload(FutureOr<void> Function() prepareError, {
@@ -854,7 +853,7 @@ void main() {
         updateMachine.debugPauseLoop();
         updateMachine.poll();
         check(store.userSettings.twentyFourHourTime)
-          .equals(TwentyFourHourTimeMode.twelveHour);
+          .equals(.twelveHour);
         connection.prepare(json: GetEventsResult(events: [
           UserSettingsUpdateEvent(id: 2,
             property: .twentyFourHourTime, value: true),
@@ -862,7 +861,7 @@ void main() {
         updateMachine.debugAdvanceLoop();
         async.elapse(Duration.zero);
         check(store.userSettings.twentyFourHourTime)
-          .equals(TwentyFourHourTimeMode.twentyFourHour);
+          .equals(.twentyFourHour);
       });
     }
 
