4835d429ee8712714a3fb2cf7280b20d17779618
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2026-06-13 10:00:56 +0430

    dart [nfc]: Use bare enum values in SystemGroupName.displayName

--- lib/api/model/permission.dart
+++ lib/api/model/permission.dart
@@ -363,13 +363,13 @@ enum SystemGroupName implements DefaultGroupName {
   /// Display names mirror the web app's display name strings.
   /// See `system_user_groups_list` in web/src/settings_config.ts.
   String displayName(ZulipLocalizations zulipLocalizations) => switch (this) {
-    .everyoneOnInternet => zulipLocalizations.systemGroupNameEveryoneOnInternet,
-    .everyone => zulipLocalizations.systemGroupNameEveryone,
-    .members => zulipLocalizations.systemGroupNameMembers,
-    .fullMembers => zulipLocalizations.systemGroupNameFullMembers,
-    .moderators => zulipLocalizations.systemGroupNameModerators,
-    .administrators => zulipLocalizations.systemGroupNameAdministrators,
-    .owners => zulipLocalizations.systemGroupNameOwners,
-    .nobody => zulipLocalizations.systemGroupNameNobody,
+    everyoneOnInternet => zulipLocalizations.systemGroupNameEveryoneOnInternet,
+    everyone => zulipLocalizations.systemGroupNameEveryone,
+    members => zulipLocalizations.systemGroupNameMembers,
+    fullMembers => zulipLocalizations.systemGroupNameFullMembers,
+    moderators => zulipLocalizations.systemGroupNameModerators,
+    administrators => zulipLocalizations.systemGroupNameAdministrators,
+    owners => zulipLocalizations.systemGroupNameOwners,
+    nobody => zulipLocalizations.systemGroupNameNobody,
   };
 }
