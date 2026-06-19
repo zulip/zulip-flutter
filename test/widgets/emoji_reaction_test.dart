026bb56be1358b8bf367a23ac4ea74ce77f48a6b
Sayed Mahmood Sayedi <smahmood.sayedi@gmail.com>
2026-06-19 22:30:17 +0430

    emoji-reaction: Highlight message reaction emojis in reaction emoji picker
    To match the behavior on the web.
    
    In the next commits where we remove a reaction when an already-voted
    emoji is tapped from the emoji picker, this change will hightlight that
    emoji so the user knows the consequence of their tap (adding a reaction
    by tapping an unhighlighted emoji and removing a reaction by tapping
    a hightlighted emoji).

--- test/widgets/emoji_reaction_test.dart
+++ test/widgets/emoji_reaction_test.dart
@@ -15,12 +15,15 @@ import 'package:legacy_checks/legacy_checks.dart';
 import 'package:zulip/api/model/events.dart';
 import 'package:zulip/api/model/model.dart';
 import 'package:zulip/api/route/realm.dart';
+import 'package:zulip/model/emoji.dart';
 import 'package:zulip/model/narrow.dart';
 import 'package:zulip/model/store.dart';
+import 'package:zulip/widgets/color.dart';
 import 'package:zulip/widgets/content.dart';
 import 'package:zulip/widgets/emoji_reaction.dart';
 import 'package:zulip/widgets/icons.dart';
 import 'package:zulip/widgets/message_list.dart';
+import 'package:zulip/widgets/theme.dart';
 
 import '../api/fake_api.dart';
 import '../example_data.dart' as eg;
@@ -520,6 +523,45 @@ void main() {
           emojiCode: '1',
           emojiName: 'buzzing'),
       ]);
+
+      debugNetworkImageHttpClientProvider = null;
+    });
+
+    testWidgets('highlight self-voted emoji entries', (tester) async {
+      void checkHighlighted(Emoji emoji, {required bool expected}) {
+        final designVariables = DesignVariables.of(
+          tester.firstElement(find.byType(EmojiPickerListEntry)));
+        final expectedHighlightColor = expected
+          ? designVariables.contextMenuItemBg.withFadedAlpha(0.20)
+          : null;
+
+        check(find.descendant(
+          of: find.byWidgetPredicate((widget) => widget is EmojiPickerListEntry
+            && Emoji.fromEmojiCandidate(widget.emoji) == emoji),
+          matching: find.byWidgetPredicate((widget) => widget is Container
+            && widget.color == expectedHighlightColor))
+        ).findsOne();
+      }
+
+      final otherReaction = Reaction(emojiName: 'tada', emojiCode: '1f389',
+        reactionType: .unicodeEmoji, userId: eg.otherUser.userId);
+      final message = eg.streamMessage(reactions: [
+        eg.unicodeEmojiReaction, eg.zulipExtraEmojiReaction, otherReaction,
+      ]);
+
+      await setupEmojiPicker(tester, message: message, narrow: TopicNarrow.ofMessage(message));
+
+      // Check that self-voted emojis are highlighted.
+      checkHighlighted(Emoji.fromReaction(eg.unicodeEmojiReaction), expected: true);
+      checkHighlighted(Emoji.fromReaction(eg.zulipExtraEmojiReaction), expected: true);
+
+      // Check that other-user-voted emojis are not highlighted.
+      checkHighlighted(Emoji.fromReaction(otherReaction), expected: false);
+
+      // Check that other emojis are not highlighted either.
+      checkHighlighted(Emoji(name: 'slight_smile', code: '1f642', type: .unicodeEmoji),
+        expected: false);
+      // …
 
       debugNetworkImageHttpClientProvider = null;
     });
