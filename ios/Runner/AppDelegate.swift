266bb9b73ce96534bf96e8ed42c37a68ab1602c0
Rajesh Malviya <rmalviya@zulip.com>
2026-08-14 16:35:21 +0530

    pigeon [nfc]: Remove the now-unused NotificationHostApi
    The previous commit stopped using `getNotificationDataFromLaunch`,
    which was this API's only method.  So remove the API and its
    `NotificationDataFromLaunch` type, along with the iOS implementation
    and the `launchOptions` read that fed it, and regenerate.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -21,18 +21,6 @@ import UIKit
     IosNativeHostApiSetup.setUp(
       binaryMessenger: controller.binaryMessenger, api: IosNativeHostApiImpl())
 
-    // Retrieve the remote notification payload from launch options;
-    // this will be null if the launch wasn't triggered by a notification.
-    let notificationPayload =
-      launchOptions?[.remoteNotification] as? [AnyHashable: Any]
-    let api = NotificationHostApiImpl(
-      notificationPayload.map { NotificationDataFromLaunch(payload: $0) }
-    )
-    NotificationHostApiSetup.setUp(
-      binaryMessenger: controller.binaryMessenger,
-      api: api
-    )
-
     notificationTapEventListener = NotificationTapEventListener()
     NotificationTapEventsStreamHandler.register(
       with: controller.binaryMessenger,
@@ -67,16 +55,3 @@ import UIKit
     }
   }
 }
-
-private class NotificationHostApiImpl: NotificationHostApi {
-  private let maybeDataFromLaunch: NotificationDataFromLaunch?
-
-  init(_ maybeDataFromLaunch: NotificationDataFromLaunch?) {
-    self.maybeDataFromLaunch = maybeDataFromLaunch
-  }
-
-  func getNotificationDataFromLaunch() -> NotificationDataFromLaunch? {
-    maybeDataFromLaunch
-  }
-}
-
